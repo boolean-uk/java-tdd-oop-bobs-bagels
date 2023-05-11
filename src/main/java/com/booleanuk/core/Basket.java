84479e5eeb20fb1cef9031ca012f396f670ae78a
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-11 17:45:28 +0200

    Added source code from previous bobs bagels exercise

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,45 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
 
+public class Basket {
+    ArrayList<Item> items;
+    Inventory inventory;
+    int basketCapacity;
+
+    public Basket() {
+        this.items = new ArrayList<>();
+        this.basketCapacity = 3;
+        this.inventory = new Inventory();
+    }
+
+    public boolean addItem(String sku){
+        if (this.items.size() < this.basketCapacity){
+            this.items.add(sku);
+            return true;
+        }
+        System.out.println("Basket is full, could not add bagel!");
+        return false;
+    }
+
+    public boolean removeItem(String sku) {
+        if (this.items.contains(sku)) {
+            this.items.remove(sku);
+            return true;
+        }
+        System.out.println("The bagel does not exist in the basket!");
+        return false;
+    }
+
+    public boolean updateBasketCapacity(int newCapacity) {
+        if(newCapacity <= 0) {
+            System.out.println("Cannot update basket capacity to zero or less.");
+            return false;
+        } else if (newCapacity < this.items.size()) {
+            System.out.println("Cannot update basket capacity to a size smaller than current basket size.");
+            return false;
+        }
+        this.basketCapacity = newCapacity;
+        return true;
+    }
 }
