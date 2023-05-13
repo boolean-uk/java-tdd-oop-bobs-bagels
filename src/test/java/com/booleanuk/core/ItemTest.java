4ee959ae815220e61de867d38c774007a0f36f72
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-13 19:30:26 +0200

    Test item type

--- src/test/java/com/booleanuk/core/ItemTest.java
+++ src/test/java/com/booleanuk/core/ItemTest.java
@@ -1,4 +1,17 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ItemTest {
+
+    @Test
+    public void testType () {
+        Item item = new Item("Bagel", 0.29, "BGLG", "Gluten-free");
+        Assertions.assertEquals("Bagel", item.getType());
+        item.setType("Coffee");
+        Assertions.assertEquals("Coffee", item.getType());
+    }
+
+
 }
