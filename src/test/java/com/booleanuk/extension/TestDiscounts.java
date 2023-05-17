29c7fa3f918dbaff98ae43abf59a24316654366f
Farhang Younis <comp-sci@hotmail.com>
2023-05-17 08:36:49 +0200

    Added test for discount with fillings

--- src/test/java/com/booleanuk/extension/TestDiscounts.java
+++ src/test/java/com/booleanuk/extension/TestDiscounts.java
@@ -9,6 +9,7 @@ public class TestDiscounts {
         Basket basket = new Basket();
         Bagel bagel = new Bagel(Sku.BGLO,0.49,0.0,2,"Bagel","Onion");
         Bagel bagel2 = new Bagel(Sku.BGLP,0.39,0.0,12,"Bagel","Plain");
+        bagel2.getFillings().add(new Filling(Sku.FILB,0.12,0.0,0,"Filling","Bacon"));
         Bagel bagel3 = new Bagel(Sku.BGLE,0.49,0.0,6,"Bagel","Everything");
         Coffee coffee = new Coffee(Sku.COFC,1.29,0.0,3,"Coffee","Capuccino");
         System.out.println(bagel.getPrice());
