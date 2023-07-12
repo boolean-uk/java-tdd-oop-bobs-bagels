23ffa594bd163366db4c185b835e4627934a830a
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-12 15:20:55 +0200

    Added bagel 1. test and neccessary implementation

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -1,10 +1,20 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BagelTest {
-    @Test
-    public void addFillingTest() {
+    public BagelTest() {
+        Item.fillItemPriceVariants();
+        Filling.fillItemPriceVariants();
+        Bagel.fillItemPriceVariants();
+    }
 
+    @Test
+    public void constructorTest() {
+        Bagel bagel = new Bagel("BGLP");
+        Assertions.assertEquals("BGLP", bagel.getSku());
+        Assertions.assertEquals(0.39, bagel.getPrice());
+        Assertions.assertEquals("Plain", bagel.getVariant());
     }
 }
