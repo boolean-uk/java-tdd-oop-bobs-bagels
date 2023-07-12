09c514709f471ed9d7f023c2510e7a16edb7f8d4
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-12 15:41:05 +0200

    Implement Basket

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Getter
 public class Basket {
@@ -17,28 +18,68 @@ public class Basket {
     }
 
     public void addBagel(Bagel bagel) {
-        // TODO
+        if (isFull()) {
+            throw new IllegalStateException("Cannot add new bagel - basket is full");
+        }
+        bagels.merge(bagel, 1, Integer::sum);
     }
 
     public void removeBagel(Bagel bagel) {
-        // TODO
+        if (!bagels.containsKey(bagel)) {
+            throw new NoSuchElementException(String.format("Cannot remove %s - it's not in the basket", bagel));
+        }
+
+        if (bagels.get(bagel) > 1) {
+            bagels.put(bagel, bagels.get(bagel) - 1);
+        } else {
+            bagels.remove(bagel);
+        }
     }
 
     public void addCoffee(Coffee coffee) {
-        // TODO
+        if (isFull()) {
+            throw new IllegalStateException();
+        }
+        coffees.merge(coffee, 1, Integer::sum);
     }
 
     public void removeCoffee(Coffee coffee) {
-        // TODO
+        if (!coffees.containsKey(coffee)) {
+            throw new NoSuchElementException(String.format("Cannot remove %s - it's not in the basket", coffee));
+        }
+
+        if (coffees.get(coffee) > 1) {
+            coffees.put(coffee, bagels.get(coffee) - 1);
+        } else {
+            coffees.remove(coffee);
+        }
     }
 
     public BigDecimal totalPrice() {
-        // TODO
-        return null;
+        var totalPrice = bagels.entrySet().stream()
+                .map(e -> {
+                    var price = e.getKey().price();
+                    var amount = e.getValue();
+
+                    return price.multiply(BigDecimal.valueOf(amount));
+                })
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return coffees.entrySet().stream()
+                .map(e -> {
+                    var price = e.getKey().getPrice();
+                    var amount = e.getValue();
+
+                    return price.multiply(BigDecimal.valueOf(amount));
+                })
+                .reduce(totalPrice, BigDecimal::add);
     }
 
     public void resize(int capacity) {
-        // TODO
+        if (capacity < itemAmount()) {
+            throw new IllegalArgumentException(String.format("New capacity cannot be smaller than amount of items in basket [%d]", itemAmount()));
+        }
+
+        this.capacity = capacity;
     }
 
     private int itemAmount() {
