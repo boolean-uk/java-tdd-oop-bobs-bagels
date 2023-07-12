474b40c8661162c01ecba1bc2a1e4ef96c88f9c9
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-12 15:12:08 +0200

    Add red test: removing bagel from basket

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,12 +1,23 @@
 package com.booleanuk.core;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasketTest {
     @Test
-    public void testAddingBagel() {
+    public void testAddingBagelToBasket() {
         Basket basket = new Basket();
-        Assertions.assertTrue(basket.add(new Bagel(Bagel.BagelType.PLAIN)));
+
+        assertTrue(basket.add(new Bagel(Bagel.BagelType.PLAIN)));
+    }
+
+    @Test
+    public void testRemovingBagelFromBasket() {
+        Basket basket = new Basket();
+        Bagel bagel = new Bagel(Bagel.BagelType.PLAIN);
+        basket.add(bagel);
+
+        assertTrue(basket.remove(bagel.getId()));
     }
 }
