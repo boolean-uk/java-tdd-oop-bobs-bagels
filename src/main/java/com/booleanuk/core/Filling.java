5c5fc8dde4a985be285559be5dde3576f6a66cc7
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-12 15:15:19 +0200

    added tests for product

--- src/main/java/com/booleanuk/core/Filling.java
+++ src/main/java/com/booleanuk/core/Filling.java
@@ -1,4 +1,4 @@
 package com.booleanuk.core;
 
-public class Filling {
+public class Filling extends Product {
 }
--- src/test/java/com/booleanuk/core/ProductTest.java
+++ src/test/java/com/booleanuk/core/ProductTest.java
@@ -1,4 +1,28 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ProductTest {
+    private Product bagel;
+    private static final String SKU = "BGLO";
+    private static final double PRICE = 0.49;
+    private static final String VARIANT = "Onion";
+
+    @BeforeEach
+    public void setUp() {
+        bagel = new Bagel(SKU, PRICE, VARIANT);
+    }
+
+    public void testGetSKU(){
+        Assertions.assertEquals(SKU, bagel.getSKU());
+    }
+
+    public void testGetPrice(){
+        Assertions.assertEquals(PRICE, bagel.getPrice());
+    }
+    
+    public void testGetVariant(){
+        Assertions.assertEquals(VARIANT, bagel.getVariant());
+    }
 }
