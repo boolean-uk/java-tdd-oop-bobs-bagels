1b9dacb381f7ae57212afa1dc734880b0f499292
PawelCagara <pawel.cagara@talan.com>
2023-07-12 14:21:10 +0200

    getPriceTest() created and getSKUTest() passed

--- src/test/java/com/booleanuk/core/BagleTest.java
+++ src/test/java/com/booleanuk/core/BagleTest.java
@@ -3,9 +3,12 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 public class BagleTest {
 
-    Bagle bagle;
+    private Bagle bagle;
+    private static final DecimalFormat decfor = new DecimalFormat("0.00");
 
     public BagleTest() {
         this.bagle = new Bagle("BGLO", 0.49, "Onion");
@@ -14,5 +17,14 @@ public class BagleTest {
     @Test
     public void getSKUTest() {
         Assertions.assertEquals("BGLO", bagle.getSKU());
+        Assertions.assertNotEquals("BGLOO", bagle.getSKU());
     }
+
+    @Test
+    public void getPriceTest() {
+        Assertions.assertEquals(0.49, bagle.getPrice());
+        Assertions.assertNotEquals(1,bagle.getPrice());
+    }
+
+    public void
 }
