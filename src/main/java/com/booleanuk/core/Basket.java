5a14bd5ca66012612b5ec4b88b2de6e1620aa58d
himok <himok.pk@gmail.com>
2023-07-12 16:31:32 +0200

    green stage of testAddProduct_WhenQuantityIsNotValid_ShouldReturnFalse and testAddProduct_WhenBasketIsFull_ShouldReturnFalse

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -5,23 +5,29 @@ import java.util.HashMap;
 public class Basket {
     private HashMap<Product, Integer> products;
     private Inventory inventory;
-    private int quantity;
+    private int basketCapacity;
+    private int basketQuantity;
 
     public Basket(Inventory inventory, int quantity) {
         this.products = new HashMap<>();
         this.inventory = inventory;
-        this.quantity = quantity;
+        this.basketCapacity = quantity;
     }
 
 
     public boolean addProduct(String sku, int quantity) {
+        if(quantity <= 0)
+            return false;
         Product newProduct = inventory.getProduct(sku);
 
-        if(newProduct != null){
-            products.put(newProduct, quantity);
-            return true;
-        }
+        if(newProduct == null)
+            return false;
 
-        return false;
+        if(basketCapacity - basketQuantity < quantity)
+            return false;
+
+        products.put(newProduct, quantity);
+        basketQuantity += quantity;
+        return true;
     }
 }
