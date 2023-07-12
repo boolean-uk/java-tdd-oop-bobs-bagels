4f9b20e1276f9912d0e492490b4e4d009214f72e
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-12 15:17:49 +0200

    Added 2. and 3. item test and neccessary implementation

--- src/test/java/com/booleanuk/core/ItemTest.java
+++ src/test/java/com/booleanuk/core/ItemTest.java
@@ -13,6 +13,21 @@ public class ItemTest {
         Item item2 = new Item("COFL");
         Assertions.assertEquals("BGLO", item1.getSku());
         Assertions.assertEquals("COFL", item2.getSku());
+    }
 
+    @Test
+    public void variantSetterAndGetterTest() {
+        Item item1 = new Item("BGLO");
+        Item item2 = new Item("COFL");
+        Assertions.assertEquals("Onion", item1.getVariant());
+        Assertions.assertEquals("Latte", item2.getVariant());
+    }
+
+    @Test
+    public void priceSetterAndGetterTest() {
+        Item item1 = new Item("BGLO");
+        Item item2 = new Item("COFL");
+        Assertions.assertEquals(0.49, item1.getPrice());
+        Assertions.assertEquals(1.29, item2.getPrice());
     }
 }
