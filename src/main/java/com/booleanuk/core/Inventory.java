cb5742251da3a25300c45b455bc30ed7e00881da
Agnieszka Zagorna <a.h.zagorna@gmail.com>
2023-07-12 18:08:36 +0200

    added rest of the items to singleton Inventory class

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -12,6 +12,18 @@ public class Inventory {
     private Inventory(){
         inventoryItemList.add(new InventoryItem("BGLO",0.49,"Bagel","Onion"));
         inventoryItemList.add(new InventoryItem("BGLP",0.39,"Bagel","Plain"));
+        inventoryItemList.add(new InventoryItem("BGLE",0.49,"Bagel","Everything"));
+        inventoryItemList.add(new InventoryItem("BGLS",0.49,"Bagel","Sesame"));
+        inventoryItemList.add(new InventoryItem("COFB",0.99,"Coffee","Black"));
+        inventoryItemList.add(new InventoryItem("COFW",1.19,"Coffee","White"));
+        inventoryItemList.add(new InventoryItem("COFC",1.29,"Coffee","Capuccino"));
+        inventoryItemList.add(new InventoryItem("COFL",1.29,"Coffee","Latte"));
+        inventoryItemList.add(new InventoryItem("FILB",0.12,"Filling","Bacon"));
+        inventoryItemList.add(new InventoryItem("FILE",0.12,"Filling","Egg"));
+        inventoryItemList.add(new InventoryItem("FILC",0.12,"Filling","Cheese"));
+        inventoryItemList.add(new InventoryItem("FILX",0.12,"Filling","Cream Cheese"));
+        inventoryItemList.add(new InventoryItem("FILS",0.12,"Filling","Smoked Salmon"));
+        inventoryItemList.add(new InventoryItem("FILH",0.12,"Filling","Ham"));
     }
 
     public static Inventory getInstance(){
