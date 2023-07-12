39170e9552cba4c599dcedee84ef450cbba96bcf
Julia Girejko <julia.girejko@talan.com>
2023-07-12 15:47:42 +0200

    inventory fullfilling

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -9,8 +9,23 @@ public class Inventory {
 
     public Inventory() {
         inventoryList = new ArrayList<>();
+        //bagel
         inventoryList.add(new Product("BGLO", 0.49, "Bagel", "Onion"));
         inventoryList.add(new Product("BGLP", 0.39, "Bagel", "Plain"));
+        inventoryList.add(new Product("BGLE", 0.49, "Bagel", "Everything"));
+        inventoryList.add(new Product("BGLS", 0.49, "Bagel", "Sesame"));
+        //coffee
+        inventoryList.add(new Product("COFB", 0.99, "Coffee", "Black"));
+        inventoryList.add(new Product("COFW", 1.19, "Coffee", "White"));
+        inventoryList.add(new Product("COFC", 1.29, "Coffee", "Capuccino"));
+        inventoryList.add(new Product("COFL", 1.29, "Coffee", "Latte"));
+        //filling
+        inventoryList.add(new Product("FILB", 0.12, "Filling", "Bacon"));
+        inventoryList.add(new Product("FILE", 0.12, "Filling", "Egg"));
+        inventoryList.add(new Product("FILC", 0.12, "Filling", "Cheese"));
+        inventoryList.add(new Product("FILX", 0.12, "Filling", "Cream Cheese"));
+        inventoryList.add(new Product("FILS", 0.12, "Filling", "Smoked Salmon"));
+        inventoryList.add(new Product("FILH", 0.12, "Filling", "Ham"));
     }
 
     public List<Product> getInventoryList() {
