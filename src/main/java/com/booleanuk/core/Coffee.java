8e1baeac38b7ea9fccca460b7f0a58f101767a74
Anastasiya <ronas0709@gmail.com>
2023-07-12 14:26:58 +0200

    added tests for Coffee, added class Coffee

---
+++ src/main/java/com/booleanuk/core/Coffee.java
@@ -0,0 +1,23 @@
+package com.booleanuk.core;
+
+public class Coffee {
+    private String SKU;
+    private double price;
+    private String variant;
+
+    public Coffee(String SKU, double price, String variant){
+        this.SKU= SKU;
+        this.price=price;
+        this.variant=variant;
+
+    }
+    public String getSKU(){
+        return SKU;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getVariant(){
+        return variant;
+    }
+}
