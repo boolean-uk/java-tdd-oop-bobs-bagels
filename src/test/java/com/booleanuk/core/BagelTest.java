8a253401ae59a88ca265768cf1f10c04945622e4
Julia Girejko <julia.girejko@talan.com>
2023-07-12 16:36:12 +0200

    RED stage bagelTest correction

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -5,32 +5,43 @@ import org.junit.jupiter.api.Test;
 
 public class BagelTest {
     @Test
-    void chooseFillingTest_ByProduct() {
-        Bagel bagelWithoutFilling = new Bagel("BGLO", 0.49, "Bagel", "Onion");
-        String messageWithout = bagelWithoutFilling.chooseFilling(bagelWithoutFilling);
-        Assertions.assertEquals("Category of product is not \"Filling\"", messageWithout);
+    void chooseFillingTest_ByProductIsNotFilling() {
+        Product coffee = new Product("COFW", 1.19, "Coffee", "White");
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        String message = bagel.chooseFilling(coffee);
+        Assertions.assertEquals("Category of product is not \"Filling\"", message);
     }
 
     @Test
-    void chooseFillingTest_ByProduct() {
+    void chooseFillingTest_ByProductIsFilling() {
         Product filling = new Product("FILB", 0.12, "Filling", "Bacon");
-        Bagel bagelWithFilling = new Bagel("BGLO", 0.49, "Bagel", "Onion", filling);
-        String messageWith = bagelWithoutFilling.chooseFilling(bagelWithFilling);
-        Assertions.assertEquals("Filling added to bagel", messageWith);
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        String message = bagel.chooseFilling(filling);
+        Assertions.assertEquals("Filling added to bagel", message);
+
+        Assertions.assertEquals(bagel.getFilling().getSku(), filling.getSku());
+        Assertions.assertEquals(bagel.getFilling().getPrice(), filling.getPrice());
+        Assertions.assertEquals(bagel.getFilling().getName(), filling.getName());
+        Assertions.assertEquals(bagel.getFilling().getVariant(), filling.getVariant());
     }
 
     @Test
-    void chooseFillingTest_ByVariant() {
-        Bagel bagelWithoutFilling = new Bagel("BGLO", 0.49, "Bagel", "Onion");
-        String messageWithout = bagelWithoutFilling.chooseFilling(bagelWithoutFilling.getVariant());
-        Assertions.assertEquals("Category of product is not \"Filling\"", messageWithout);
+    void chooseFillingTest_ByVariantDoesNotExist() {
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        String message = bagel.chooseFilling("chocolate");
+        Assertions.assertEquals("Variant does not exist", message);
     }
 
     @Test
-    void chooseFillingTest_ByProduct() {
+    void chooseFillingTest_ByVariantExist() {
         Product filling = new Product("FILB", 0.12, "Filling", "Bacon");
-        Bagel bagelWithFilling = new Bagel("BGLO", 0.49, "Bagel", "Onion", filling);
-        String messageWith = bagelWithoutFilling.chooseFilling(bagelWithFilling.getVariant());
-        Assertions.assertEquals("Filling added to bagel", messageWith);
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        String message = bagel.chooseFilling("Bacon");
+        Assertions.assertEquals("Filling added to bagel", message);
+
+        Assertions.assertEquals(bagel.getFilling().getSku(), filling.getSku());
+        Assertions.assertEquals(bagel.getFilling().getPrice(), filling.getPrice());
+        Assertions.assertEquals(bagel.getFilling().getName(), filling.getName());
+        Assertions.assertEquals(bagel.getFilling().getVariant(), filling.getVariant());
     }
 }
