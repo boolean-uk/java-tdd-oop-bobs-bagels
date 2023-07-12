a82386e3c5e0d8725aad416dc5b6ad69c1ef8e56
Justyna Dudar <justyna.dudar@talan.com>
2023-07-12 15:43:01 +0200

    inventory class

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -1,4 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+
+    private List<Product> inventoryList;
+
+    public Inventory() {
+        inventoryList = new ArrayList<>();
+        inventoryList.add(new Product("BGLO", 0.49, "Bagel", "Onion"));
+        inventoryList.add(new Product("BGLP", 0.39, "Bagel", "Plain"));
+    }
+
+    public List<Product> getInventoryList() {
+        return inventoryList;
+    }
 }
