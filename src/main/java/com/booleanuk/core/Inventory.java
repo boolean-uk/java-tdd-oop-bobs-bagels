c2e7752c8cbde283ba1fdb21f497d4ae7558c233
Jan Rusak <jrusak001@gmail.com>
2023-07-13 10:49:23 +0200

    Change keys in products map in Inventory to SKUs

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -4,22 +4,22 @@ import java.util.HashMap;
 
 public class Inventory {
     private static final HashMap<String, Product> products = new HashMap<>() {{
-        put("Onion", new Product("BGLO", 0.49d, "Bagle", "Onion"));
-        put("Plain", new Product("BGLP", 0.39d, "Bagle", "Plain"));
-        put("Everything", new Product("BGLE", 0.49d, "Bagle", "Everything"));
-        put("Sesame", new Product("BGLS", 0.49d, "Bagle", "Sesame"));
+        put("BGLO", new Product("BGLO", 0.49d, "Bagle", "Onion"));
+        put("BGLP", new Product("BGLP", 0.39d, "Bagle", "Plain"));
+        put("BGLE", new Product("BGLE", 0.49d, "Bagle", "Everything"));
+        put("BGLS", new Product("BGLS", 0.49d, "Bagle", "Sesame"));
 
-        put("Black", new Product("COFB", 0.99d, "Coffee", "Black"));
-        put("White", new Product("COFW", 1.19d, "Coffee", "White"));
-        put("Capuccino", new Product("COFC", 1.29d, "Coffee", "Capuccino"));
-        put("Latte", new Product("COFL", 1.29d, "Coffee", "Latte"));
+        put("COFB", new Product("COFB", 0.99d, "Coffee", "Black"));
+        put("COFW", new Product("COFW", 1.19d, "Coffee", "White"));
+        put("COFC", new Product("COFC", 1.29d, "Coffee", "Capuccino"));
+        put("COFL", new Product("COFL", 1.29d, "Coffee", "Latte"));
 
-        put("Bacon", new Product("FILB", 0.12d, "Filling", "Bacon"));
-        put("Egg", new Product("FILE", 0.12d, "Filling", "Egg"));
-        put("Cheese", new Product("FILC", 0.12d, "Filling", "Cheese"));
-        put("Cream Cheese", new Product("FILX", 0.12d, "Filling", "Cream Cheese"));
-        put("Smoked Salmon", new Product("FILS", 0.12d, "Filling", "Smoked Salmon"));
-        put("Ham", new Product("FILH", 0.12d, "Filling", "Ham"));
+        put("FILB", new Product("FILB", 0.12d, "Filling", "Bacon"));
+        put("FILE", new Product("FILE", 0.12d, "Filling", "Egg"));
+        put("FILC", new Product("FILC", 0.12d, "Filling", "Cheese"));
+        put("FILX", new Product("FILX", 0.12d, "Filling", "Cream Cheese"));
+        put("FILS", new Product("FILS", 0.12d, "Filling", "Smoked Salmon"));
+        put("FILH", new Product("FILH", 0.12d, "Filling", "Ham"));
     }};
 
     public static HashMap<String, Product> getProducts() {
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -7,31 +7,31 @@ public class BasketTest {
     @Test
     public void addTest() {
         Basket basket = new Basket(5);
-        Assertions.assertTrue(basket.add("Plain", 1));
-        Assertions.assertFalse(basket.add("Everything", 8));
+        Assertions.assertTrue(basket.add("BGLP", 1));
+        Assertions.assertFalse(basket.add("BGLE", 8));
         Assertions.assertFalse(basket.add("Plastic", 3));
-        Assertions.assertFalse(basket.add("Plain", 0));
-        Assertions.assertTrue(basket.add("Egg", 1));
+        Assertions.assertFalse(basket.add("BGLP", 0));
+        Assertions.assertTrue(basket.add("FILE", 1));
     }
 
     @Test
     public void removeTest() {
         Basket basket = new Basket(10);
-        basket.add("Plain", 6);
-        basket.add("Everything", 2);
-        basket.add("Sesame", 2);
-        Assertions.assertTrue(basket.remove("Plain", 4));
-        Assertions.assertFalse(basket.remove("Everything", 4));
+        basket.add("BGLP", 6);
+        basket.add("BGLE", 2);
+        basket.add("BGLS", 2);
+        Assertions.assertTrue(basket.remove("BGLP", 4));
+        Assertions.assertFalse(basket.remove("BGLE", 4));
         Assertions.assertFalse(basket.remove("Plastic", 2));
-        Assertions.assertFalse(basket.remove("Plain", 0));
+        Assertions.assertFalse(basket.remove("BGLP", 0));
     }
 
     @Test
     public void changeCapacityTest() {
         Basket basket = new Basket(10);
-        basket.add("Plain", 6);
-        basket.add("Everything", 2);
-        basket.add("Sesame", 2);
+        basket.add("BGLP", 6);
+        basket.add("BGLE", 2);
+        basket.add("BGLS", 2);
         Assertions.assertTrue(basket.changeCapacity(15));
         Assertions.assertFalse(basket.changeCapacity(8));
     }
@@ -39,17 +39,17 @@ public class BasketTest {
     @Test
     public void totalCostTest() {
         Basket basket = new Basket(10);
-        basket.add("Plain", 6);
-        basket.add("Everything", 2);
-        basket.add("Sesame", 2);
+        basket.add("BGLP", 6);
+        basket.add("BGLE", 2);
+        basket.add("BGLS", 2);
         Assertions.assertEquals(4.3, basket.totalCost());
     }
 
     @Test
     public void checkCostOfProducts(){
         Basket basket = new Basket(10);
-        Assertions.assertEquals(0.39, basket.checkCostOfProduct("Plain"));
-        Assertions.assertEquals(0.12, basket.checkCostOfProduct("Egg"));
+        Assertions.assertEquals(0.39, basket.checkCostOfProduct("BGLP"));
+        Assertions.assertEquals(0.12, basket.checkCostOfProduct("FILE"));
         Assertions.assertEquals(0.0, basket.checkCostOfProduct("Pizza"));
     }
 
