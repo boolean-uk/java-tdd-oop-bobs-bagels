30aa8f607bd0d486ce192e7fac55daf9b80f00fd
Adam Ciesielski <ciesielski.adam2002@gmail.com>
2023-07-13 12:33:40 +0200

    more receipt tests

--- src/test/java/com/booleanuk/core/ReceiptTest.java
+++ src/test/java/com/booleanuk/core/ReceiptTest.java
@@ -2,6 +2,9 @@ package com.booleanuk.core;
 
 import com.booleanuk.core.basket.Basket;
 import com.booleanuk.core.products.Bagel;
+import com.booleanuk.core.products.BagelVariant;
+import com.booleanuk.core.products.Filling;
+import com.booleanuk.core.products.FillingVariant;
 import com.booleanuk.core.receipt.Receipt;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -25,10 +28,63 @@ public class ReceiptTest {
     {
 
         Basket basket = new Basket();
-        Bagel bagel = new Bagel("SKU1",0.51);
-        basket.addProduct(bagel);
+        Bagel onionBagel = new Bagel("BGLO",0.49);
+        Bagel plainBagel = new Bagel("BGLP",0.39);
+        Bagel everythingBagel = new Bagel("BGLE",0.49);
+        Bagel sesameBagel = new Bagel("BGLS",0.49);
+        onionBagel.setVariant(BagelVariant.Onion);
+        plainBagel.setVariant(BagelVariant.Plain);
+        everythingBagel.setVariant(BagelVariant.Everything);
+        sesameBagel.setVariant(BagelVariant.Sesame);
+        basket.addProduct(onionBagel);
+        basket.addProduct(onionBagel);
+        basket.addProduct(onionBagel);
+        basket.addProduct(onionBagel);
+        basket.addProduct(plainBagel);
+        basket.addProduct(everythingBagel);
+        basket.addProduct(sesameBagel);
+
         Receipt receipt = new Receipt();
 
         Assertions.assertNotNull(receipt.printReceipt(basket));
     }
+
+   @Test
+   public void shouldPrintBagelOnReceiptProperly()
+   {
+       Bagel onionBagel = new Bagel("BGLO",0.49);
+       onionBagel.setVariant(BagelVariant.Onion);
+       Filling filling = new Filling("FILB",0.12);
+       filling.setVariant(FillingVariant.Bacon);
+       onionBagel.addFilling(filling);
+       Receipt receipt = new Receipt();
+       receipt.showBagelOnReceipt(onionBagel,1);
+       Assertions.assertNotNull(receipt.showBagelOnReceipt(onionBagel,1));
+   }
+
+
+   @Test
+    public  void shouldPrintReceiptWithFillings()
+   {
+       Basket basket = new Basket();
+       Bagel onionBagel = new Bagel("BGLO",0.49);
+       Bagel plainBagel = new Bagel("BGLP",0.39);
+       Bagel everythingBagel = new Bagel("BGLE",0.49);
+       Bagel sesameBagel = new Bagel("BGLS",0.49);
+       onionBagel.setVariant(BagelVariant.Onion);
+       plainBagel.setVariant(BagelVariant.Plain);
+       everythingBagel.setVariant(BagelVariant.Everything);
+       sesameBagel.setVariant(BagelVariant.Sesame);
+       Filling filling = new Filling("FILB",0.12);
+       filling.setVariant(FillingVariant.Bacon);
+       onionBagel.addFilling(filling);
+       basket.addProduct(onionBagel);
+       basket.addProduct(plainBagel);
+       basket.addProduct(everythingBagel);
+       basket.addProduct(sesameBagel);
+
+       Receipt receipt = new Receipt();
+       System.out.println(receipt.printReceipt(basket));
+       Assertions.assertNotNull(receipt.showBagelOnReceipt(onionBagel,1));
+   }
 }
