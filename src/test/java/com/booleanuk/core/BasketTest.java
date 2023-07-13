93ad24b88b3a9c7013b18e89282be9f3cdcf0d58
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-13 12:18:16 +0200

    red stage of testGetReceipt_WhenBasketIsNotEmpty_ShouldReturnReceipt

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,10 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BasketTest {
     private static Inventory inventory;
@@ -195,5 +199,37 @@ public class BasketTest {
 
         //Then
         Assertions.assertEquals(result, 2.49F + 3.99F + 0.98F + 1.19F*2);
+    }
+
+    @Test
+    public void testGetReceipt_WhenBasketIsNotEmpty_ShouldReturnReceipt() {
+        //Given
+        StringBuilder expected = new StringBuilder();
+        LocalDateTime ldt = LocalDateTime.now();
+
+        expected.append("    ~~~ Bob's Bagels ~~~    \n\n")
+                .append("    " + ldt.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "   \n\n")
+                .append("-".repeat(28) + "\n\n")
+                .append("Onion Bagel       2   £0.98\n")
+                .append("Plain Bagel       12  £3.99\n")
+                .append("Everything Bagel  6   £2.49\n")
+                .append("Black Coffee      3   £2.97\n\n")
+                .append("-".repeat(28) + "\n\n")
+                .append("Total                £10.43\n\n")
+                .append("        Thank you\n")
+                .append("      for your order! \n");
+
+
+        Basket basket = new Basket(inventory, 23);
+        basket.addProduct("BGLO", 2);
+        basket.addProduct("BGLP", 12);
+        basket.addProduct("BGLE", 6);
+        basket.addProduct("COFB", 3);
+
+        //When
+        String result = basket.getReceipt();
+
+        //Then
+        Assertions.assertEquals(result, expected);
     }
 }
