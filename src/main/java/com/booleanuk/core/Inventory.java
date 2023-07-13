b6ef6528c8a0cf116e0fc63407cd794f170ae09c
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-13 09:56:16 +0200

    inventory updated

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -7,10 +7,23 @@ public class Inventory {
 
     public Inventory() {
         this.products = new HashMap<>();
-    }
+        products.put("BGLO", new Product("BGLO", 0.49F, "Bagel", "Onion"));
+        products.put("BGLP", new Product("BGLP", 0.39F, "Bagel", "Plain"));
+        products.put("BGLE", new Product("BGLE",0.49F, "Bagel", "Everything"));
+        products.put("BGLS", new Product("BGLS",0.49F, "Bagel", "Sesame"));
 
-    public void addProduct(String sku, float price, String name, String variant) {
-        products.put(sku, new Product(sku, price, name, variant));
+        products.put("COFB", new Product("COFB",0.99F, "Coffee", "Black"));
+        products.put("COFW", new Product("COFW", 1.19F, "Coffee", "White"));
+        products.put("COFC", new Product("COFC", 1.29F, "Coffee", "Cappuccino"));
+        products.put("COFL", new Product("COFL", 1.29F, "Coffee", "Latte"));
+
+        products.put("FILB", new Product("FILB", 0.12F, "Filling", "Bacon"));
+        products.put("FILE", new Product("FILE", 0.12F, "Filling", "Egg"));
+        products.put("FILC", new Product("FILC", 0.12F, "Filling", "Cheese"));
+        products.put("FILX", new Product("FILX", 0.12F, "Filling", "Cream Cheese"));
+        products.put("FILS", new Product("FILS", 0.12F, "Filling", "Smoked Salmon"));
+        products.put("FILH", new Product("FILH", 0.12F, "Filling", "Ham"));
+        products.put("FILNOT", new Product("FILNOT", 0.12F, "NotFilling", "Feeling"));
     }
 
     public Product getProduct(String sku) {
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -10,23 +10,6 @@ public class BasketTest {
     @BeforeAll
     static void setup() {
         inventory = new Inventory();
-        inventory.addProduct("BGLO", 0.49F, "Bagel", "Onion");
-        inventory.addProduct("BGLP", 0.39F, "Bagel", "Plain");
-        inventory.addProduct("BGLE", 0.49F, "Bagel", "Everything");
-        inventory.addProduct("BGLS", 0.49F, "Bagel", "Sesame");
-
-        inventory.addProduct("COFB", 0.99F, "Coffee", "Black");
-        inventory.addProduct("COFW", 1.19F, "Coffee", "White");
-        inventory.addProduct("COFC", 1.29F, "Coffee", "Cappuccino");
-        inventory.addProduct("COFL", 1.29F, "Coffee", "Latte");
-
-        inventory.addProduct("FILB", 0.12F, "Filling", "Bacon");
-        inventory.addProduct("FILE", 0.12F, "Filling", "Egg");
-        inventory.addProduct("FILC", 0.12F, "Filling", "Cheese");
-        inventory.addProduct("FILX", 0.12F, "Filling", "Cream Cheese");
-        inventory.addProduct("FILS", 0.12F, "Filling", "Smoked Salmon");
-        inventory.addProduct("FILH", 0.12F, "Filling", "Ham");
-        inventory.addProduct("FILNOT", 0.12F, "NotFilling", "Feeling");
     }
 
     @Test
