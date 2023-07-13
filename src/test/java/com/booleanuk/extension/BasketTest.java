13d771a7a4e4167c45635da67c498cf9ffb7245a
Michał Klukowski <michal.klukowski@talan.com>
2023-07-13 11:57:47 +0200

    Create test for calculating discounts

--- src/test/java/com/booleanuk/extension/BasketTest.java
+++ src/test/java/com/booleanuk/extension/BasketTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 public class BasketTest {
 
@@ -67,6 +68,39 @@ public class BasketTest {
 
     @Test
     void ShouldReturnDiscountsForProducts() {
-        basket.add(bagel);
+        basket.setCapacity(1000);
+
+        Bagel BGLO = new Bagel("BGLO", BigDecimal.valueOf(0.49), "Onion");  // 6 for 2.49
+        Bagel BGLP = new Bagel("BGLP", BigDecimal.valueOf(0.39), "Plain");  // 12 for 3.99
+        Bagel BGLE = new Bagel("BGLE", BigDecimal.valueOf(0.49), "Everything"); // 6 for 2.49
+        Coffee COFB = new Coffee("COFB", BigDecimal.valueOf(0.99), "Black");    // Coffee & Bagel for 1.25
+
+        for(int i = 0; i < 5; i++)
+            basket.add(BGLO);
+        Assertions.assertEquals(Map.of(BGLO, BigDecimal.valueOf(0)), basket.calculateDiscounts());
+
+        basket.add(BGLO);
+        Assertions.assertEquals(Map.of(BGLO, BigDecimal.valueOf(0.45)), basket.calculateDiscounts());
+
+        for(int i = 0; i < 12; i++)
+            basket.add(BGLP);
+        Assertions.assertEquals(Map.of(
+                BGLO, BigDecimal.valueOf(0.45),
+                BGLP, BigDecimal.valueOf(0.69)
+        ), basket.calculateDiscounts());
+
+        for(int i = 0; i < 12; i++)
+            basket.add(BGLP);
+        Assertions.assertEquals(Map.of(
+                BGLO, BigDecimal.valueOf(0.45),
+                BGLP, BigDecimal.valueOf(1.38)
+        ), basket.calculateDiscounts());
+
+        basket.add(COFB);
+        Assertions.assertEquals(Map.of(
+                BGLO, BigDecimal.valueOf(0.45),
+                BGLP, BigDecimal.valueOf(1.38),
+                COFB, BigDecimal.valueOf(0.25)
+        ), basket.calculateDiscounts());
     }
 }
