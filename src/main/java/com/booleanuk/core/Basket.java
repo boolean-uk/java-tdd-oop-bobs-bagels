848ff87962905efc2acadb2e77520f1d1d9e6839
Marlena Luczak <dziubichmarlena@gmail.com>
2023-07-13 10:16:23 +0200

    all methods for core part

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -5,22 +5,60 @@ import java.util.List;
 
 public class Basket {
 
-    List<Bagel> bagles;
-    Bagel bagel;
-    int capacity;
-    public Basket(int capacity){
-        bagles = new ArrayList<>();
+    private List<Item> items;
+    private int capacity;
+    private Inventory inventory;
+
+    public Basket(int capacity) {
+        items = new ArrayList<>();
         this.capacity = capacity;
+        inventory = new Inventory();
     }
 
-    public boolean add(Bagel bagel) {
-        if(bagles.size() < capacity){
-            bagles.add(bagel);
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean add(Item item) {
+        if (items.size() < capacity) {
+            items.add(item);
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
+
+    public boolean removeItem(Item item) {
+        if (items.contains(item)) {
+            items.remove(item);
+            return true;
+        }
+        return false;
+    }
+
+    public int getItemsCount() {
+        return items.size();
+    }
+
+    public boolean isFull() {
+        return items.size() >= capacity;
+    }
+
+    public void changeCapacity(int newCapacity){
+        if(capacity > newCapacity)
+            throw new RuntimeException("New capacity cannot be smaller than the old one");
+        capacity = newCapacity;
+
+    }
+    public double totalCost() {
+        return items.stream()
+                .map(Item::getPrice)
+                .reduce(Double::sum)
+                .orElse(0.0);
+    }
+
 
 }
