4b346c9b07fe9f6cb311437b7db92ee7b189b614
Wojciech Żółkowski <wojciechzolkowski@gmail.com>
2023-07-13 15:16:31 +0200

    fixed BasketTest.java

--- src/main/java/com/booleanuk/extension/Basket.java
+++ src/main/java/com/booleanuk/extension/Basket.java
@@ -1,8 +1,6 @@
 package com.booleanuk.extension;
 
-import com.booleanuk.extension.types.FillingType;
-
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,7 +12,7 @@ public class Basket {
     public final static int SINGLE_FILLING_PRICE = 12;
     public final static int COFFEE_PLUS_BAGEL = 125;
 
-    private Map<Item,Integer> items;
+    private Map<Item, Integer> items;
 
     public Basket(int capacity) {
         this.capacity = capacity;
@@ -50,15 +48,15 @@ public class Basket {
         items.clear();
     }
 
-    public Map<Item,Integer> getItems() {
+    public Map<Item, Integer> getItems() {
         return items;
     }
 
     public void add(Item item) {
         if (isFull()) throw new IllegalStateException("You can't add this item");
-        else{
-            int quantity = items.getOrDefault(item,0);
-            items.put(item,quantity + 1);
+        else {
+            int quantity = items.getOrDefault(item, 0);
+            items.put(item, quantity + 1);
         }
     }
 
@@ -72,16 +70,46 @@ public class Basket {
                 .map(entry -> entry.getValue() * ((Bagel) entry.getKey()).getFillingType().getPrice())
                 .reduce(0L, Long::sum);
 
-        long priceOfItems = items.entrySet().stream().map(entry -> {
-            if(entry.getKey() instanceof Bagel && entry.getValue() >= 6 && entry.getValue() < 12){
-                return SIX_BAGELS_PRICE + (entry.getValue()%6)*entry.getKey().getPrice();
-            }
-            else if(entry.getKey() instanceof Bagel && entry.getValue() >= 12){
-                return TWELVE_BAGELS_PRICE + (entry.getValue()%12)*entry.getKey().getPrice();
-            }
-            else return entry.getKey().getPrice() * entry.getValue();
-        }).reduce(0L, Long::sum);
+        long priceOfSpecialOfferBagels = items.entrySet().stream()
+                .filter(entry -> entry.getKey() instanceof Bagel)
+                .map(entry -> {
+                    if (entry.getValue() >= 6) {
+                        int result = entry.getValue() / 6;
+                        return (long) TWELVE_BAGELS_PRICE * (result / 2) + SIX_BAGELS_PRICE * (result % 2);
+                    } else return 0L;
+                }).reduce(0L, Long::sum);
 
-        return (priceOfFillings + priceOfItems) / 100.0;
+        List<Long> coffeePrices = items.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey() instanceof Coffee)
+                .flatMap(entry -> Collections.nCopies(entry.getValue(), entry.getKey().getPrice()).stream())
+                .sorted()
+                .toList();
+
+        List<Long> remainingBagelsPrices = items.entrySet().stream()
+                .filter(entry -> entry.getKey() instanceof Bagel)
+                .filter(entry -> entry.getValue() % 6 != 0)
+                .flatMap(entry -> Collections.nCopies(entry.getValue() % 6, ((Bagel) entry.getKey()).getBagelType().getPrice()).stream())
+                .sorted()
+                .toList();
+
+        int numberOfCoffeeAndBagelSpecialOffers = Math.min(coffeePrices.size(), remainingBagelsPrices.size());
+
+        long coffeeAndBagelPrice = (long) numberOfCoffeeAndBagelSpecialOffers * COFFEE_PLUS_BAGEL;
+
+        long remainingItemsPrice = 0L;
+        if (coffeePrices.size() > remainingBagelsPrices.size()) {
+            remainingItemsPrice = coffeePrices.subList(numberOfCoffeeAndBagelSpecialOffers, coffeePrices.size())
+                    .stream()
+                    .mapToLong(Long::longValue)
+                    .sum();
+        } else if (coffeePrices.size() < remainingBagelsPrices.size()) {
+            remainingItemsPrice = remainingBagelsPrices.subList(numberOfCoffeeAndBagelSpecialOffers, remainingBagelsPrices.size())
+                    .stream()
+                    .mapToLong(Long::longValue)
+                    .sum();
+        }
+
+        return (priceOfFillings + priceOfSpecialOfferBagels + coffeeAndBagelPrice + remainingItemsPrice) / 100.0;
     }
 }
--- src/test/java/com/booleanuk/extension/BasketTest.java
+++ src/test/java/com/booleanuk/extension/BasketTest.java
@@ -6,30 +6,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static com.booleanuk.extension.Basket.*;
-import static com.booleanuk.extension.types.BagelType.*;
-import static com.booleanuk.extension.types.CoffeeType.*;
-import static com.booleanuk.extension.types.FillingType.*;
+import static com.booleanuk.extension.types.BagelType.PLAIN;
+import static com.booleanuk.extension.types.BagelType.SESAME;
+import static com.booleanuk.extension.types.CoffeeType.BLACK;
+import static com.booleanuk.extension.types.CoffeeType.WHITE;
+import static com.booleanuk.extension.types.FillingType.BACON;
+import static com.booleanuk.extension.types.FillingType.WITHOUT_FILLING;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
 
     private Basket basket;
     private Bagel bagel;
+    private Bagel bagelWithoutFilling;
     private Coffee coffee;
-    private Map<Item,Integer> expectedItems;
+    private Coffee whiteCoffee;
+    private Map<Item, Integer> expectedItems;
 
     @BeforeEach
     public void setup() {
-        basket = new Basket(15);
-        bagel = new Bagel(PLAIN,BACON);
+        basket = new Basket(20);
+        bagel = new Bagel(SESAME, BACON);
+        bagelWithoutFilling = new Bagel(PLAIN, WITHOUT_FILLING);
         coffee = new Coffee(BLACK);
+        whiteCoffee = new Coffee(WHITE);
         expectedItems = new HashMap<>();
-        expectedItems.put(bagel,1);
-        expectedItems.put(coffee,2);
+        expectedItems.put(bagel, 1);
+        expectedItems.put(coffee, 2);
 
     }
 
@@ -41,13 +47,14 @@ class BasketTest {
         basket.add(coffee);
         basket.add(coffee);
 
-        assertEquals(3 ,basket.getItemCount());
+        assertEquals(3, basket.getItemCount());
 
         assertEquals(basket.getItems(), expectedItems);
     }
 
     @Test
     public void addThrowsExceptionIfBasketIsFull() {
+        basket.changeCapacity(5);
         basket.add(bagel);
         basket.add(bagel);
         basket.add(bagel);
@@ -59,15 +66,15 @@ class BasketTest {
 
     @Test
     public void removeRemovesItems() {
-        Map<Item,Integer> expected = new HashMap<>();
-        expected.put(coffee,1);
+        Map<Item, Integer> expected = new HashMap<>();
+        expected.put(coffee, 1);
 
         basket.add(bagel);
         basket.add(bagel);
         basket.add(coffee);
         basket.remove(bagel);
 
-        assertEquals(1 ,basket.getItems().size());
+        assertEquals(1, basket.getItems().size());
         assertEquals(basket.getItems(), expected);
     }
 
@@ -82,6 +89,7 @@ class BasketTest {
     @Test
     public void isFullReturnsTrueIfBasketIsFull() {
         assertFalse(basket.isFull());
+        basket.changeCapacity(5);
         basket.add(bagel);
         basket.add(bagel);
         basket.add(coffee);
@@ -104,7 +112,7 @@ class BasketTest {
 
     @Test
     public void changeCapacityChangesCapacity() {
-        assertEquals(5, basket.getCapacity());
+        assertEquals(20, basket.getCapacity());
 
         basket.changeCapacity(7);
 
@@ -119,7 +127,7 @@ class BasketTest {
     public void removeItemRemovesItemProperly() {
         basket.add(bagel);
         basket.add(coffee);
-        assertEquals(2 ,basket.getItemCount());
+        assertEquals(2, basket.getItemCount());
         assertTrue(basket.getItems().keySet().contains(bagel));
 
         basket.remove(coffee);
@@ -131,44 +139,141 @@ class BasketTest {
 
     @Test
     public void getTotalCostReturnsTotalCost() {
+        basket.add(bagelWithoutFilling);
+        basket.add(bagel);
+
+        assertEquals((bagelWithoutFilling.getPrice() + bagel.getPrice()) / 100.0, basket.getTotalCost());
+    }
+
+    @Test
+    public void getTotalCostWithCoffeeAndBagelDiscountReturnsTotalCost() {
+        basket.add(bagelWithoutFilling);
         basket.add(bagel);
         basket.add(coffee);
-        basket.add(bagel);
-        basket.add(bagel);
-        basket.add(bagel);
-        basket.add(bagel);
+        assertEquals((COFFEE_PLUS_BAGEL + bagel.getPrice()) / 100.0, basket.getTotalCost());
+    }
+
+//    @Test
+//    public void getTotalCostReturnsTotalCos() {
+//        basket.add(bagelWithoutFilling);
+//        basket.add(bagelWithoutFilling);
+//        basket.add(bagelWithoutFilling);
+//        basket.add(bagelWithoutFilling);
+//        basket.add(bagelWithoutFilling);
+//        basket.add(bagelWithoutFilling);
+//        assertEquals((SIX_BAGELS_PRICE) / 100.0, basket.getTotalCost());
+//
+//        basket.add(coffee);
+//        assertEquals((SIX_BAGELS_PRICE + coffee.getPrice()) / 100.0, basket.getTotalCost());
+//
+//        basket.add(bagel);
+//        basket.add(bagel);
+//        basket.add(bagel);
+//        basket.add(bagel);
+//        basket.add(bagel);
+//        basket.add(bagel);
+//        assertEquals((2 * SIX_BAGELS_PRICE + 6 * SINGLE_FILLING_PRICE + coffee.getPrice()) / 100.0, basket.getTotalCost());
+//
+//        basket.add(bagel);
+//        assertEquals((2 * SIX_BAGELS_PRICE + 7 * SINGLE_FILLING_PRICE + COFFEE_PLUS_BAGEL) / 100.0, basket.getTotalCost());
+//
+//        basket.add(coffee);
+//        basket.add(bagel);
+//        basket.add(bagel);
+//        assertEquals((2 * SIX_BAGELS_PRICE + 9 * SINGLE_FILLING_PRICE + 2 * COFFEE_PLUS_BAGEL + bagel.getPrice()) / 100.0, basket.getTotalCost());
+//
+//    }
+
+    @Test
+    public void getTotalCostReturnsTotalCostFor6BagelsSpecialOffer() {
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+
+        assertEquals(SIX_BAGELS_PRICE / 100.0, basket.getTotalCost());
+    }
+
+    @Test
+    public void getTotalCostReturnsTotalCostFor6BagelsSpecialOfferPlusOneBagel() {
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
         basket.add(bagel);
 
-        assertEquals((SIX_BAGELS_PRICE + 6* SINGLE_FILLING_PRICE + coffee.getPrice()) / 100.0, basket.getTotalCost());
-        basket.add(bagel);
-        basket.add(bagel);
-        basket.add(bagel);
-        basket.add(bagel);
-        basket.add(bagel);
-        basket.add(bagel);
-        assertEquals((TWELVE_BAGELS_PRICE + 12 * SINGLE_FILLING_PRICE + coffee.getPrice()) / 100.0,basket.getTotalCost());
-        basket.add(bagel);
-        assertEquals((TWELVE_BAGELS_PRICE + 12 * SINGLE_FILLING_PRICE + COFFEE_PLUS_BAGEL) / 100.0,basket.getTotalCost());
+        assertEquals((SIX_BAGELS_PRICE + bagel.getPrice()) / 100.0, basket.getTotalCost());
+    }
 
+    @Test
+    public void getTotalCostReturnsTotalCostFor6BagelsSpecialOfferPlusOneCoffeeOffer() {
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagel);
+        basket.add(coffee);
+
+        assertEquals((SIX_BAGELS_PRICE + COFFEE_PLUS_BAGEL + SINGLE_FILLING_PRICE) / 100.0, basket.getTotalCost());
+    }
+
+    @Test
+    public void getTotalCostReturnsTotalCostFor6BagelsSpecialOfferPlusOneCoffeeOfferAndOneBagel() {
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagelWithoutFilling);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(coffee);
+
+        assertEquals((SIX_BAGELS_PRICE + COFFEE_PLUS_BAGEL + SINGLE_FILLING_PRICE + bagel.getPrice()) / 100.0, basket.getTotalCost());
+    }
+
+    @Test
+    public void getTotalCostReturnsTotalCostFor12BagelsSpecialOfferPlusOneCoffeeOffer() {
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(bagel);
+        basket.add(coffee);
+
+        assertEquals((TWELVE_BAGELS_PRICE + 12 * SINGLE_FILLING_PRICE + coffee.getPrice()) / 100.0, basket.getTotalCost());
     }
 
     @Test
     public void getItemCountReturnsProperValue() {
         basket.add(bagel);
         basket.add(coffee);
-        assertEquals(2 ,basket.getItemCount());
+        assertEquals(2, basket.getItemCount());
         assertTrue(basket.getItems().keySet().contains(bagel));
 
         basket.remove(bagel);
 
-        assertEquals(1 ,basket.getItemCount());
+        assertEquals(1, basket.getItemCount());
 
         basket.add(coffee);
 
-        assertEquals(2 ,basket.getItemCount());
+        assertEquals(2, basket.getItemCount());
 
         basket.remove(coffee);
-        assertEquals(0 ,basket.getItemCount());
+        assertEquals(0, basket.getItemCount());
     }
 
     @Test
@@ -181,11 +286,11 @@ class BasketTest {
     @Test
     public void freeSpaceReturnsProperValue() {
         basket.add(bagel);
-        assertEquals(4, basket.freeSpace());
+        assertEquals(19, basket.freeSpace());
 
         basket.remove(bagel);
 
-        assertEquals(5, basket.freeSpace());
+        assertEquals(20, basket.freeSpace());
     }
 
     @Test
