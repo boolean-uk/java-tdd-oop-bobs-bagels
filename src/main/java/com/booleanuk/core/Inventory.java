dd68a5d781498b9cbdc909366457112f611c3af9
Jan Rusak <jrusak001@gmail.com>
2023-07-13 10:15:05 +0200

    Add products to Inventory

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -3,14 +3,22 @@ package com.booleanuk.core;
 import java.util.HashMap;
 
 public class Inventory {
-    private HashMap<String, Product> products;
+    private static final HashMap<String, Product> products = new HashMap<>() {{
+        put("Onion", new Product("BGLO", 0.49d, "Bagle", "Onion"));
+        put("Plain", new Product("BGLP", 0.39d, "Bagle", "Plain"));
+        put("Everything", new Product("BGLE", 0.49d, "Bagle", "Everything"));
+        put("Sesame", new Product("BGLS", 0.49d, "Bagle", "Sesame"));
 
-    public Inventory(){
-        products = new HashMap<String, Product>();
-        populateInventory();
-    }
+        put("Black", new Product("COFB", 0.99d, "Coffee", "Black"));
+        put("White", new Product("COFW", 1.19d, "Coffee", "White"));
+        put("Capuccino", new Product("COFC", 1.29d, "Coffee", "Capuccino"));
+        put("Latte", new Product("COFL", 1.29d, "Coffee", "Latte"));
 
-    private void populateInventory() {
-
-    }
+        put("Bacon", new Product("FILB", 0.12d, "Filling", "Bacon"));
+        put("Egg", new Product("FILE", 0.12d, "Filling", "Egg"));
+        put("Cheese", new Product("FILC", 0.12d, "Filling", "Cheese"));
+        put("Cream Cheese", new Product("FILX", 0.12d, "Filling", "Cream Cheese"));
+        put("Smoked Salmon", new Product("FILS", 0.12d, "Filling", "Smoked Salmon"));
+        put("Ham", new Product("FILH", 0.12d, "Filling", "Ham"));
+    }};
 }
