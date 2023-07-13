c925af6ab3d4c37ecb32b5f025b8adcd250dbe03
Ilias Belghoul <ilias.belghoul@talan.com>
2023-07-13 11:30:49 +0200

    MainMenu extension

--- src/main/java/com/booleanuk/core/Main.java
+++ src/main/java/com/booleanuk/core/Main.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class Main {
     private static ProductManager productManager = new ProductManager();
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         boolean running = true;
 
         while (running) {
             System.out.println("===== MENU =====");
             System.out.println("A - Order a bagel");
-            System.out.println("B - Remove bagel from the basket");
-            System.out.println("C - Change basket capacity");
+            System.out.println("B - Order a coffee");
+            System.out.println("C - Remove product from the basket");
+            System.out.println("D - Change basket capacity");
+            System.out.println("E - Show basket");
             System.out.println("Q - Quit");
             System.out.print("Enter your choice: ");
 
@@ -51,15 +54,35 @@ public class Main {
         System.out.println("Q - Exit");
         String choice = scanner.nextLine().trim().toUpperCase();
         switch (choice) {
-            case "A":
-                System.out.println("Type the variant:");
-                choice = scanner.nextLine().trim();
-                // Dodac obsluge bledow
-                productManager.orderProduct(choice);
-                break;
-            case "Q":
-                System.out.println("Returning to menu...");
-                break;
+            case "A" -> {
+                boolean isAdd;
+                do {
+
+                    System.out.println("Type the variant:");
+                    choice = scanner.nextLine().trim();
+                    // Dodac obsluge bledow
+                    isAdd = productManager.orderProduct(choice);
+                } while (!isAdd);
+                System.out.println("Do you want to add filling?");
+                System.out.println("Y/N");
+                choice = scanner.nextLine().trim().toUpperCase();
+                if ("Y".equals(choice)) {
+                    for (String sku : productManager.getInventory().keySet()) {
+                        if (sku.startsWith("F")) {
+                            System.out.println(productManager.getInventory().get(sku));
+                        }
+                    }
+                    do {
+                        System.out.println("Choose filling variant:");
+                        choice = scanner.nextLine().trim();
+                        productManager.orderProduct(choice);
+                        System.out.println("Want to add next filling?");
+                        System.out.println("Y/N");
+                        choice = scanner.nextLine().trim().toUpperCase();
+                    } while ("Y".equals(choice));
+                }
+            }
+            case "Q" -> System.out.println("Returning to menu...");
         }
     }
 }
