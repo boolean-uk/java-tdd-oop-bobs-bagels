ae70fe19090ac156ff41c05d029082c0ae7535f6
Michał Klukowski <michal.klukowski@talan.com>
2023-07-13 15:10:10 +0200

    Create test for receipt with discounts

--- src/test/java/com/booleanuk/extension/ReceiptTest.java
+++ src/test/java/com/booleanuk/extension/ReceiptTest.java
@@ -17,11 +17,9 @@ public class ReceiptTest {
         Filling FILE = new Filling("FILE", BigDecimal.valueOf(3.00), "Egg");
         BGLO.setFillings(List.of(FILE, FILE));
         Bagel BGLP = new Bagel("BGLP", BigDecimal.valueOf(0.39), "Plain");
-        Coffee COFB = new Coffee("COFB", BigDecimal.valueOf(0.99), "Black");
         basket.add(BGLO);
         basket.add(BGLP);
         basket.add(BGLP);
-        basket.add(COFB);
         Receipt receipt = new Receipt(basket);
         String actual = receipt.printReceipt();
         LocalDateTime time = LocalDateTime.now();
@@ -33,9 +31,39 @@ public class ReceiptTest {
                 "Onion Bagel        1 6.49\n" +
                 "Egg Filling        2 (6.0)\n" +
                 "Plain Bagel        2 0.78\n" +
-                "Black Coffee        1 0.99\n" +
                 "-------------------------------\n" +
-                "Total                     8.26\n" +
+                "Total                     7.27\n" +
+                "Thank you for your order!";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnReceiptStringWithDiscounts() {
+        Basket basket = new Basket(100);
+        Bagel BGLO = new Bagel("BGLO", BigDecimal.valueOf(0.49), "Onion");
+        Bagel BGLP = new Bagel("BGLP", BigDecimal.valueOf(0.39), "Plain");
+        Coffee COFB = new Coffee("COFB", BigDecimal.valueOf(0.99), "Black");
+
+        for(int i = 0; i < 6; i++)
+            basket.add(BGLO);
+
+        basket.add(COFB);
+
+        Receipt receipt = new Receipt(basket);
+        String actual = receipt.printReceipt();
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedTime = time.format(formatter);
+        String expected = "   ~~~ Bob's Bagels ~~~   \n" +
+                formattedTime+"\n" +
+                "-------------------------------\n" +
+                "Onion Bagel        6 2.94\n" +
+                "                    (-0.45)\n" +
+                "Black Coffee        1 0.99\n" +
+                "                    (-0.25)\n" +
+                "-------------------------------\n" +
+                "Total                     3.23\n" +
                 "Thank you for your order!";
 
         Assertions.assertEquals(expected, actual);
