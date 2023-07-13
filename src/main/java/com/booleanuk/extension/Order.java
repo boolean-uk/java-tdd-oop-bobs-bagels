7588f33b2f13606062870805b7946d1fe75199fd
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-13 12:14:25 +0200

    Add implementation: getting total price of placed order

--- src/main/java/com/booleanuk/extension/Order.java
+++ src/main/java/com/booleanuk/extension/Order.java
@@ -1,11 +1,25 @@
 package com.booleanuk.extension;
 
-import java.util.Map;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Order {
+    private UUID id;
+    private LocalDateTime orderDate;
     private final Basket basket;
 
     public Order(Basket basket) {
+        id = UUID.randomUUID();
+        orderDate = LocalDateTime.now();
         this.basket = basket;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return basket.getTotalPrice();
     }
 }
