938549900b9919f63e85d610f426a603d3c9adc8
Jan Rusak <jrusak001@gmail.com>
2023-07-16 12:00:36 +0200

    Update Product in Extension

--- src/main/java/com/booleanuk/extension/Product.java
+++ src/main/java/com/booleanuk/extension/Product.java
@@ -1,31 +1,4 @@
 package com.booleanuk.extension;
 
-public class Product {
-    private String sku;
-    private int price;
-    private String name;
-    private String variant;
-
-    public Product(String sku, int price, String name, String variant) {
-        this.sku = sku;
-        this.price = price;
-        this.name = name;
-        this.variant = variant;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getVariant() {
-        return variant;
-    }
+public record Product(String sku, int price, String name, String variant) {
 }
