8a51f1cc6ca52e492490faf8c34e618611d54f23
Jan Rusak <jrusak001@gmail.com>
2023-07-16 12:01:25 +0200

    Update Inventory in Extension

--- src/main/java/com/booleanuk/extension/Inventory.java
+++ src/main/java/com/booleanuk/extension/Inventory.java
@@ -1,14 +1,15 @@
 package com.booleanuk.extension;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Inventory {
     private static final HashMap<String, Product> products = new HashMap<>() {{
-        put("BGLO", new Product("BGLO", 49, "Bagle", "Onion"));
-        put("BGLP", new Product("BGLP", 39, "Bagle", "Plain"));
-        put("BGLE", new Product("BGLE", 49, "Bagle", "Everything"));
-        put("BGLS", new Product("BGLS", 49, "Bagle", "Sesame"));
+        put("BGLO", new Product("BGLO", 49, "Bagel", "Onion"));
+        put("BGLP", new Product("BGLP", 39, "Bagel", "Plain"));
+        put("BGLE", new Product("BGLE", 49, "Bagel", "Everything"));
+        put("BGLS", new Product("BGLS", 49, "Bagel", "Sesame"));
 
         put("COFB", new Product("COFB", 99, "Coffee", "Black"));
         put("COFW", new Product("COFW", 119, "Coffee", "White"));
@@ -23,57 +24,60 @@ public class Inventory {
         put("FILH", new Product("FILH", 12, "Filling", "Ham"));
     }};
 
-    private static final HashMap<Integer, Integer> discounts = new HashMap<>()
-    {{
-        put(6,249);
-        put(12,399);
+    private static final List<CombinationBargain> combinationBargains = CombinationBargain.coffeePlusBagel();
+
+    private static final HashMap<String, List<Bargain>> bargains = new HashMap<>() {{
+        put("BGLO", new ArrayList<>() {{
+            add(Bargain.bargain6Bagels());
+            add(Bargain.bargain12Bagels());
+        }});
+        put("BGLP", new ArrayList<>() {{
+            add(Bargain.bargain6Bagels());
+            add(Bargain.bargain12Bagels());
+        }});
+        put("BGLE", new ArrayList<>() {{
+            add(Bargain.bargain6Bagels());
+            add(Bargain.bargain12Bagels());
+        }});
+        put("BGLS", new ArrayList<>() {{
+            add(Bargain.bargain6Bagels());
+            add(Bargain.bargain12Bagels());
+        }});
     }};
+
     public static HashMap<String, Product> getProducts() {
         return products;
     }
 
-    public static int getDiscount(String productSku, HashMap<String, Integer> basketProducts){
-        if(!productSku.startsWith("BGL"))
+    public static List<Bargain> getBargains(String productSKU) {
+        return bargains.getOrDefault(productSKU, List.of());
+    }
+
+    public static List<CombinationBargain> getCombinationBargains() {
+        return combinationBargains;
+    }
+
+    public static boolean productNotInInventory(String productSKU) {
+        return !products.containsKey(productSKU);
+    }
+
+    public static int checkCostOfTheProduct(String productSKU){
+        if (productNotInInventory(productSKU))
             return 0;
 
-        int twelvePacks = basketProducts.get(productSku) / 12;
-        int discount = discountCounter(12, twelvePacks, productSku, basketProducts);
-
-        int sixPacks = basketProducts.get(productSku) / 6;
-        discount += discountCounter(6, sixPacks, productSku, basketProducts);
-
-        return discount;
+        return products.get(productSKU)
+                .price();
     }
 
-    public static int getCoffeeDiscount(String coffeeSKU, HashMap<String, Integer> basketProducts){
-        int discount = 0;
-
-        List<String> bagels = basketProducts.keySet()
-                .stream()
-                .filter(s -> s.startsWith("BGL"))
-                .toList();
-
-        for (String productSKU: bagels) {
-            while (basketProducts.get(coffeeSKU) > 0 && basketProducts.get(productSKU) > 0) {
-                basketProducts.put(productSKU, basketProducts.get(productSKU) - 1);
-                basketProducts.put(coffeeSKU, basketProducts.get(coffeeSKU) - 1);
-
-                discount += products.get(productSKU).getPrice() + products.get(coffeeSKU).getPrice()
-                        - 125;
-            }
-
-            if (basketProducts.get(coffeeSKU) == 0)
-                return discount;
-        }
-
-        return discount;
+    public static Product getProduct(String productSKU) {
+        return products.getOrDefault(productSKU, null);
     }
 
-    private static int discountCounter(int packSize, int packCounter,String productSku, HashMap<String, Integer> basketProducts){
-        int quantity = packCounter * packSize;
-        basketProducts.put(productSku, basketProducts.get(productSku) - quantity);
-        return quantity * products.get(productSku).getPrice()
-                - packCounter * discounts.get(packSize);
-    }
+    public static String getProductDescription(String productSKU) {
+        if (productNotInInventory(productSKU))
+            return productSKU;
 
+        Product product = getProduct(productSKU);
+        return product.variant() + " " + product.name();
+    }
 }
