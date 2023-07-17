82293a31ecf133f561a4af4ed4b8fc57c4aa8b3d
Anna Sperkach <annsperkach@gmail.com>
2023-07-17 13:14:37 +0300

    Finish all core requirements

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,4 +1,5 @@
 package com.booleanuk.core;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -44,6 +45,20 @@ public class BasketTest {
         products.add(bagel2);
         double expectedCost = bagel1.getPrice() + bagel2.getPrice();
         assertEquals(expectedCost, basket.getTotalCost(products), 0.001);
+    }
+
+    @Test
+    public void changeBasketCapacity() {
+        Basket basket = new Basket(3);
+        Bagel bagel1 = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        Bagel bagel2 = new Bagel("BGLP", 0.39, "Bagel", "Plain");
+        basket.addProduct(bagel1);
+        basket.addProduct(bagel2);
+        ArrayList<Bagel> products = new ArrayList<>();
+        products.add(bagel1);
+        products.add(bagel2);
+        basket.changeCapacity(5);
+        Assertions.assertFalse(basket.isFull());
     }
 }
 
