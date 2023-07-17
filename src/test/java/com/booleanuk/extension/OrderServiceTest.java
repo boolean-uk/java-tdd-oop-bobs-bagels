c4723b7c1e07d7d72a3bcf653bef61b6aadecbcc
Michał Klukowski <24416088+MicKlu@users.noreply.github.com>
2023-07-17 18:17:06 +0200

    Create test for checking SMS notification text.

---
+++ src/test/java/com/booleanuk/extension/OrderServiceTest.java
@@ -0,0 +1,84 @@
+package com.booleanuk.extension;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class OrderServiceTest {
+
+    static Order order;
+    OrderService orderService;
+
+    @BeforeAll
+    static void beforeAll() {
+        Bagel BGLO = new Bagel("BGLO", BigDecimal.valueOf(0.49), "Onion");
+        Bagel BGLP = new Bagel("BGLP", BigDecimal.valueOf(0.39), "Plain");
+        Bagel BGLE = new Bagel("BGLE", BigDecimal.valueOf(0.49), "Everything");
+        Coffee COFB = new Coffee("COFB", BigDecimal.valueOf(0.99), "Black");
+        Filling FILE = new Filling("FILE", BigDecimal.valueOf(3.00), "Egg");
+
+        Basket basket = new Basket(1000);
+
+        IntStream.range(0, 6).forEach(i -> basket.add(BGLO));
+        basket.add(BGLP);
+        basket.add(BGLE);
+
+        BGLE.setFillings(List.of(FILE));
+
+        basket.add(COFB);
+
+        order = new Order(basket);
+    }
+
+    @BeforeEach
+    void setUp() {
+        orderService = OrderService.getInstance();
+    }
+
+    @Test
+    public void shouldReturnNotificationMessage() {
+        LocalDateTime receiptTime = LocalDateTime.now();
+        LocalDateTime deliveryTime = receiptTime.plusDays(1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String actual = orderService.getNotificationBody(new Receipt(order.getBasket()));
+        String expected = String.format("""
+                Your order is being processed. ETA: %s
+                                
+                Here's your receipt:
+                    ~~~ Bob's Bagels ~~~
+                                
+                    %s
+                                
+                ----------------------------
+                                
+                Onion Bagel         6  £2.94
+                                     (-£0.45)
+                Plain Bagel         1  £0.39
+                Everything Bagel    1  £0.49
+                Egg Filling          1  £3.0
+                Black Coffee        1  £0.99
+                                     (-£0.13)
+                                
+                ----------------------------
+                                
+                Total                  £7.23
+                                
+                 You saved a total of £0.58
+                        on this shop
+                                
+                         Thank you
+                      for your order!""", deliveryTime.format(formatter), receiptTime.format(formatter));
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
