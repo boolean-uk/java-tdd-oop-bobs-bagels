0e83fa08a484b924641c2eb419133586659ad599
himok <himok.pk@gmail.com>
2023-07-17 15:06:02 +0200

    refactoring extension classes

---
+++ src/main/java/com/booleanuk/extension/Coffee.java
@@ -0,0 +1,11 @@
+package com.booleanuk.extension;
+
+public class Coffee extends Product {
+    public Coffee(String sku, float price, String name, String variant) {
+        super(sku, price, name, variant);
+    }
+
+    public Coffee(String sku, float price, String name, String variant, int specialOfferQuantity, float specialOfferPrice) {
+        super(sku, price, name, variant, specialOfferQuantity, specialOfferPrice);
+    }
+}
