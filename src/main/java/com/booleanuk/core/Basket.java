f22c6e41445bfe5bbf085832f784d005093f07cf
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-08-02 08:49:18 +0200

    adding basket and manager tests

---
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -0,0 +1,9 @@
+package com.booleanuk.core;
+
+public class Basket {
+    private static int capacity;
+
+    public static void setCapacity(int basketCapacity) {
+        capacity = basketCapacity;
+    }
+}
---
+++ src/test/java/com/booleanuk/core/ManagerTest.java
@@ -0,0 +1,41 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ManagerTest {
+    private Manager bob;
+    private static final int INITIAL_BASKET_CAPACITY = 15;
+    private final List<Product> initialInventory = List.of(
+           //products
+    );
+
+    public ManagerTest() {
+    }
+
+    @BeforeEach
+    public void setUp(){
+        bob = new Manager(INITIAL_BASKET_CAPACITY,
+                initialInventory);
+    }
+
+    @Test
+    public void testChangeBasketCapacity(){
+        Assertions.assertDoesNotThrow(() -> bob.changeBasketsCapacity(INITIAL_BASKET_CAPACITY));
+        Assertions.assertEquals(INITIAL_BASKET_CAPACITY, bob.getBasketCapacity());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bob.changeBasketsCapacity(5));
+    }
+
+    @Test
+    public void testGetInventory() {
+        // Execute
+        List<Product> inventory = bob.getInventory();
+
+        // Verify
+        Assertions.assertEquals(initialInventory, inventory);
+
+    }
+}
