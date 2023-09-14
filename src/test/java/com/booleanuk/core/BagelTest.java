8d6f09b4ebe58008bfd1f751a952f0c35854a33f
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-14 20:27:28 +0200

    added bagel class tests

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -1,5 +1,18 @@
 package com.booleanuk.core;
 
-public class BagelTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class BagelTest {
+    @Test
+    public void testBagel() {
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        Assertions.assertEquals("BGLO" , bagel.getSku());
+        Assertions.assertEquals(0.49, bagel.getPrice());
+        Assertions.assertEquals("Bagel", bagel.getName());
+        Assertions.assertEquals("Onion", bagel.getVariant());
+        Assertions.assertTrue( bagel.setVariant("Onion-Garlic"));
+        Assertions.assertEquals("Onion-Garlic", bagel.getVariant());
+
+    }
 }
