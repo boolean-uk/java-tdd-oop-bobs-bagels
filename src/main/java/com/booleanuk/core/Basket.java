9327f8454941d1678e345eaa6ade08c9f0598c52
Irene <irene.maniou@hotmail.com>
2023-09-14 18:33:02 +0300

    Irene Maniou

---
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -0,0 +1,156 @@
+package com.booleanuk.core;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Basket {
+
+    private ArrayList<InventoryItem> list;
+    private ArrayList<InventoryItem> basket = new ArrayList<>();
+    private int capacity;
+
+    public Basket() {
+        this.list = new ArrayList<>();
+        this.capacity = 4;
+    }
+
+    public boolean addBagel(String variant, double price) {
+        fillList();
+        Iterator<InventoryItem> iterator = list.iterator();
+
+        if (price != 0 && basket.size() < capacity) {
+            boolean added = true;
+
+            while (iterator.hasNext()) {
+                InventoryItem item = iterator.next();
+                if (item.getVariant().equals(variant) && item.getName().equals("Bagel")) {
+                    added = false;
+                }
+            }
+            if (!added) {
+                System.out.println("this " + variant + "item already exist");
+            } else {
+                String sku = "BGL" + variant.substring(0, 1).toUpperCase();
+                basket.add(new InventoryItem(sku, price, "Bagel", variant));
+                System.out.println("this " + variant + "item added successfully");
+            }
+            return added;
+        }
+        return false;
+    }
+
+    public boolean removeBagel(String variant) {
+        Iterator<InventoryItem> iterator = basket.iterator();
+        boolean removed = false;
+
+        if (!variant.equals("")) {
+            while (iterator.hasNext()) {
+                InventoryItem item = iterator.next();
+                if (item.getVariant().equals(variant) && item.getName().equals("Bagel")) {
+                    iterator.remove();
+                    removed = true;
+                }
+            }
+        }
+
+        if (!removed) {
+            System.out.println("item doesn't exist");
+        } else {
+            System.out.println("item removed successfully");
+        }
+        return removed;
+    }
+
+    public boolean setCapacity(int newCapacity) {
+        if (basket.size() != 0) {
+            return false;
+        } else {
+            if (capacity == newCapacity) {
+                return false;
+            } else {
+                this.capacity = newCapacity;
+                return true;
+            }
+        }
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public double totalCost() {
+        Iterator<InventoryItem> iterator = basket.iterator();
+        double sum = 0;
+
+        while (iterator.hasNext()) {
+            InventoryItem item = iterator.next();
+            sum += item.getPrice();
+        }
+        return sum;
+    }
+
+    public double showBagelCost(String variant) {
+        fillList();
+
+        for (InventoryItem item : list) {
+            if (item.getVariant().equals(variant) && item.getName().equals("Bagel")) {
+                return item.getPrice();
+            }
+        }
+        return 0;
+    }
+
+    public boolean addFilling(String variant, double price) {
+        fillList();
+        Iterator<InventoryItem> iterator = list.iterator();
+
+        if (price != 0 && basket.size() < capacity) {
+            boolean added = true;
+
+            while (iterator.hasNext()) {
+                InventoryItem item = iterator.next();
+                if (item.getVariant().equals(variant) && item.getName().equals("Filling")) {
+                    added = false;
+                }
+            }
+            if (!added) {
+                System.out.println("this " + variant + "item already exist");
+            } else {
+                String sku = "FIL" + variant.substring(0, 1).toUpperCase();
+                basket.add(new InventoryItem(sku, price, "Filling", variant));
+                System.out.println("this " + variant + "item added successfully");
+            }
+            return added;
+        }
+        return false;
+    }
+
+    public double showFillingCost(String variant) {
+        fillList();
+
+        for (InventoryItem item : list) {
+            if (item.getVariant().equals(variant) && item.getName().equals("Filling")) {
+                return item.getPrice();
+            }
+        }
+        return 0;
+    }
+
+
+    public void fillList() {
+        list.add(new InventoryItem("BGLO", 0.49, "Bagel", "Onion"));
+        list.add(new InventoryItem("BGLP", 0.39, "Bagel", "Plain"));
+        list.add(new InventoryItem("BGLE", 0.49, "Bagel", "Everything"));
+        list.add(new InventoryItem("BGLS", 0.49, "Bagel", "Sesame"));
+        list.add(new InventoryItem("COFB", 0.99, "Coffee", "Black"));
+        list.add(new InventoryItem("COFW", 1.19, "Coffee", "White"));
+        list.add(new InventoryItem("COFC", 1.29, "Coffee", "Capuccino"));
+        list.add(new InventoryItem("COFL", 1.29, "Coffee", "Latte"));
+        list.add(new InventoryItem("FILB", 0.12, "Filling", "Bacon"));
+        list.add(new InventoryItem("FILE", 0.12, "Filling", "Egg"));
+        list.add(new InventoryItem("FILC", 0.12, "Filling", "Cheese"));
+        list.add(new InventoryItem("FILX", 0.12, "Filling", "Cream Cheese"));
+        list.add(new InventoryItem("FILS", 0.12, "Filling", "Smoked Salmon"));
+        list.add(new InventoryItem("FILH", 0.12, "Filling", "Ham"));
+    }
+}
---
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -0,0 +1,83 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasketTest {
+
+    @Test
+    public void addBagel() {
+        Basket theBasket = new Basket();
+
+        Assertions.assertFalse(theBasket.addBagel("Onion", 1.6));
+        Assertions.assertFalse(theBasket.addBagel("Sesame", 0));
+        Assertions.assertTrue(theBasket.addBagel("Cranberry", 1.5));
+    }
+
+    @Test
+    public void removeBagel() {
+        Basket theBasket = new Basket();
+
+        theBasket.addBagel("Oat", 1.6);
+        theBasket.addBagel("Multigrain", 1.2);
+        theBasket.addBagel("Egg", 0.8);
+
+        Assertions.assertTrue(theBasket.removeBagel("Oat"));
+        Assertions.assertFalse(theBasket.removeBagel("Blueberry"));
+        Assertions.assertFalse(theBasket.removeBagel(""));
+    }
+
+    @Test
+    public void setCapacity() {
+        Basket theBasket = new Basket();
+
+        Assertions.assertTrue(theBasket.setCapacity(3));
+
+        theBasket.addBagel("Oat", 1.6);
+        theBasket.addBagel("Multigrain", 1.2);
+        theBasket.addBagel("Egg", 0.8);
+
+        Assertions.assertFalse(theBasket.setCapacity(6));
+    }
+
+    @Test
+    public void totalCost() {
+        Basket theBasket = new Basket();
+
+        Assertions.assertEquals(0, theBasket.totalCost(), 0.001);
+
+        theBasket.addBagel("Oat", 1.6);
+        theBasket.addBagel("Multigrain", 1.2);
+        theBasket.addBagel("Egg", 0.8);
+
+        Assertions.assertEquals(3.6, theBasket.totalCost(), 0.001);
+
+        theBasket.addBagel("Blueberry", 1.4);
+        Assertions.assertEquals(5, theBasket.totalCost(), 0.001);
+    }
+
+    @Test
+    public void showBagelCost() {
+        Basket theBasket = new Basket();
+
+        Assertions.assertEquals(0.39, theBasket.showBagelCost("Plain"));
+        Assertions.assertEquals(0, theBasket.showBagelCost("Blueberry"));
+    }
+
+    @Test
+    public void addFilling() {
+        Basket theBasket = new Basket();
+
+        Assertions.assertFalse(theBasket.addFilling("Bacon", 1.4));
+        Assertions.assertFalse(theBasket.addFilling("Egg", 0));
+        Assertions.assertTrue(theBasket.addFilling("Sauce", 0.5));
+    }
+
+    @Test
+    public void showFillingCost() {
+        Basket theBasket = new Basket();
+
+        Assertions.assertEquals(0.12, theBasket.showFillingCost("Bacon"));
+        Assertions.assertEquals(0, theBasket.showFillingCost("Sauce"));
+    }
+}
