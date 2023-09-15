d5ab33d4a1733187da73b3f3f31846137b6e4ef6
iliask796 <iliaskal7@gmail.com>
2023-09-15 17:22:20 +0300

    Basket Class + Tests Succeed

---
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -0,0 +1,59 @@
+package com.booleanuk.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Basket {
+    private final HashMap<Product,Integer> items;
+    private int capacity;
+
+    public Basket(int maxCapacity) {
+        this.items = new HashMap<>();
+        this.capacity = maxCapacity;
+    }
+
+    private int getSize() {
+        int size = 0;
+        for (int item: this.items.values()){
+            size += item;
+        }
+        return size;
+    }
+
+    public boolean addProduct(Product product){
+        if (this.getSize() == this.capacity){
+            return false;
+        }
+        int quantity = this.items.getOrDefault(product,0);
+        this.items.put(product,quantity+1);
+        return true;
+    }
+
+    public boolean removeProduct(Product product){
+        boolean flag = true;
+        int quantity = this.items.getOrDefault(product, 0);
+        switch (quantity){
+            case 0 -> flag = false;
+            case 1 -> this.items.remove(product);
+            default -> this.items.replace(product, quantity-1);
+        }
+        return flag;
+    }
+
+    public boolean setCapacity(int newCapacity) {
+        if (newCapacity > this.capacity){
+            this.capacity = newCapacity;
+            return true;
+        }
+        return false;
+    }
+
+    public double getTotalCost() {
+        double cost = 0.00d;
+        if (this.items.isEmpty()) return cost;
+        for (Map.Entry<Product,Integer> item: this.items.entrySet()) {
+            cost += item.getKey().getCost() * item.getValue();
+        }
+        return cost;
+    }
+}
---
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -0,0 +1,78 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasketTest {
+    @Test
+    public void shouldAddProductsIfSpaceExists() {
+        Basket basket = new Basket(3);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        Assertions.assertTrue(basket.addProduct(bagel));
+        Product coffee = new Product("Black", 0.99, "COFB");
+        Assertions.assertTrue(basket.addProduct(coffee));
+    }
+
+    @Test
+    public void shouldNotAddProductsIfFull() {
+        Basket basket = new Basket(1);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        Assertions.assertTrue(basket.addProduct(bagel));
+        Product coffee = new Product("Black", 0.99, "COFB");
+        Assertions.assertFalse(basket.addProduct(coffee));
+    }
+
+    @Test
+    public void emptyBasketShouldHaveZeroCost() {
+        Basket basket = new Basket(4);
+        Assertions.assertEquals(0,basket.getTotalCost());
+    }
+
+    @Test
+    public void shouldGetTotalCost() {
+        Basket basket = new Basket(3);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        basket.addProduct(bagel);
+        Product coffee = new Product("Black", 0.99, "COFB");
+        basket.addProduct(coffee);
+        Assertions.assertEquals(1.48, basket.getTotalCost());
+    }
+
+    @Test
+    public void capacityCantDecrease() {
+        Basket basket = new Basket(3);
+        Assertions.assertFalse(basket.setCapacity(2));
+    }
+
+    @Test
+    public void capacityShouldIncrease() {
+        Basket basket = new Basket(1);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        Assertions.assertTrue(basket.addProduct(bagel));
+        Product coffee = new Product("Black", 0.99, "COFB");
+        Assertions.assertFalse(basket.addProduct(coffee));
+        basket.setCapacity(2);
+        Assertions.assertTrue(basket.addProduct(coffee));
+    }
+
+    @Test
+    public void shouldRemoveExistingProduct() {
+        Basket basket = new Basket(3);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        basket.addProduct(bagel);
+        Assertions.assertEquals(0.49,basket.getTotalCost());
+        Assertions.assertTrue(basket.removeProduct(bagel));
+        Assertions.assertEquals(0,basket.getTotalCost());
+    }
+
+    @Test
+    public void shouldReturnFalseOnFalseRemove() {
+        Basket basket = new Basket(3);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        Product coffee = new Product("Black", 0.99, "COFB");
+        basket.addProduct(bagel);
+        Assertions.assertEquals(0.49,basket.getTotalCost());
+        Assertions.assertFalse(basket.removeProduct(coffee));
+        Assertions.assertEquals(0.49,basket.getTotalCost());
+    }
+}
