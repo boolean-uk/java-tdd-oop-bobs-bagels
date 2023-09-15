2a095af188735165532b61a440e54a2a3f1d79eb
Esther <estherbuijnsters@gmail.com>
2023-09-15 15:55:09 +0200

    removeFromBasketFailed Test created

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -43,4 +43,17 @@ public class BasketTest {
         assertTrue(basket.removeFromBasket(itemBGLO));
         assertEquals(0, basket.getItemBasket().size());
     }
+
+    @Test
+    public void testRemoveFromBasketFailed() {
+        Inventory inventory = new Inventory();
+        Basket basket = new Basket(inventory, 2);
+        InventoryItem itemBGLO = new InventoryItem("BGLO", 0.49, "Bagel", "Onion");
+        InventoryItem itemBGLP = new InventoryItem("BGLP", 0.39, "Bagel", "Plain");
+
+        assertTrue(basket.addToBasket(itemBGLO));
+        assertEquals(1, basket.getItemBasket().size());
+        assertFalse(basket.removeFromBasket(itemBGLP));
+        assertEquals(1, basket.getItemBasket().size());
+    }
 }
