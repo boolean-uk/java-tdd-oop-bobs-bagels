afda509e694610f3cd1ca87d09e61c822514763e
iliask796 <iliaskal7@gmail.com>
2023-09-15 16:02:00 +0300

    Small changes on existing methods + Populate Data Methods + txt files for data

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -1,35 +1,70 @@
 package com.booleanuk.core;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Inventory {
-    private final HashMap<String, String> nameToSkus;
-    private final HashMap<String, Product> skusToProducts;
+    private final HashMap<String, String> baseProducts;
+    private final HashMap<String, String> namesToSkus;
+    private final LinkedHashMap<String, Product> skusToProducts;
 
-    public Inventory() {
-        this.nameToSkus = new HashMap<>();
-        this.skusToProducts = new HashMap<>();
+    public Inventory(File file) {
+        this.namesToSkus = new HashMap<>();
+        this.skusToProducts = new LinkedHashMap<>();
+        this.baseProducts = new HashMap<>();
+        this.populateBaseData(file);
+    }
+
+    private void populateBaseData(File file) {
+        try {
+            Scanner myReader = new Scanner(file);
+            String[] dataArr;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                dataArr = data.split(" ");
+                baseProducts.put(dataArr[0],dataArr[1]);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadProducts(File file) {
+        try {
+            Scanner myReader = new Scanner(file);
+            String[] dataArr;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                dataArr = data.split(" ");
+                Product product = new Product(dataArr[2].replace('_', ' '), Double.parseDouble(dataArr[1]), dataArr[0]);
+                this.addNewProduct(product);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String constructProductName(String sku, String type) {
+        String productName = baseProducts.getOrDefault(sku.substring(0,3), null);
+        return productName == null? null: type + " " + productName;
     }
 
     public boolean addNewProduct(Product product) {
         if (skusToProducts.containsKey(product.getSku())) {
             return false;
         } else {
-            skusToProducts.put(product.getSku(), product);
             String productName = constructProductName(product.getSku(), product.getType());
-            nameToSkus.put(productName, product.getSku());
+            if (productName == null) return false;
+            namesToSkus.put(productName, product.getSku());
+            skusToProducts.put(product.getSku(), product);
             return true;
         }
-    }
-
-    private String constructProductName(String sku, String type) {
-        String productName = type + " ";
-        switch (sku.substring(0,3)){
-            case "BGL" -> productName += "Bagel";
-            case "COF" -> productName += "Coffee";
-            case "FIL" -> productName += "Filling";
-        }
-        return productName;
     }
 
     public String getProductList() {
@@ -37,13 +72,16 @@ public class Inventory {
         for (String sku: skusToProducts.keySet()) {
             Product product = skusToProducts.get(sku);
             String productName = constructProductName(product.getSku(), product.getType());
-            productList.append(product.getSku()).append(" | ").append(productName).append(" | ").append(product.getCost()).append("$\n");
+            productList.append(product.getSku()).append(" | ").append(product.getCost()).append("$ | ").append(productName).append("\n");
+        }
+        if (Objects.equals(String.valueOf(productList), "")){
+            return "Inventory empty.";
         }
         return String.valueOf(productList).substring(0,productList.length()-1);
     }
 
     private String searchProduct(String productName) {
-        return nameToSkus.getOrDefault(productName, null);
+        return namesToSkus.getOrDefault(productName, null);
     }
 
     public Product getProductByName(String productName) {
--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -3,11 +3,14 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class InventoryTest {
     @Test
     public void shouldAddNewProductsToInventory() {
-        Inventory inventory = new Inventory();
-        Product bagel = new Product("bagel",0.49, "BAG");
+        File skuFile = new File("./SkusOfBaseProducts.txt");
+        Inventory inventory = new Inventory(skuFile);
+        Product bagel = new Product("bagel",0.49, "BGL");
         Product coffee = new Product("coffee",1.49, "COF");
         Assertions.assertTrue(inventory.addNewProduct(bagel));
         Assertions.assertTrue(inventory.addNewProduct(coffee));
@@ -15,33 +18,49 @@ public class InventoryTest {
 
     @Test
     public void shouldNotAddExistingProductsAgain() {
-        Inventory inventory = new Inventory();
-        Product bagel1 = new Product("bagel",0.49, "BAG");
-        Product bagel2 = new Product("bagel",0.49, "BAG");
+        File skuFile = new File("./SkusOfBaseProducts.txt");
+        Inventory inventory = new Inventory(skuFile);
+        Product bagel1 = new Product("bagel",0.49, "BGL");
+        Product bagel2 = new Product("bagel",0.49, "BGL");
         Assertions.assertTrue(inventory.addNewProduct(bagel1));
         Assertions.assertFalse(inventory.addNewProduct(bagel2));
     }
 
     @Test
     public void shouldReturnListOfProducts() {
-        Inventory inventory = new Inventory();
-        Product bagel = new Product("Onion",0.49, "BGLON");
+        File skuFile = new File("./SkusOfBaseProducts.txt");
+        Inventory inventory = new Inventory(skuFile);
+        Product bagel = new Product("Onion",0.49, "BGLO");
         inventory.addNewProduct(bagel);
-        String expected = "BGLON | Onion Bagel | 0.49$";
+        Product coffee = new Product("Black", 0.99, "COFB");
+        inventory.addNewProduct(coffee);
+        String expected = "BGLO | 0.49$ | Onion Bagel\nCOFB | 0.99$ | Black Coffee";
         Assertions.assertEquals(expected, inventory.getProductList());
     }
 
     @Test
     public void shouldGetExistingProductWithCorrectName() {
-        Inventory inventory = new Inventory();
-        Product bagel = new Product("Onion",0.49, "BGLON");
+        File skuFile = new File("./SkusOfBaseProducts.txt");
+        Inventory inventory = new Inventory(skuFile);
+        Product bagel = new Product("Onion",0.49, "BGLO");
         inventory.addNewProduct(bagel);
         Assertions.assertEquals(bagel.toString(), inventory.getProductByName("Onion Bagel").toString());
     }
 
     @Test
     public void shouldGetNullForNonExistingProduct() {
-        Inventory inventory = new Inventory();
+        File skuFile = new File("./SkusOfBaseProducts.txt");
+        Inventory inventory = new Inventory(skuFile);
         Assertions.assertNull(inventory.getProductByName("Black Coffee"));
+    }
+
+    @Test
+    public void shouldGetProductWhenLoadingToInventory() {
+        File skuFile = new File("./SkusOfBaseProducts.txt");
+        Inventory inventory = new Inventory(skuFile);
+        File productsFile = new File("./FullProductList.txt");
+        inventory.loadProducts(productsFile);
+        Product bagel = new Product("Onion",0.49, "BGLO");
+        Assertions.assertEquals(bagel.toString(), inventory.getProductByName("Onion Bagel").toString());
     }
 }
