6be1a5ab9c7324cd1c4ffd32ae015e8d3939e70f
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-19 22:35:24 +0200

    Added method

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,7 +1,22 @@
 package com.booleanuk.core;
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 class Basket {
+
+    private List<String> items;
+
+    public Basket() {
+        this.items = new ArrayList<>();
+    }
+
+    public void add(String item) {
+        items.add(item);
+    }
+
+    public boolean contains(String item) {
+        return items.contains(item);
+    }
 
 }
