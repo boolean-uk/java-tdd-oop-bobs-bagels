f6212146c073c50e971d55620bfd7a50cedb4da0
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-17 15:17:06 +0100

    Make customer Getting total cost of itemlist and single item pass

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -6,6 +6,6 @@ public class Bagel extends Item {
     public Bagel(String variant) {
         super();
 
-
+        this.setPrice(0.39);
     }
 }
--- src/main/java/com/booleanuk/core/Customer.java
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Customer {
+    public double getTotalCost(ArrayList<Item> items) {
+        return items.stream()
+                .map(Item::getPrice)
+                .reduce(0.00, Double::sum);
+    }
+
+    public double getCostOfItem(Bagel bagel) {
+        return bagel.getPrice();
+    }
 }
