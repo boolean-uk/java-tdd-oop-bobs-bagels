556edf0a3690cbc78b5e3259ea8766e860ce3c59
Marit <marit.moe2@ntebb.no>
2024-01-17 15:22:47 +0100

    Test Item().

--- src/test/java/com/booleanuk/core/ItemTest.java
+++ src/test/java/com/booleanuk/core/ItemTest.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ItemTest {
+
+    @Test
+    public void testItemConstructor() {
+        Item bglo = new Item("Bagel", "Onion", 0.49);
+        Assertions.assertEquals("Bagel", bglo.name);
+        Assertions.assertEquals("Onion", bglo.variant);
+        Assertions.assertEquals(0.49, bglo.price);
+    }
 }
