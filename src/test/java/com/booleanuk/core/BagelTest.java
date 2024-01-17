a709e92af08d418274954ec7c224e3e4bfe79687
Giar Rehani <giarreh1@gmail.com>
2024-01-17 14:24:21 +0100

    Test: createBagel: added overloading for price

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -8,7 +8,13 @@ public class BagelTest {
     @Test
     public void testCreateBagel(){
         Bagel bagel = new Bagel("plain");
+        Bagel bagel2 = new Bagel("chocolate", 5.00);
+
         Assertions.assertEquals("plain", bagel.getName());
         Assertions.assertEquals(2.00, bagel.getPrice());
+
+        Assertions.assertEquals("chocolate", bagel2.getName());
+        Assertions.assertEquals(5.00, bagel2.getPrice());
+
     }
 }
