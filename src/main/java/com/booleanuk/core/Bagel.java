72cef170252cfe38c10de90d06ce74a6d0542fac
Giar Rehani <giarreh1@gmail.com>
2024-01-17 12:50:39 +0100

    Source: constructor, getName(), getPrice()

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -2,4 +2,20 @@ package com.booleanuk.core;
 
 public class Bagel {
 
+    String bagel;
+    double price;
+
+    public Bagel(String name){
+        this.bagel = name;
+        this.price = 2.00;
+    }
+
+    public String getName(){
+        return this.bagel;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
 }
