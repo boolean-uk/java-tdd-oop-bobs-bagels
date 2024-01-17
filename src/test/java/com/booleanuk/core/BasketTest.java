2d43f4e154185fe6bc85a75d1234d9acedcb7eaa
Giar Rehani <giarreh1@gmail.com>
2024-01-17 13:20:51 +0100

    Test: testTryRemoveBagelFromList() wrong syntax on tests

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -8,9 +8,17 @@ class BasketTest {
     @Test
     public void addBagelToBasket(){
         Basket basket = new Basket();
-
         Assertions.assertTrue(basket.add("plain"));
         Assertions.assertFalse(basket.add(""));
     }
 
+    @Test
+    public void testTryRemoveBagelFromList(){
+        Basket basket = new Basket();
+        basket.add("plain");
+        basket.add("onion");
+        Assertions.assertEquals(1, basket.basket.size());
+        Assertions.assertTrue(basket.remove("onion"));
+        Assertions.assertEquals(0, basket.basket.size());
+    }
 }
