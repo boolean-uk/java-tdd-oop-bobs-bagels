bbd7d3b2abe9c35bd2936a1a5254ac90bd5e3dab
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-17 17:00:57 +0100

    addFillingThatAllGivenBagelsAlreadyHaveTest added and passed

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -21,4 +21,17 @@ public class BasketTest {
         Assertions.assertEquals("Your basket doesn't contain that bagel.", actual);
     }
 
+    @Test
+    public void addFillingThatAllGivenBagelsAlreadyHaveTest() {
+        Basket basket = new Basket();
+        basket.addBagel(new Bagel("Onion"));
+        basket.addBagel(new Bagel("Onion"));
+
+        basket.addFilling("Cheese", "Onion");
+        basket.addFilling("Cheese", "Onion");
+        basket.addFilling("Egg", "Onion");
+
+        String actual = basket.addFilling("Cheese", "Onion");
+        Assertions.assertEquals("All bagels of that kind in your basket already has that filling.", actual);
+    }
 }
