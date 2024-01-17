ffa1252de1d70c7b6889b11a9594e15937354382
Marit <marit.moe2@ntebb.no>
2024-01-17 15:24:27 +0100

    Pass test Item().

--- src/main/java/com/booleanuk/core/Item.java
+++ src/main/java/com/booleanuk/core/Item.java
@@ -1,4 +1,13 @@
 package com.booleanuk.core;
 
 public class Item {
+    public String name;
+    public String variant;
+    public double price;
+
+    public Item(String name, String variant, double price) {
+        this.name = name;
+        this.variant = variant;
+        this.price = price;
+    }
 }
