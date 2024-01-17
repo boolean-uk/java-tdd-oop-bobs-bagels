e3efc9278618963269a2d84051ae61f03774876b
Ateeb <ateeb@live.no>
2024-01-17 13:12:13 +0100

    Ateeb Salam

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -3,6 +3,11 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BagelTest {
     @Test
     public void testAddBagel(){
@@ -16,6 +21,21 @@ public class BagelTest {
         abagel.addBagel("bagel1");
         Assertions.assertTrue(abagel.removeBagel("bagel1"));
         Assertions.assertFalse(abagel.removeBagel("bagel2"));
+    }
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Test
+    public void testBasketFull(){
+        Bagel abagel = new Bagel();
+        abagel.addBagel("bagel1");
+        abagel.addBagel("bagel2");
+        abagel.addBagel("bagel4");
+        abagel.addBagel("bagel5");
+        System.setOut(new PrintStream(outContent));
+        Assertions.assertTrue(abagel.basketFull());
+        assertEquals("Basket Full", outContent.toString().trim());
+
+
     }
 
 
