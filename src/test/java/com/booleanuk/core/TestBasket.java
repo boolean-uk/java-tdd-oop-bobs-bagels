a2ed89370cb2aa034c0c3ec514a5a6323a3713b3
Maha Riazi <maha.riazi@hotmail.no>
2024-01-17 13:42:30 +0100

    Red: written 3 first tests

--- src/test/java/com/booleanuk/core/TestBasket.java
+++ src/test/java/com/booleanuk/core/TestBasket.java
@@ -1,21 +1,56 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestBasket {
     @Test
     public void testAdd(){
+        Basket basket = new Basket(new User());
+        basket.add("BGLP");
+        basket.add("BGLO");
+        basket.add("BGLE");
+        basket.add("BGLO");
+        basket.add("FILE");
 
+        Assertions.assertEquals(5, basket.items);
+
+        basket.add("BGLP");
+        basket.add("BGLO");
+        basket.add("BGLE");
+
+        Assertions.assertEquals(8, basket.items);
     }
 
     @Test
     public void testAddProductNotInMenu(){
+        boolean thrown = false;
+        Basket basket = new Basket(new User());
 
+        try {
+            basket.add("BOOGIE");
+        } catch (IllegalStateException e) {
+            thrown = true;
+        }
+
+        Assertions.assertTrue(thrown);
     }
 
     @Test
     public void testRemove(){
+        Basket basket = new Basket(new User());
+        basket.add("FILS");
+        basket.add("COFL");
+        basket.add("COFW");
+        basket.add("COFC");
 
+        Assertions.assertEquals(4, basket.items);
+
+        Assertions.assertTrue(basket.remove("COFC"));
+        Assertions.assertTrue(basket.remove("COFW"));
+        Assertions.assertTrue(basket.remove("FILS"));
+        Assertions.assertEquals(1, basket.items);
+        Assertions.assertFalse(basket.remove("FILS"));
     }
 
     @Test
