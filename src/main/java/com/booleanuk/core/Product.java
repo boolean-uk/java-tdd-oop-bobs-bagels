921bd496376eb123cbefb5b0a80fada9fbd26b54
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 15:02:21 +0100

    Implement product

--- src/main/java/com/booleanuk/core/Product.java
+++ src/main/java/com/booleanuk/core/Product.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
 public class Product {
+    String sku;
+    double price;
+    String name;
+    String variant;
+
+    public Product(String sku, double price, String name, String variant) {
+        this.sku = sku;
+        this.price = price;
+        this.name = name;
+        this.variant = variant;
+    }
 }
