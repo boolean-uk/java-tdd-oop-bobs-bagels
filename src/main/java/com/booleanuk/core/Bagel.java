679bd68d200c398a46a22ffdfd51b6f6e824a63d
Giar Rehani <giarreh1@gmail.com>
2024-01-17 14:50:51 +0100

    Source: addFilling(String filling), ArrayList<String> fillings, getFilling(), Bagel can now have fillings

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -1,18 +1,37 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Bagel implements Product {
 
     String bagel;
+    String filling;
     double price;
+    ArrayList<String> fillings = new ArrayList<>(Arrays.asList("Bacon", "Egg", "Cheese", "Cream Cheese", "Smoked Salmon", "Ham"));
+
 
     public Bagel(String name){
         this.bagel = name;
         this.price = 2.00;
+        this.filling = "bland";
     }
     public Bagel(String name, double price){
         this.bagel = name;
         this.price = price;
+        this.filling = "bland";
     }
+
+    public boolean addFilling(String filling){
+        if (fillings.contains(filling)){
+            this.filling = filling;
+            System.out.println("Filling " + this.filling + " added to bagel");
+            return true;
+        }
+        System.out.println("Could not find filling in fillingList");
+        return false;
+    }
+
     @Override
     public String getName(){
         return this.bagel;
@@ -20,6 +39,10 @@ public class Bagel implements Product {
     @Override
     public double getPrice(){
         return this.price;
+    }
+
+    public String getFilling(){
+        return this.filling;
     }
 
 }
