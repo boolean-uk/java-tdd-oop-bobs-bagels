d1de99219af93700b92736121498b7073b200af2
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-17 15:08:35 +0100

    Add test for getting cost of itemlist and single item

--- src/test/java/com/booleanuk/core/CustomerTest.java
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -1,6 +1,10 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CustomerTest {
 
@@ -14,12 +18,19 @@ public class CustomerTest {
     public void testGetTotalCost() {
         Customer customer = new Customer();
 
+        Bagel bagel1 = new Bagel("Plain");
+        Bagel bagel2 = new Bagel("Plain");
+        Bagel bagel3 = new Bagel("Plain");
 
+
+        Assertions.assertEquals(0.39*3, customer.getTotalCost(new ArrayList<Item>(Arrays.asList(bagel1, bagel2, bagel3))));
     }
 
     @Test
     public void testGetCostOfItem() {
         Customer customer = new Customer();
+
+        Assertions.assertEquals(0.39, customer.getCostOfItem(new Bagel("plain")));
 
 
     }
