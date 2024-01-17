c6b3bea3e3000058879cee726f36a59c176bbbf1
Marit <marit.moe2@ntebb.no>
2024-01-17 15:45:42 +0100

    Pass test BobsBagelsShop(). Empty showInventory() method.

--- src/main/java/com/booleanuk/core/BobsBagelsShop.java
+++ src/main/java/com/booleanuk/core/BobsBagelsShop.java
@@ -1,4 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class BobsBagelsShop {
+    public HashMap<Item, Integer> inventory;
+    public int basketCapacity;
+
+    public BobsBagelsShop(HashMap<Item, Integer> newInventory, int capacity) {
+        this.inventory = newInventory;
+        this.basketCapacity = capacity;
+    }
+
+    public String showInventory() {
+        return "";
+    }
 }
