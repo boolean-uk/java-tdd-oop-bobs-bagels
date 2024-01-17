66320fb3dcbdd87bb508b8d05d59cff737359586
Ateeb <ateeb@live.no>
2024-01-17 13:00:44 +0100

    Ateeb Salam

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -10,4 +10,12 @@ public class BagelTest {
         Assertions.assertEquals("bagel1", abagel.addBagel("bagel1"));
 
     }
+    @Test
+    public void testRemoveBagel(){
+        Bagel abagel = new Bagel();
+        Assertions.assertTrue(abagel.removeBagel("bagel1"));
+        Assertions.assertFalse(abagel.removeBagel("bagel2"));
+    }
+
+
 }
