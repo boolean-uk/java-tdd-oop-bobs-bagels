36f4b2dfeaf8359208b00de2aac18220cf4eccc8
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 15:48:33 +0100

    Create basket add tests

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,7 +1,33 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
 
+    @Test
+    public void testAddBagelToBasket() {
+        Basket basket = new Basket();
+        basket.add("BGLO");
+        Assertions.assertTrue(basket.basketMap.containsKey("BGLO"));
+    }
+
+    @Test
+    public void testAddMultipleBagelsToBasket() {
+        Basket basket = new Basket();
+        basket.add("BGLO");
+        basket.add("BGLP");
+        basket.add("BGLE");
+        Assertions.assertTrue(basket.basketMap.containsKey("BGLO"));
+        Assertions.assertTrue(basket.basketMap.containsKey("BGLP"));
+        Assertions.assertTrue(basket.basketMap.containsKey("BGLE"));
+    }
+
+    @Test
+    public void testAddNoneExistingBagelToBasket() {
+        Basket basket = new Basket();
+        Assertions.assertFalse(basket.add("AAAHHH"));
+    }
 }
