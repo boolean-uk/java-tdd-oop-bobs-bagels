7aa6ba8606ad5d141ab92a2f458ae40d4e9a9219
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-17 15:54:42 +0100

    addNonExistingBagelToBasketTest passed (+ small change to domain model)

--- src/main/java/com/booleanuk/core/Store.java
+++ src/main/java/com/booleanuk/core/Store.java
@@ -1,14 +1,14 @@
 package com.booleanuk.core;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 
 public class Store {
     private HashMap<Integer, Basket> baskets;
+    private HashMap<String, Double> inventory;
 
     public Store() {
         baskets = new HashMap<>();
+        initilizeInventory();
     }
     public int createBasket() {
         Basket basket = new Basket();
@@ -21,7 +21,30 @@ public class Store {
     }
 
     public String addBagelToBasket(String bagel, int basketId) {
+        String sku = "BGL" + bagel.toUpperCase().charAt(0);
+        if(!inventory.containsKey(sku)) {
+            return "Bob's bagels doesn't have that bagel.";
+        }
         baskets.get(basketId).addBagel(new Bagel(bagel));
         return "Bagel added.";
+    }
+
+    private void initilizeInventory() {
+        inventory = new HashMap<>();
+        inventory.put("BGLO", 0.49);
+        inventory.put("BGLP", 0.39);
+        inventory.put("BGLE", 0.49);
+        inventory.put("BGLS", 0.49);
+        inventory.put("COFB", 0.99);
+        inventory.put("COFW", 1.19);
+        inventory.put("COFC", 1.29);
+        inventory.put("COFL", 1.29);
+        inventory.put("FILB", 0.12);
+        inventory.put("FILE", 0.12);
+        inventory.put("FILC", 0.12);
+        inventory.put("FILX", 0.12);
+        inventory.put("FILS", 0.12);
+        inventory.put("FILH", 0.12);
+
     }
 }
