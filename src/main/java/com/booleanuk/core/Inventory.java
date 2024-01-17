1182e5c3de6121d9545a9fd56e71cabeac2925f4
Sebastian <sebastian.thunberg@hotmail.se>
2024-01-17 13:00:31 +0100

    updated bagel id from int to string in Test

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -2,18 +2,24 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 
+
+
 public class Inventory {
 	ArrayList<Integer> inventory;
 
 	public Inventory() {
 		this.inventory = new ArrayList<>();
 	}
-
+	inti();
 	public void addItem(int id) {
 		inventory.add(id);
 	}
 
 	public boolean inInventory(int id) {
 		return inventory.contains(id);
+	}
+	public void init(){
+		ArrayList<>
+
 	}
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,76 +4,64 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
-	Inventory inventory;
+
 
 	@Test
 	public void addBagelTest() {
-		initInventory();
+		Inventory inventory = new Inventory();
 		Basket basket = new Basket(inventory);
-		Assertions.assertFalse(basket.inBasket(1));
-		Assertions.assertFalse(basket.inBasket(2));
+		Assertions.assertFalse(basket.inBasket("BGLP"));
+		Assertions.assertFalse(basket.inBasket("BGLO"));
 		try {
-			basket.addBagel(1);
+			basket.addBagel("BGLP");
 		} catch (NotInInventoryException e) {
 			throw new RuntimeException(e);
 		}
-		Assertions.assertTrue(basket.inBasket(1));
-		Assertions.assertFalse(basket.inBasket(2));
+		Assertions.assertTrue(basket.inBasket("BGLP"));
+		Assertions.assertFalse(basket.inBasket("BGLO"));
 	}
 
 	@Test
 	public void removeBagelTest() {
-		initInventory();
+		Inventory inventory = new Inventory();
 		Basket basket = new Basket(inventory);
 		try {
-			basket.addBagel(1);
+			basket.addBagel("BGLP");
 		} catch (NotInInventoryException e) {
 			throw new RuntimeException(e);
 		}
-		Assertions.assertTrue(basket.inBasket(1));
-		Assertions.assertFalse(basket.inBasket(2));
+		Assertions.assertTrue(basket.inBasket("BGLP"));
+		Assertions.assertFalse(basket.inBasket("BGLO"));
 		try {
-			basket.removeBagel(1);
+			basket.removeBagel("BGLP");
 		} catch (NotInBasketException e) {
 			throw new RuntimeException(e);
 		}
-		Assertions.assertFalse(basket.inBasket(1));
-		Assertions.assertFalse(basket.inBasket(2));
+		Assertions.assertFalse(basket.inBasket("BGLP"));
+		Assertions.assertFalse(basket.inBasket("BGLO"));
 	}
 
 	@Test
 	public void basketIsFullTest() {
-		initInventory();
+		Inventory inventory = new Inventory();
 		Basket basket = new Basket(inventory);
 		Assertions.assertFalse(basket.isFull());
 		try {
-			basket.addBagel(1);
-			basket.addBagel(2);
-			basket.addBagel(2);
+			basket.addBagel("BGLP");
+			basket.addBagel("BGLO");
+			basket.addBagel("BGLP");
 		} catch (NotInInventoryException e) {
 			throw new RuntimeException(e);
 		}
 		Assertions.assertTrue(basket.isFull());
 
 		try {
-			basket.removeBagel(1);
+			basket.removeBagel("BGLP");
 		} catch (NotInBasketException e) {
 			throw new RuntimeException(e);
 		}
-
 		Assertions.assertFalse(basket.isFull());
 
 	}
 
-	private void initInventory() {
-		inventory = new Inventory();
-		Bagel bagel1 = new Bagel(0, "Plain", 1.99);
-		Bagel bagel2 = new Bagel(1, "Ham", 1.99);
-		Bagel bagel3 = new Bagel(2, "Cheese", 1.99);
-		Bagel bagel4 = new Bagel(3, "Everything", 2.49);
-		inventory.addItem(bagel1.id);
-		inventory.addItem(bagel2.id);
-		inventory.addItem(bagel3.id);
-		inventory.addItem(bagel4.id);
-	}
 }
