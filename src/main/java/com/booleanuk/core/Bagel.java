772f16f3699ef60fad140347141aee3f095f7212
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-17 16:13:03 +0100

    addFillingThatAlreadyExistsTest passed

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -1,9 +1,13 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Bagel {
     private String name;
+    private ArrayList<String> fillings;
     public Bagel(String name) {
         this.name = name;
+        fillings = new ArrayList<>();
     }
 
     @Override
@@ -16,6 +20,11 @@ public class Bagel {
     }
 
     public boolean addFilling(String filling) {
+        filling = filling.toLowerCase();
+        if(fillings.contains(filling)) {
+            return false;
+        }
+        fillings.add(filling);
         return true;
     }
 }
