ce9a751acd3968a8392a8bcf53e5159a7d4bf411
Giar Rehani <giarreh1@gmail.com>
2024-01-17 13:23:48 +0100

    Source: remove()

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,7 +1,38 @@
 package com.booleanuk.core;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Basket {
+
+    ArrayList<Bagel> basket;
+    double total;
+    public Basket(){
+        this.basket = new ArrayList<>();
+        this.total = 0;
+    }
+
+    public boolean add(String bagelType){
+        Bagel bagel = new Bagel(bagelType);
+        if(!bagel.getName().isEmpty()){
+            basket.add(bagel);
+            System.out.println("Added " + bagel.getName());
+            return true;
+        }
+        System.out.println("Invalid bagel");
+        return false;
+    }
+
+    public boolean remove(String bagelType){
+        for (int i = 0; i < basket.size(); i++) {
+            if(Objects.equals(basket.get(i).getName(), bagelType)){
+                System.out.println("Removing " + basket.get(i).getName() + " from list");
+                basket.remove(basket.get(i));
+                return true;
+            }
+        }
+        System.out.println("Bagel not found");
+        return false;
+    }
 
 }
