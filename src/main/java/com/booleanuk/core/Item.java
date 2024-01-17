395eebbe91290b4240e05e358ebabae1bf14c279
Marit <marit.moe2@ntebb.no>
2024-01-17 15:31:32 +0100

    Pass test checkPrice().

--- src/main/java/com/booleanuk/core/Item.java
+++ src/main/java/com/booleanuk/core/Item.java
@@ -3,11 +3,15 @@ package com.booleanuk.core;
 public class Item {
     public String name;
     public String variant;
-    public double price;
+    private final double price;
 
     public Item(String name, String variant, double price) {
         this.name = name;
         this.variant = variant;
         this.price = price;
+    }
+
+    public double checkPrice() {
+        return this.price;
     }
 }
