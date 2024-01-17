82469ca3b15747705045a0c34248e077d71f3e2b
Giar Rehani <giarreh1@gmail.com>
2024-01-17 12:48:13 +0100

    Test: testCreateBagel()

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BagelTest {
+
+    @Test
+    public void testCreateBagel(){
+        Bagel bagel = new Bagel("plain");
+        Assertions.assertTrue("plain", bagel.getName());
+        Assertions.assertTrue(2.00, bagel.getPrice());
+        Assertions.assertFalse("chocolate", bagel.getName());
+        Assertions.assertFalse(4.20, bagel.getPrice());
+    }
 }
