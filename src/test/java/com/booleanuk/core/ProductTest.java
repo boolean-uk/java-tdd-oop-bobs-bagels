5cedfe542456642ad5307acb5dd78edfb085de45
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 15:01:56 +0100

    Create test for product

--- src/test/java/com/booleanuk/core/ProductTest.java
+++ src/test/java/com/booleanuk/core/ProductTest.java
@@ -1,7 +1,18 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    @Test
+    public void testProductConstructor() {
+        Product product = new Product("BGLO",0.49,"Bagel","Onion");
+        Assertions.assertEquals("BGLO",product.sku);
+        Assertions.assertEquals(0.49,product.price);
+        Assertions.assertEquals("Bagel",product.name);
+        Assertions.assertEquals("Onion",product.variant);
+    }
 }
