caacb29e9ea3f6dd1b4a349599b7418a305753f9
stp <steventphung6@gmail.com>
2024-01-17 15:43:26 +0100

    testing removing items and fake items

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -20,4 +20,38 @@ public class BasketTest {
         Assertions.assertFalse(basket.add("Carrot"));
     }
 
+    @Test
+    public void testCapacityCheck() {
+        Basket basket = new Basket();
+        basket.add("BGLO");
+        basket.add("BGLP");
+        basket.add("COFB");
+        basket.add("COFW");
+
+        Assertions.assertEquals(3,basket.basketList.size());
+    }
+
+    @Test
+    public void testRemoveItem() {
+        Basket basket = new Basket();
+        basket.add("BGLO");
+        basket.add("BGLP");
+        basket.add("COFB");
+
+        Assertions.assertEquals(3, basket.basketList.size());
+        Assertions.assertTrue(basket.remove("BGLP"));
+        Assertions.assertEquals(2, basket.basketList.size());
+    }
+    @Test
+    public void testRemoveFakeItem() {
+        Basket basket = new Basket();
+        basket.add("BGLO");
+        basket.add("BGLP");
+        basket.add("COFB");
+
+        Assertions.assertEquals(3, basket.basketList.size());
+        Assertions.assertFalse(basket.remove("Carrot"));
+        Assertions.assertEquals(3, basket.basketList.size());
+
+    }
 }
