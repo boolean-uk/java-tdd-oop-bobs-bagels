91fc8477c3e17d9df8eff010fb2dcb1213959cbc
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 15:14:38 +0100

    Create test for inventory constructor

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,7 +1,16 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryTest {
+
+    @Test
+    public void testInventoryConstructorAddsProducts() {
+        Inventory inventory = new Inventory();
+        Assertions.assertFalse(inventory.products.isEmpty());
+    }
 
 }
