2c0cf8b9d11f4e407780370d6a4b78a9d5c5f2f0
Maha Riazi <maha.riazi@hotmail.no>
2024-01-17 13:01:24 +0100

    Code skeleton

---
+++ src/test/java/com/booleanuk/core/TestBasket.java
@@ -0,0 +1,45 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class TestBasket {
+    @Test
+    public void testAdd(){
+
+    }
+
+    @Test
+    public void testAddProductNotInMenu(){
+
+    }
+
+    @Test
+    public void testRemove(){
+
+    }
+
+    @Test
+    public void testRemoveProductNotInBasket(){
+
+    }
+
+    @Test
+    public void testCapacity(){
+
+    }
+
+    @Test
+    public void testShrinkingCapacityWhenItemsInBasket(){
+
+    }
+
+    @Test
+    public void testDisplayMenu(){
+
+    }
+
+    @Test
+    public void testTotalCost(){
+
+    }
+}
