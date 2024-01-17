69e606f385114165d88d05fb09e27603398d65a6
noahlenn <noahlenn@hotmail.com>
2024-01-17 14:11:18 +0100

    added code from old repo

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,4 +1,44 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+
+    public ArrayList<String> bagelsList = new ArrayList<>();
+    int basketCapacity = 4;
+    boolean basketIsFull = false;
+
+    public Basket() {
+        this.bagelsList = new ArrayList<>();
+    }
+
+    public boolean addBagel(String bagel) {
+        if (bagelsList.size() == basketCapacity) {
+            System.out.println("Basket is full");
+            return false;
+        } else {
+            return this.bagelsList.add(bagel);
+        }
+
+    }
+
+    public boolean removeBagel(String bagel) {
+        if (bagelsList.isEmpty()) {
+            System.out.println("Can't remove item from empty basket");
+            return false;
+        }
+        return this.bagelsList.remove(bagel);
+
+    }
+
+    public int changeBasketCapacity(int change){
+        if((this.basketCapacity + change) < 0){
+            System.out.println("Can't have negative basket capacity");
+            return basketCapacity;
+        } else {
+            return basketCapacity += change;
+        }
+
+    }
 }
+
--- src/test/java/com/booleanuk/core/BobsBagelsTest.java
+++ src/test/java/com/booleanuk/core/BobsBagelsTest.java
@@ -1,4 +1,64 @@
 package com.booleanuk.core;
 
-public class BobsBagelsTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BobsBagelsTest {
+
+    @Test
+    public void addItemToBasketReturnTrue() {
+        Basket basket = new Basket();
+
+        boolean result = basket.addBagel("plain");
+        Assertions.assertTrue(result);
+    }
+
+
+    @Test
+    public void removeItemFromBasket() {
+        Basket basket = new Basket();
+
+        basket.addBagel("plain");
+        boolean result = basket.removeBagel("plain");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void removeItemNotInBasket() {
+        Basket basket = new Basket();
+
+        boolean result = basket.removeBagel("bread");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void checkIfBasketIsFull() {
+        Basket basket = new Basket();
+        basket.addBagel("plain");
+        basket.addBagel("chocolate");
+        basket.addBagel("honey");
+        basket.addBagel("salmon");
+
+        Assertions.assertFalse(basket.addBagel("cheese"));
+
+
+    }
+
+    @Test
+    public void checkIfBasketSizeCanIncrease() {
+        Basket basket = new Basket();
+
+        basket.changeBasketCapacity(5);
+        Assertions.assertEquals(9, basket.basketCapacity);
+    }
+
+    @Test
+    public void checkIfBasketSizeCanDecrease() {
+        Basket basket = new Basket();
+
+        basket.changeBasketCapacity(-2);
+        Assertions.assertEquals(2, basket.basketCapacity);
+
+
+    }
 }
