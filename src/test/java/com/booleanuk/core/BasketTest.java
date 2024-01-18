acfcd5a6c0c4d22893140118a085f1b48c2a5138
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 20:38:12 +0100

    Test add filling written

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -25,8 +25,6 @@ class BasketTest {
         Basket basket = new Basket();
         Bagel bagel1 = new Bagel("BGLO", new String[]{"FILX", "FILC"});
         Bagel bagel2 = new Bagel("BGLO");
-
-
         basket.add(bagel1);
 
         //Remove bagel not in basket
@@ -34,5 +32,24 @@ class BasketTest {
 
         //Remove bagel in basket
         Assertions.assertTrue(basket.remove(bagel1));
+    }
+
+    @Test
+    public void testAddFillings(){
+        Basket basket = new Basket();
+        Bagel bagel = new Bagel("BGLO", new String[]{"FILX", "FILC"});
+        Bagel bagel2 = new Bagel("BGLO");
+        Filling filling1 = new Filling("FILB");
+        Filling filling2 = new Filling("FILE");
+        basket.add(bagel);
+
+        //Add single filling to bagel in basket
+        Assertions.assertTrue(basket.addFillings(bagel, new Filling[]{filling1}));
+
+        //Add multiple filling to bagel in basket
+        Assertions.assertTrue(basket.addFillings(bagel, new Filling[]{filling1, filling2}));
+
+        //Add filling to bagel not in basket
+        Assertions.assertFalse(basket.addFillings(bagel2, new Filling[]{filling1}));
     }
 }
