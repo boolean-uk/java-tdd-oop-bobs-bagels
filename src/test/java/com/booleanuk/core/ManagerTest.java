7ccdab240fba8ab5153fec2b295f42902ddc4bc7
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 21:47:49 +0100

    Test for clearing baskets with a size larger than new basket size implemented

--- src/test/java/com/booleanuk/core/ManagerTest.java
+++ src/test/java/com/booleanuk/core/ManagerTest.java
@@ -1,7 +1,29 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerTest {
 
+    @Test
+    public void testSetBasket(){
+        Manager manager = new Manager();
+        Basket basket1 = new Basket();
+        Basket basket2 = new Basket();
+
+        basket1.add(new Bagel("BGLO"));
+        basket1.add(new Bagel("BGLO"));
+        basket2.add(new Bagel("BGLP"));
+
+        //
+
+        //Test if baskets with a larger size than new max clears
+        manager.setBasketSize(1);
+
+        Assertions.assertEquals(0, basket1.getProducts.size());
+        Assertions.assertEquals(1, basket2.getProducts.size());
+
+    }
 }
