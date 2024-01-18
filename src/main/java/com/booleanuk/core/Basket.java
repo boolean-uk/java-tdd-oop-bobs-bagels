099979f832b735db66cfc522653934c5dfe688ef
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-18 12:59:37 +0100

    Implement add method with basket capacity

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,14 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Basket {
     private HashMap<String, Integer> basketMap;
     private Inventory inventory;
+    private int capacity;
 
     public Basket() {
         this.basketMap = new HashMap<>();
         this.inventory = new Inventory();
+        this.capacity = 5;
     }
 
     public HashMap<String, Integer> getBasketMap() {
@@ -19,19 +22,24 @@ public class Basket {
         return inventory;
     }
 
-    public boolean add(String sku) {
+    public String add(String sku) {
+        if (this.basketMap.values().stream()
+                .mapToInt(Integer::valueOf)
+                .sum() >= this.capacity) {
+            return "Basket is full";
+        }
         if (this.basketMap.containsKey(sku)) {
             this.basketMap.put(sku, this.basketMap.get(sku)+1);
-            return true;
+            return "Product added to basket";
         } else {
             for (Product product : inventory.getProducts()) {
                 if (sku.equals(product.getSku())) {
                     this.basketMap.put(sku, 1);
-                    return true;
+                    return "Product added to basket";
                 }
             }
         }
-        return false;
+        return "Product not found";
     }
 
     public boolean remove(String sku) {
