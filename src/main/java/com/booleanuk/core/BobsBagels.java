7520597433bfe83b274b287e6545319775044d02
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-18 14:57:01 +0100

    Implement setBasketCapacity method

--- src/main/java/com/booleanuk/core/BobsBagels.java
+++ src/main/java/com/booleanuk/core/BobsBagels.java
@@ -1,4 +1,33 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BobsBagels {
+    private List<Basket> baskets;
+
+    public BobsBagels() {
+        this.baskets = new ArrayList<>();
+    }
+
+    public List<Basket> getBaskets() {
+        return baskets;
+    }
+
+    public String setBasketCapacity(int cap) {
+        if (cap < 1) {
+            return "Minimum capacity for baskets are 1";
+        }
+        boolean overCap = false;
+        for (Basket basket: this.baskets) {
+            if (basket.getNumberOfItems() > cap) {
+                overCap = true;
+            }
+            basket.setCapacity(cap);
+        }
+        if (overCap) {
+            return "Capacity set to 1. There are customers with more than 1 product in basket";
+        }
+        return "Capacity set to 3";
+    }
 }
