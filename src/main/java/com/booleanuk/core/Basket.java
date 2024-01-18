d5759d674ebc00a3a78e437e74443718165a7ea5
Marit <marit.moe2@ntebb.no>
2024-01-18 13:59:38 +0100

    Skeleton of Basket class.

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,4 +1,37 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    private HashMap<Item, Integer> basketContent;
+    private int basketCapacity;
+
+    public Basket(int basketCapacity) {
+        this.basketContent = new HashMap<>();
+        this.basketCapacity = basketCapacity;
+    }
+
+    public HashMap<Item, Integer> getBasketContent() {
+        return this.basketContent;
+    }
+
+    public int getBasketCapacity() {
+        return this.basketCapacity;
+    }
+
+    public void add(Item item) {
+
+    }
+
+    public void remove(Item item) {
+
+    }
+
+    public double totalCost() {
+        return 0.0;
+    }
+
+    public String changeCapacity(int newCapacity) {
+        return "";
+    }
 }
