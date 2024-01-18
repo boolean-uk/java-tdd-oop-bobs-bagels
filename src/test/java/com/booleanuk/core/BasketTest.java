03d6209463575e4237bfbbe7fd2ea56f18fcb486
Marit <marit.moe2@ntebb.no>
2024-01-18 14:46:35 +0100

    Test totalCost().

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -45,6 +45,21 @@ public class BasketTest {
 
     @Test
     public void totalCost() {
+        HashMap<Item, Integer> testInventory = new HashMap<>();
+        Item bglo = new Item("BGLO","Bagel", "Onion", 0.49);
+        Item bglp = new Item("BGLP","Bagel", "Plain", 0.39);
+        testInventory.put(bglo, 100);
+        testInventory.put(bglp, 100);
+        BobsBagelsShop shop = new BobsBagelsShop(testInventory);
+        Basket basket = new Basket(shop, 3);
+        basket.add(bglo);
+        basket.add(bglp);
+        basket.add(bglo);
+
+        Assertions.assertEquals(1.37, basket.totalCost());
+
+        Basket basket2 = new Basket(shop, 3);
+        Assertions.assertEquals(0.0, basket2.totalCost());
     }
 
     @Test
