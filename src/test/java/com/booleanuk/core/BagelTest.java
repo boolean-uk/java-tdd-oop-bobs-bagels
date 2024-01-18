bb9b69d2e1a25ecb5fb5780083be8801a3bea701
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 13:56:07 +0100

    Tests for getting name variant and price on bagels created

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -1,7 +1,42 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BagelTest {
 
+    @BeforeEach
+    void setUp() {
+        Bagel plain = new Bagel("BGLP");
+        Bagel onion = new Bagel("BGLO");
+        Bagel everything = new Bagel("BGLE");
+        Bagel sesame = new Bagel("BGLS");
+    }
+
+    @Test
+    public void testGetName(){
+        Assertions.assertEquals("Bagel", plain.getName());
+        Assertions.assertEquals("Bagel", onion.getName());
+        Assertions.assertEquals("Bagel", everything.getName());
+        Assertions.assertEquals("Bagel", sesame.getName());
+    }
+
+    @Test
+    public void testGetVariant(){
+        Assertions.assertEquals("Plain", plain.getVariant());
+        Assertions.assertEquals("Onion", onion.getVariant());
+        Assertions.assertEquals("Everything", everything.getVariant());
+        Assertions.assertEquals("Sesame", sesame.getVariant());
+    }
+
+    @Test
+    public void testGetPrice(){
+        Assertions.assertEquals(0.39, plain.getPrice());
+        Assertions.assertEquals(0.49, onion.getPrice());
+        Assertions.assertEquals(0.49, everything.getPrice());
+        Assertions.assertEquals(0.49, sesame.getPrice());
+    }
 }
