89a96693009ca5d96fb7692cbb06c76060fc4553
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 14:59:37 +0100

    Test for adding bagels written.

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,7 +1,28 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
+    @Test
+    public void testAdd(){
+       Basket basket = new Basket();
+       basket.setMaxSize(2);
 
+       //Add Coffee or Bagel
+       Assertions.assertTrue(basket.add("BGLO"));
+       Assertions.assertTrue(basket.add("COFB"));
+
+       //Add Filling (You can not only buy a filling)
+       Assertions.assertFalse(basket.add("FILC"));
+
+       //Add With wrong ID
+       Assertions.assertFalse(basket.add("BSGO"));
+
+       //Try adding with a full basket
+       Assertions.assertFalse(basket.add("BGLO"));
+
+    }
 }
