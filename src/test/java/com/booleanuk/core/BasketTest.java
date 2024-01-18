33d96806328985e7b1bb3029f0a5238972480a2d
Marit <marit.moe2@ntebb.no>
2024-01-18 14:12:21 +0100

    Test add().

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,4 +1,40 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 public class BasketTest {
+    @Test
+    public void add() {
+        HashMap<Item, Integer> testInventory = new HashMap<>();
+        Item bglo = new Item("BGLO","Bagel", "Onion", 0.49);
+        Item bglp = new Item("BGLP","Bagel", "Plain", 0.39);
+        testInventory.put(bglo, 100);
+        testInventory.put(bglp, 100);
+        BobsBagelsShop shop = new BobsBagelsShop(testInventory);
+        Basket basket = new Basket(3);
+        Assertions.assertEquals("Item BGLO added to basket.", basket.add(bglo));
+        Assertions.assertEquals("Item BGLP added to basket.", basket.add(bglp));
+
+        Item bgle = new Item("BGLE","Bagel", "Everything", 0.49);
+        Assertions.assertEquals("Chosen item not in stock.", basket.add(bgle));
+
+        testInventory.put(bgle, 100);
+        Assertions.assertEquals("Item BGLE added to basket.", basket.add(bgle));
+        Assertions.assertEquals("Basket is full.", basket.add(bglo));
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void totalCost() {
+    }
+
+    @Test
+    public void changeCapacity() {
+    }
 }
