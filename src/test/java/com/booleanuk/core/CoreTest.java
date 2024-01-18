eb913ba29ef32efa2394e211bd36cdea0112c8c5
Espen Solhaug <espen.solhaug@no.experis.com>
2024-01-18 12:41:35 +0100

    tests refactored to fit new classes

--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -28,8 +28,8 @@ class CoreTest {
             add(new Filling("FILS", 0.12, "Filling", "Smoked Salmon"));
             add(new Filling("FILH", 0.12, "Filling", "Ham"));
         }};
-        store = new Store(5, new ArrayList<Customer>(), itemsInStock);
-        customer = store.addCustomer(new Customer("Tom"));
+        store = new Store(5);
+        customer = store.addCustomer("Tom");
     }
     @Test
     public void orderCoffeeInEmptyBasketTest() {
@@ -38,7 +38,7 @@ class CoreTest {
 
     @Test
     public void orderCoffeeInBasketWithElementsTest() {
-        customer.getBasket().addItem(store.getItemsInStock().get(5));
+        customer.getInventory().addItem(store.getItemsInStock().get(5));
         Assertions.assertTrue(customer.order("COFW"));
     }
 
@@ -51,7 +51,7 @@ class CoreTest {
             add(new Filling("FILB", 0.12, "Filling", "Bacon"));
             add(new Filling("FILE", 0.12, "Filling", "Egg"));
         }};
-        customer.getBasket().addAll(fillItems);
+        customer.getInventory().getItems().addAll(fillItems);
         Assertions.assertFalse(customer.order("COFL"));
     }
 
@@ -61,7 +61,7 @@ class CoreTest {
             add(new Coffee("COFW", 0.99, "Coffee", "White"));
             add(new Coffee("COFC", 1.29, "Coffee", "Cappuccino"));
         }};
-        customer.getBasket().addAll(fillItems);
+        customer.getInventory().getItems().addAll(fillItems);
         Assertions.assertTrue(customer.order("COFW"));
     }
 
@@ -70,8 +70,8 @@ class CoreTest {
         ArrayList<Item> fillItems = new ArrayList<Item>() {{
             add(new Coffee("COFW", 0.99, "Coffee", "White"));
         }};
-        customer.getBasket.addAll(fillItems);
-        Assertions.assertEquals("Coffee White has been deleted", customer.deleteItem("COFW"));
+        customer.getInventory().getItems().addAll(fillItems);
+        Assertions.assertEquals("Coffee White has been deleted", customer.deleteItems("COFW"));
     }
 
     @Test
@@ -79,8 +79,8 @@ class CoreTest {
         ArrayList<Item> fillItems = new ArrayList<Item>() {{
             add(new Bagel("BGLO", 0.49, "Bagel", "Onion"));
         }};
-        customer.getBasket.addAll(fillItems);
-        Assertions.assertEquals("Bagel Plain was not found", customer.deleteItem("BGLP"));
+        customer.getInventory().getItems().addAll(fillItems);
+        Assertions.assertEquals("Bagel Plain was not found", customer.deleteItems("BGLP"));
     }
 
     @Test
@@ -90,9 +90,9 @@ class CoreTest {
             add(new Coffee("COFB", 0.99, "Coffee", "Black"));
             add(new Bagel("BGLO", 0.49, "Bagel", "Onion"));
         }};
-        customer.getBasket.addAll(fillItems);
-        Assertions.assertEquals("Bagel Onion has been deleted", customer.deleteItem("BGLO"));
-        Assertions.assertEquals("Vanilla", customer.getBasket().get(customer.getBasket().size()-1).getSKU());
+        customer.getInventory().getItems().addAll(fillItems);
+        Assertions.assertEquals("Bagel Onion has been deleted", customer.deleteItems("BGLO"));
+        Assertions.assertEquals("Vanilla", customer.getInventory().getItems().get(customer.getInventory().getItems().size()-1).getSKU());
     }
 
     @Test
@@ -111,7 +111,7 @@ class CoreTest {
             add(new Filling("FILB", 0.12, "Filling", "Bacon"));
             add(new Filling("FILX", 0.12, "Filling", "Cream Cheese"));
         }};
-        customer.getBasket.addAll(fillItems);
+        customer.getInventory().getItems().addAll(fillItems);
         store.setCapacity(6);
         Assertions.assertTrue(customer.order("COFW"));
     }
@@ -122,12 +122,12 @@ class CoreTest {
             add(new Bagel("BGLO", 0.49, "Bagel", "Onion"));
             add(new Coffee("COFB", 0.99, "Coffee", "Black"));
         }};
-        customer.getBasket.addAll(fillItems);
+        customer.getInventory().getItems().addAll(fillItems);
 
         store.setCapacity(1);
-        Assertions.assertEquals(1, basket.bagels.size());
-        Assertions.assertEquals("BGLO", customer.getBasket().get(0).getSKU());
-        Assertions.assertFalse(customer.getBasket().getAll().contains("COFB"));
+        Assertions.assertEquals(1, customer.getInventory().getItems().size());
+        Assertions.assertEquals("BGLO", customer.getInventory().getItems().get(0).getSKU());
+        Assertions.assertFalse(customer.getInventory().getItems().contains("COFB"));
     }
 
     @Test
@@ -139,11 +139,11 @@ class CoreTest {
             add(new Filling("FILB", 0.12, "Filling", "Bacon"));
             add(new Filling("FILX", 0.12, "Filling", "Cream Cheese"));
         }};
-        customer.getBasket.addAll(fillItems);
+        customer.getInventory().getItems().addAll(fillItems);
         store.setCapacity(3);
-        Assertions.assertEquals(3, customer.getBasket().size());
+        Assertions.assertEquals(3, customer.getInventory().getItems().size());
         Assertions.assertEquals(3, Store.BASKETCAPACITY);
-        Assertions.assertEquals("Plain", customer.getBasket().get(basket.bagels.size()-1).getSKU());
+        Assertions.assertEquals("Plain", customer.getInventory().getItems().get(customer.getInventory().getItems().size()-1).getSKU());
     }
 
     @Test
@@ -152,8 +152,8 @@ class CoreTest {
             add(new Bagel("BGLO", 0.49, "Bagel", "Onion"));
             add(new Coffee("COFB", 0.99, "Coffee", "Black"));
         }};
-        customer.getBasket().addAll(fillItems);
-        Assertions.assertEquals(1.48, customer.getBasket().calculateTotalCost());
+        customer.getInventory().getItems().addAll(fillItems);
+        Assertions.assertEquals(1.48, customer.getInventory().calculateTotalCost());
     }
 
     @Test
@@ -163,7 +163,7 @@ class CoreTest {
             add(new Coffee("COFB", 0.99, "Coffee", "Black"));
             add(new Filling("FILB", 0.12, "Filling", "Bacon"));
         }};
-        customer.getBasket().addAll(fillItems);
+        customer.getInventory().getItems().addAll(fillItems);
         Assertions.assertEquals(1.60, customer.calculateCostBeforeOrder());
     }
 
@@ -172,13 +172,13 @@ class CoreTest {
         ArrayList<Item> fillItems = new ArrayList<Item>() {{
             add(new Bagel("BGLO", 0.49, "Bagel", "Onion"));
         }};
-        customer.getBasket().addAll(fillItems);
-        Assertions.assertEquals("Bacon", customer.chooseFilling("FILB").get(0).getVariant());
+        customer.getInventory().getItems().addAll(fillItems);
+        Assertions.assertEquals("Bacon", customer.chooseFilling("FILB").getItems().get(0).getVariant());
     }
 
     @Test
     public void wrongInputInOrderGivesFalse() {
-        Assertions.AssertFalse(customer.order("Hi there"));
+        Assertions.assertFalse(customer.order("Hi there"));
     }
 }
 
