c9401dd150c9c9dbd85b4dbbc4493ced5add3086
Sandersaether <sander.sather@gmail.com>
2024-01-18 14:25:16 +0100

    test for filling

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -51,10 +51,19 @@ class BasketTest {
     @Test
     void testTotalCost(){
         Basket basket =new Basket();
-        Product product = new Product( "",5,"Bagel","");
+        Product bagel = new Bagel( "",5,"Bagel");
         Product product2 = new Product( "",5,"Bagel2","");
-        basket.addItem(product);
+        basket.addItem(bagel);
         basket.addItem(product2);
         Assertions.assertEquals(10,basket.calculateTotalCost());
+    }
+    @Test
+    public void checkIfFillingIsAdded(){
+        Basket basket =new Basket();
+        Filling filling = new Filling( "",5,"Filling");
+        basket.addItem(filling);
+        Assertions.assertTrue(basket.bagelBasket.contains(filling));
+        Assertions.assertEquals("Filling",basket.bagelBasket.get(0).getName());
+
     }
 }
