c48ed79d285c9386ec98c04ead7ff3dc83a1d0f3
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-18 15:55:35 +0100

    Changed basket code to work with current inventory and product code. Updated test to match the new code.

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -3,42 +3,51 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Basket {
-    ArrayList<String> bagelBasket;
+    ArrayList<Product> itemBasket;
     int capacity;
 
-    public Basket(){
-        this.capacity = 6;
-        this.bagelBasket = new ArrayList<>(capacity);
+    public ArrayList<Product> getItemBasket() {
+        return itemBasket;
     }
 
-    public boolean isEmpty(){
-        return bagelBasket.isEmpty();
+    public Basket(int capacity){
+        this.capacity = capacity;
+        this.itemBasket = new ArrayList<>();
     }
+
     public boolean basketIsFull(){
-        return bagelBasket.size() >= changeCapacity(capacity);
+        return itemBasket.size() >= changeCapacity(capacity);
     }
 
-    public String addBagel(String bagel){
-        if (bagel == null && isEmpty()){
+    public String addItem(Product item){
+        if (item == null && itemBasket.isEmpty()){
             return "Basket is empty";
         }
         else if (basketIsFull()){
-            return "The Basket is full";
+            return "Can't add anymore, basket is full";
         }
-        bagelBasket.add(bagel);
-        return bagel + " added to your basket";
+        itemBasket.add(item);
+        return "Item has been added to your basket";
     }
 
-    public String removeBagel(String bagel){
-        if (!bagelBasket.contains(bagel)){
+    public String removeItem(Product item){
+        if (!itemBasket.contains(item)){
             return "Can not remove non-existing item.";
         }
-        bagelBasket.remove(bagel);
-        return bagel + " removed from your basket";
+        itemBasket.remove(item);
+        return "Item has been removed from your basket";
     }
 
     public int changeCapacity(int newCapacity){
         this.capacity = newCapacity;
         return newCapacity;
+    }
+
+    public double calculateTotalCost(){
+        double totalCost = 0;
+        for (Product item : itemBasket){
+            totalCost += item.getPrice();
+        }
+        return totalCost;
     }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,4 +1,64 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BasketTest {
+    private Basket basket;
+    private Bagel bagel;
+    private Coffee coffee;
+    private Filling filling;
+    @BeforeEach
+    void setUp(){
+        basket = new Basket(3);
+        bagel = new Bagel("BGLO","Onion",0.49);
+        coffee = new Coffee("COFB","Black",0.99);
+        filling = new Filling("FILB","Bacon",0.12);
+    }
+    @Test
+    public void testAddNoItemIntoBasket(){
+        Assertions.assertEquals("Basket is empty",basket.addItem(null));
+    }
+    @Test
+    public void testAddItem(){
+        basket.addItem(bagel);
+        Assertions.assertEquals(1, basket.getItemBasket().size());
+
+        basket.addItem(coffee);
+        Assertions.assertEquals(2, basket.getItemBasket().size());
+
+        basket.addItem(filling);
+        Assertions.assertEquals(3, basket.getItemBasket().size());
+    }
+
+    @Test
+    public void testAddItemIfBasketIsFull(){
+        basket.addItem(bagel);
+        basket.addItem(bagel);
+        basket.addItem(bagel);
+        Assertions.assertEquals("Can't add anymore, basket is full", basket.addItem(bagel));
+    }
+
+    @Test
+    public void testRemoveItemFromBasket(){
+        basket.addItem(bagel);
+        basket.addItem(coffee);
+        Assertions.assertEquals("Item has been removed from your basket", basket.removeItem(bagel));
+    }
+
+    @Test
+    public void testRemoveNonExistingItem(){
+        basket.addItem(bagel);
+        Assertions.assertEquals("Can not remove non-existing item.", basket.removeItem(coffee));
+    }
+
+    @Test
+    public void testCalculateTotalCost(){
+        basket.addItem(bagel);
+        basket.addItem(coffee);
+        basket.addItem(filling);
+        Assertions.assertEquals(1.6, basket.calculateTotalCost());
+    }
+
 }
