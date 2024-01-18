801e00a66f556cc8bc3abc8694b973dea772b745
hanna <hannaphamm1007@gmail.com>
2024-01-18 21:25:32 +0100

    add function 8

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -4,23 +4,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Basket {
-    ArrayList<Product> basket;
-    int size;
-    public Basket(int size){
+    private ArrayList<Product> basket;
+    private int size;
+     Inventory inventoryProduct;
+    public Basket( Inventory inventoryProduct,int size ){
         this.basket = new ArrayList<>();
         this.size = size;
+        this.inventoryProduct = inventoryProduct;
     }
-    public boolean add(Product item){
-       if(this.basket.contains(item)){
-           System.out.println(item +" already added!");
-           return false;
-       }
-       this.basket.add(item);
-       System.out.println(item + " added successfully!");
-       return true;
 
+    public boolean add(Bagel item) {
+        if (isFull()) {
+            return false;
+        } else if (this.basket.contains(item)) {
+            System.out.println(item + " already added!");
+            return false;
+        }
+        this.basket.add(item);
+        System.out.println(item + " added successfully!");
+        return true;
     }
-    public String remove(Product item){
+    public String remove(Bagel item){
         if(this.basket.isEmpty()){
             return "Basket is empty";
         }
@@ -36,6 +40,7 @@ public class Basket {
             return true;
         }
         return false;
+
     }
     public String changeCapacity(int newCapacity){
         this.basket.ensureCapacity(newCapacity);
@@ -52,5 +57,45 @@ public class Basket {
     public double getItemCost(Product item){
         return item.getPrice();
     }
+    public String addingFillingWhenBagelInBasket( Filling filling) {
+        boolean bagelInBasket = false;
+        for(Product item : this.basket){
+            if(item instanceof Bagel){
+                bagelInBasket = true;
+                break;
+            }
+        }
+        if(bagelInBasket){
+            this.basket.add(filling);
+            return filling + " is added";
+        }else {
+            return "Please select a bagel before adding filling";
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
+        Basket myBasket = new Basket(inventory,2);
+        Bagel bagel1 = new Bagel("BGLO",0.49, "Bagel", "Onion" );
+        Bagel bagel2 = new Bagel("BGLP", 0.39, "Bagel", "Plain");
+        Filling filling1 = new Filling("FILB",0.12, "Filling", "Bacon");
+        Filling filling2 = new Filling("FILC",0.12, "Filling", "Cheese");
+
+        myBasket.add(bagel1);
+        myBasket.add(bagel2);
+
+        System.out.println(myBasket.addingFillingWhenBagelInBasket(filling1));
+        System.out.println(myBasket.addingFillingWhenBagelInBasket(filling2));
+
+
+    }
+
+
+
+
+
+
 
 }
