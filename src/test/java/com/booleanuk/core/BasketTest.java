1529dcb997e168359cf7b98433c6a1871873f0a8
Sebastian <sebastian.thunberg@hotmail.se>
2024-01-18 12:57:05 +0100

    updated basketTest

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -33,7 +33,7 @@ class BasketTest {
 		Assertions.assertTrue(basket.inBasket("BGLP"));
 		Assertions.assertFalse(basket.inBasket("BGLO"));
 		try {
-			basket.removeItem("BGLP");
+			basket.removeItem(0);
 		} catch (NotInBasketException e) {
 			throw new RuntimeException(e);
 		}
@@ -56,15 +56,16 @@ class BasketTest {
 		Assertions.assertTrue(basket.isFull());
 
 		try {
-			basket.removeItem("BGLP");
+			basket.removeItem(0);
 		} catch (NotInBasketException e) {
 			throw new RuntimeException(e);
 		}
 		Assertions.assertFalse(basket.isFull());
 
 	}
+
 	@Test
-	public void inBasketTest(){
+	public void inBasketTest() {
 		Inventory inventory = new Inventory();
 		Basket basket = new Basket(inventory);
 		Assertions.assertFalse(basket.inBasket("BGLP"));
@@ -74,7 +75,72 @@ class BasketTest {
 			throw new RuntimeException(e);
 		}
 		Assertions.assertTrue(basket.inBasket("BGLP"));
-
 	}
 
+	@Test
+	public void showFillingsTest() {
+		Inventory inventory = new Inventory();
+		Basket basket = new Basket(inventory);
+		Assertions.assertEquals("Bacon:\t0.12\nEgg:\t0.12\nCheese:\t0.12\nCream Cheese:\t0.12\nSmoked Salmon:\t0.12\nHam:\t0.12", basket.showFillings());
+	}
+
+	@Test
+	public void setSizeTest() {
+		Inventory inventory = new Inventory();
+		Basket basket = new Basket(inventory);
+		Assertions.assertEquals(3, basket.getSize());
+		try {
+			basket.setSize(2);
+		} catch (UnableToChangeBasketSizeException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			basket.addItem("BGLO");
+			basket.addItem("BGLP");
+		} catch (NotInInventoryException e) {
+			throw new RuntimeException(e);
+		}
+		Assertions.assertThrows(UnableToChangeBasketSizeException.class, () -> {
+			basket.setSize(1);
+		});
+	}
+
+	@Test
+	public void getTotalCostTest() {
+		Inventory inventory = new Inventory();
+		Basket basket = new Basket(inventory);
+		try {
+			basket.addItem("BGLP");
+		} catch (NotInInventoryException e) {
+			throw new RuntimeException(e);
+		}
+		Assertions.assertEquals(0.39,basket.getTotalCost());
+	}
+	@Test
+	public void addFillingTest(){
+		Inventory inventory = new Inventory();
+		Basket basket = new Basket(inventory);
+		try {
+			basket.addItem("BGLP");
+			basket.addExtra(0,"FILS");
+		} catch (NotInInventoryException e) {
+			throw new RuntimeException(e);
+		}
+
+
+
+	}
+	@Test
+	public void removeFillingTest(){
+		Inventory inventory = new Inventory();
+		Basket basket = new Basket(inventory);
+		try {
+			basket.addItem("BGLP");
+			basket.addExtra(0,"FILS");
+		} catch (NotInInventoryException e) {
+			throw new RuntimeException(e);
+		}
+		basket.removeExtra(0,"FILS");
+
+	}
 }
