44631f0fa445af3aca300b443dd4d88a3fb65df9
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 14:34:59 +0100

    tests for Filling created

--- src/test/java/com/booleanuk/core/FillingTest.java
+++ src/test/java/com/booleanuk/core/FillingTest.java
@@ -1,7 +1,46 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FillingTest {
 
+    @Test
+    public void testGetName(){
+        Bagel bacon = new Bagel("BGLP");
+        Bagel egg = new Bagel("BGLO");
+        Bagel cheese = new Bagel("BGLE");
+
+
+        Assertions.assertEquals("Bagel", bacon.getName());
+        Assertions.assertEquals("Bagel", egg.getName());
+        Assertions.assertEquals("Bagel", cheese.getName());
+
+    }
+
+    @Test
+    public void testGetVariant(){
+        Bagel bacon = new Bagel("BGLP");
+        Bagel egg = new Bagel("BGLO");
+        Bagel cheese = new Bagel("BGLE");
+
+        Assertions.assertEquals("Bagel", bacon.getVariant());
+        Assertions.assertEquals("Bagel", egg.getVariant());
+        Assertions.assertEquals("Bagel", cheese.getVariant());
+    }
+
+    @Test
+    public void testGetPrice(){
+        Bagel bacon = new Bagel("BGLP");
+        Bagel egg = new Bagel("BGLO");
+        Bagel cheese = new Bagel("BGLE");
+
+        Assertions.assertEquals(0.12, bacon.getPrice());
+        Assertions.assertEquals(0.12, egg.getPrice());
+        Assertions.assertEquals(0.12, cheese.getPrice());
+    }
 }
