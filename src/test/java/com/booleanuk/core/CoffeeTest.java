ab3dd1a62348dfff255c05454ab0cebb9788dc1a
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 14:41:18 +0100

    Tests for coffee written

--- src/test/java/com/booleanuk/core/CoffeeTest.java
+++ src/test/java/com/booleanuk/core/CoffeeTest.java
@@ -1,7 +1,42 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoffeeTest {
 
+    @Test
+    public void testGetName(){
+        Coffee black = new Coffee("COFB");
+        Coffee white = new Coffee("COFW");
+        Coffee latte = new Coffee("COFL");
+
+        Assertions.assertEquals("Coffee", black.getName());
+        Assertions.assertEquals("Coffee", white.getName());
+        Assertions.assertEquals("Coffee", latte.getName());
+    }
+
+    @Test
+    public void testGetVariant(){
+        Coffee black = new Coffee("COFB");
+        Coffee white = new Coffee("COFW");
+        Coffee latte = new Coffee("COFL");
+
+        Assertions.assertEquals("Black", black.getVariant());
+        Assertions.assertEquals("White", white.getVariant());
+        Assertions.assertEquals("Latte", latte.getVariant());
+    }
+
+    @Test
+    public void testGetPrice(){
+        Coffee black = new Coffee("COFB");
+        Coffee white = new Coffee("COFW");
+        Coffee latte = new Coffee("COFL");
+
+        Assertions.assertEquals(0.99, black.getPrice());
+        Assertions.assertEquals(1.19, white.getPrice());
+        Assertions.assertEquals(1.29, latte.getPrice());
+    }
 }
