1c8c5ee3f15fa1c6a5a5a5cf747b0d8d7585dfa0
Sebastian <sebastian.thunberg@hotmail.se>
2024-01-18 12:58:10 +0100

    Updated bagel and bagelTest. moved functionality to Basket

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -2,14 +2,12 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 
-public class Bagel extends Item{
-	String id;
-	String name;
-	double price;
-	ArrayList<Filling> fillings;
+public class Bagel extends Item {
+	private String id;
+	private String name;
+	private double price;
 
 	public Bagel(String id, String name, double price) {
 		super(id, name, price);
 	}
-
 }
