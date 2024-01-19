d5a109fe88a06a3c83c8fc8d61cf238d30540991
Marit <marit.moe2@ntebb.no>
2024-01-19 13:01:56 +0100

    Test generateReceipt().

--- src/test/java/com/booleanuk/extension/ReceiptTest.java
+++ src/test/java/com/booleanuk/extension/ReceiptTest.java
@@ -1,10 +1,31 @@
 package com.booleanuk.extension;
 
+import com.booleanuk.core.Basket;
+import com.booleanuk.core.Item;
+import com.booleanuk.core.BobsBagelsShop;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 public class ReceiptTest {
     @Test
     void generateReceipt() {
+        HashMap<Item, Integer> testInventory = new HashMap<>();
+        Item bglo = new Item("BGLO","Bagel", "Onion", 0.49);
+        Item bglp = new Item("BGLP","Bagel", "Plain", 0.39);
+        testInventory.put(bglo, 10);
+        testInventory.put(bglp, 10);
+        BobsBagelsShop shop = new BobsBagelsShop(testInventory);
+        Basket basket = new Basket(shop, 3);
+        Receipt receipt = new Receipt(basket);
+        Assertions.assertEquals("Basket is empty.", receipt.generateReceipt());
+        basket.add(bglo);
+        Assertions.assertEquals("Header\nBGLO\nFooter", receipt.generateReceipt());
+        basket.add(bglo);
+        basket.add(bglp);
+        Assertions.assertEquals("Header\nBGLO\nBGLP\nFooter", receipt.generateReceipt());
     }
 
     @Test
