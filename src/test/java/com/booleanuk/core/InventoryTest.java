f99be80820b96b609682963cd5fa7f7f6037161d
Petter Karstensen <petterkarstensen@gmail.com>
2024-01-19 16:06:12 +0100

    Add small test for inventory

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,5 +1,26 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 public class InventoryTest {
+
+    private Inventory inventory;
+    @BeforeEach
+    public void setup(){
+        inventory = new Inventory("src/main/java/com/booleanuk/core/inventory.csv");
+    }
+    @Test
+    public void testInventoryNotNull(){
+        HashMap<String, Item> bagelInventory = inventory.getBagelInventory();
+        HashMap<String, Item> fillingInventory = inventory.getFillingInventory();
+        HashMap<String, Item> coffeeInventory = inventory.getCoffeeInventory();
+        Assertions.assertNotNull(coffeeInventory);
+        Assertions.assertNotNull(fillingInventory);
+        Assertions.assertNotNull(bagelInventory);
+    }
 
 }
