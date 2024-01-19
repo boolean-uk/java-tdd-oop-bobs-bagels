7e7bb6e1e030150f3c1cced7c665481fcac49f85
Petter Karstensen <petterkarstensen@gmail.com>
2024-01-19 11:00:42 +0100

    Red test for changeBasketCapacity

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -16,7 +16,38 @@ public class BasketTest {
 
         basket.removeItems(testItem, 1);
         Assertions.assertEquals(2.5, basket.getTotalCost());
-
     }
+
+    @Test
+    public void testItem(){
+        basket = new Basket(10);
+        testItem = new Item("Bagel", 2.5, "test");
+        basket.addItems(testItem, 2);
+
+        Assertions.assertEquals("Bagel", testItem.getType());
+        Assertions.assertEquals(2.5, testItem.getPrice());
+        Assertions.assertEquals("test", testItem.getVariant());
+    }
+
+    @Test
+    public void testBasketCapacity(){
+        basket = new Basket(5);
+        testItem = new Item("Bagel", 1, "test");
+        basket.addItems(testItem, 3);
+
+        Assertions.assertFalse(basket.addItems(testItem, 3));
+        Assertions.assertTrue(basket.addItems(testItem, 2));
+    }
+    @Test
+    public void changeBasketCapacity(){
+        basket = new Basket(5);
+        Assertions.assertTrue((basket.changeCapacity(10)));
+
+        testItem = new Item("Bagel", 1, "test");
+        basket.addItems(testItem, 10);
+        Assertions.assertFalse((basket.changeCapacity(9)));
+        Assertions.assertTrue((basket.changeCapacity(20)));
+    }
+
 
 }
