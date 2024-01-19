49a1cdf2969b29a5b65922dad7cc41aeec0a8025
Marit <marit.moe2@ntebb.no>
2024-01-19 10:30:17 +0100

    Added main to print out some output.

---
+++ src/main/java/com/booleanuk/core/Main.java
@@ -0,0 +1,8 @@
+package com.booleanuk.core;
+
+public class Main {
+    public static void main(String[] args) {
+        BobsBagelsShop shop = new BobsBagelsShop();
+        System.out.println(shop.showInventory());
+    }
+}
