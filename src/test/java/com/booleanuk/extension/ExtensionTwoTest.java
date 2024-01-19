78a41ee33ec19457cabd2fb1c5b29d566e908216
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-19 12:55:45 +0100

    EXTENSION 2: createReceiptForSeveralProductsTest added

--- src/test/java/com/booleanuk/extension/ExtensionTwoTest.java
+++ src/test/java/com/booleanuk/extension/ExtensionTwoTest.java
@@ -29,6 +29,33 @@ public class ExtensionTwoTest {
                         "\n        Thank you" +
                         "\n     for your order!";
 
+        Assertions.assertEquals(expected, receipt.toString());
+    }
+
+    @Test
+    public void createReceiptForSeveralProductsTest() {
+        Store store = new Store();
+        int basketId = store.createBasket();
+        for(int i=0; i < 16; i++) {
+            store.addBagelToBasket("plain", basketId);
+        }
+        Receipt receipt = store.createReceipt(basketId);
+
+        String expected =
+                "    ~~~ Bob's Bagels ~~~   " +
+                        "\n" +
+                        "\n   2021-03-16 21:38:44 " +
+                        "\n" +
+                        "\n----------------------------" +
+                        "\n" +
+                        "\nPlain Bagel        16  \u00A35.55" +
+                        "\n" +
+                        "\n----------------------------" +
+                        "\nTotal                  \u00A35.55" +
+                        "\n" +
+                        "\n        Thank you" +
+                        "\n     for your order!";
+
         System.out.println(expected);
         Assertions.assertEquals(expected, receipt.toString());
 
