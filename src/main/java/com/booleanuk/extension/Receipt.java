17349e6ac15d32e5f3fed0cd90fcdd3ef8811c18
Marit <marit.moe2@ntebb.no>
2024-01-19 13:21:54 +0100

    Pass initial test generateReceipt().

--- src/main/java/com/booleanuk/extension/Receipt.java
+++ src/main/java/com/booleanuk/extension/Receipt.java
@@ -1,27 +1,41 @@
 package com.booleanuk.extension;
 
 import com.booleanuk.core.Basket;
+import com.booleanuk.core.Item;
 
 import java.time.LocalDateTime;
 
 public class Receipt {
     private String receipt;
     private LocalDateTime dateAndTime;
+    private Basket basket;
 
     public Receipt(Basket basket) {
-
+        this.basket = basket;
+        this.dateAndTime = LocalDateTime.now();
     }
 
-    public String generateReceipt(Basket basket) {
-        return "";
+    public String generateReceipt() {
+        if (basket.getBasketContent().isEmpty()) {
+            return "Basket is empty.";
+        }
+        StringBuilder receipt = new StringBuilder();
+        receipt.append(createHeader());
+        for (Item item : basket.getBasketContent().keySet()) {
+            String itemRow = item.getSku() + "\n";
+            receipt.append(itemRow);
+        }
+        receipt.append(createFooter());
+        this.receipt = receipt.toString();
+        return receipt.toString();
     }
 
     public String createHeader() {
-        return "";
+        return "Header\n";
     }
 
     public String createFooter() {
-        return "";
+        return "Footer";
     }
 
     public boolean printReceipt() {
