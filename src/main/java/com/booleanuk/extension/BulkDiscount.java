b697985a0a921dfc99bc04775568313933345a98
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-19 13:17:34 +0100

    Implement BulkDiscount class

--- src/main/java/com/booleanuk/extension/BulkDiscount.java
+++ src/main/java/com/booleanuk/extension/BulkDiscount.java
@@ -1,5 +1,23 @@
 package com.booleanuk.extension;
 
 public class BulkDiscount {
+    private String sku;
+    private int number;
+    private double price;
 
+    public BulkDiscount(String sku, int number, double price) {
+        this.sku = sku;
+        this.number = number;
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
