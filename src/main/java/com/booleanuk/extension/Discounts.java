297a8e5cf240691a9e9f664544f77669be1d4d3d
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-19 13:22:49 +0100

    Implement Discounts class

--- src/main/java/com/booleanuk/extension/Discounts.java
+++ src/main/java/com/booleanuk/extension/Discounts.java
@@ -1,5 +1,24 @@
 package com.booleanuk.extension;
 
-public class Discounts {
+import com.booleanuk.core.Inventory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Discounts {
+    private List<BulkDiscount> bulkDiscounts;
+    private Inventory inventory;
+
+    public Discounts(Inventory inventory) {
+        this.inventory = inventory;
+        this.bulkDiscounts = new ArrayList<>();
+
+        this.bulkDiscounts.add(new BulkDiscount("BGLO", 6, 2.49));
+        this.bulkDiscounts.add(new BulkDiscount("BGLP", 12, 3.99));
+        this.bulkDiscounts.add(new BulkDiscount("BGLE", 6, 2.49));
+    }
+
+    public List<BulkDiscount> getBulkDiscounts() {
+        return bulkDiscounts;
+    }
 }
