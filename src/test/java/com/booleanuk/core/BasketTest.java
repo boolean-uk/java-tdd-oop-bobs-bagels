d01bc4236e4a7823254073dc6a1a198a6472aff6
Marit <marit.moe2@ntebb.no>
2024-01-22 13:53:14 +0100

    Moved extension tests to new test file in extension folder.

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -98,25 +98,6 @@ public class BasketTest {
     }
 
     @Test
-    public void discountPerItem() {
-        HashMap<Item, Integer> testInventory = new HashMap<>();
-        Item bglo = new Item("BGLO","Bagel", "Onion", 0.49);
-        testInventory.put(bglo, 50);
-        BobsBagelsShop shop = new BobsBagelsShop(testInventory);
-        Basket basket = new Basket(shop, 16);
-        basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo);
-
-        // 1 for 0.49, 6 for 2.29, 12 for 3.99
-        Assertions.assertEquals(3.27, basket.discountPerItem().get(bglo)[0]);
-        Assertions.assertEquals(0.65, basket.discountPerItem().get(bglo)[1]);
-
-        basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo);
-
-        Assertions.assertEquals(5.95, basket.discountPerItem().get(bglo)[0]);
-        Assertions.assertEquals(1.89, basket.discountPerItem().get(bglo)[1]);
-    }
-
-    @Test
     public void checkSize() {
         HashMap<Item, Integer> testInventory = new HashMap<>();
         Item bglo = new Item("BGLO","Bagel", "Onion", 0.49);
---
+++ src/test/java/com/booleanuk/extension/BasketTest.java
@@ -0,0 +1,31 @@
+package com.booleanuk.extension;
+
+import com.booleanuk.core.Basket;
+import com.booleanuk.core.Item;
+import com.booleanuk.core.BobsBagelsShop;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+public class BasketTest {
+
+    @Test
+    public void discountPerItem() {
+        HashMap<Item, Integer> testInventory = new HashMap<>();
+        Item bglo = new Item("BGLO","Bagel", "Onion", 0.49);
+        testInventory.put(bglo, 50);
+        BobsBagelsShop shop = new BobsBagelsShop(testInventory);
+        Basket basket = new Basket(shop, 16);
+        basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo);
+
+        // 1 for 0.49, 6 for 2.29, 12 for 3.99
+        Assertions.assertEquals(3.27, basket.discountPerItem().get(bglo)[0]);
+        Assertions.assertEquals(0.65, basket.discountPerItem().get(bglo)[1]);
+
+        basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo); basket.add(bglo);
+
+        Assertions.assertEquals(5.95, basket.discountPerItem().get(bglo)[0]);
+        Assertions.assertEquals(1.89, basket.discountPerItem().get(bglo)[1]);
+    }
+}
