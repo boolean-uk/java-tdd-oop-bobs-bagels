248423d8d9e3cb140b22ad9565cb65f259058375
Enock97 <enockladu@hotmail.com>
2024-01-22 13:19:55 +0100

    Cleaned up my code

--- src/main/java/com/booleanuk/extension/ExtensionExercise2.java
+++ src/main/java/com/booleanuk/extension/ExtensionExercise2.java
@@ -36,20 +36,6 @@ public class ExtensionExercise2 {
     public ExtensionExercise2() {
         inventoryList = new ArrayList<>();
         inventoryMap = new HashMap<>();
-        inventoryList.add(new Inventory("BGLO", 0.49, "Bagel", "Onion"));
-        inventoryList.add(new Inventory("BGLP", 0.39, "Bagel", "Plain"));
-        inventoryList.add(new Inventory("BGLE", 0.49, "Bagel", "Everything"));
-        inventoryList.add(new Inventory("BGLS", 0.49, "Bagel", "Sesame"));
-        inventoryList.add(new Inventory("COFB", 0.99, "Coffee", "Black"));
-        inventoryList.add(new Inventory("COFW", 1.19, "Coffee", "White"));
-        inventoryList.add(new Inventory("COFC", 1.29, "Coffee", "Cappuccino"));
-        inventoryList.add(new Inventory("COFL", 1.29, "Coffee", "Latte"));
-        inventoryList.add(new Inventory("FILB", 0.12, "Filling", "Bacon"));
-        inventoryList.add(new Inventory("FILE", 0.12, "Filling", "Egg"));
-        inventoryList.add(new Inventory("FILC", 0.12, "Filling", "Cheese"));
-        inventoryList.add(new Inventory("FILX", 0.12, "Filling", "Cream Cheese"));
-        inventoryList.add(new Inventory("FILS", 0.12, "Filling", "Smoked Salmon"));
-        inventoryList.add(new Inventory("FILH", 0.12, "Filling", "Ham"));
     }
 
     public String returnReceipt() {
