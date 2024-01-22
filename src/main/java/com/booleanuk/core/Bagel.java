a2e94b2d8153ae3c562563d823547b95ad57ddb5
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-22 17:38:32 +0100

    Test 1 implemented

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -1,6 +1,14 @@
 package com.booleanuk.core;
 
 public class Bagel {
-    public Bagel(String sesame, String number, int i) {
+    String name;
+    String productID;
+    String uniqueID;
+    double price;
+    public Bagel(String name, String productID, double price) {
+        this.name = name;
+        this.productID = productID;
+        this.uniqueID = "abc";  //If there are more than one e.g. sesame bagel, this ID can be used to navigate to the right bagel
+        this.price = price;
     }
 }
--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,10 +1,25 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Basket {
+    Inventory inventory;
+    HashMap<Bagel, ArrayList<Filling>> basket;
+    int maxCapacity = 5;
+    int currentIndex = 0;
+
     public Basket(Inventory inventory) {
+        this.inventory = inventory;
+        this.basket = new HashMap<>();
     }
 
     public boolean addBagel(Bagel bagel) {
-        return true;
+        if(currentIndex < maxCapacity) {
+            basket.put(bagel, new ArrayList<>());
+            currentIndex++;
+            return true;
+        }
+        return false;
     }
 }
