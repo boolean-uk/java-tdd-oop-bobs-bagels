a23ae4001d8f95f26c61defe07f0b3565c478d47
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-23 11:09:52 +0100

    Created method getPrice in Bagel with default return

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -11,4 +11,8 @@ public class Bagel {
         this.uniqueID = "abc";  //If there are more than one e.g. sesame bagel, this ID can be used to navigate to the right bagel
         this.price = price;
     }
+
+    public double getPrice() {
+        return 10.10;
+    }
 }
