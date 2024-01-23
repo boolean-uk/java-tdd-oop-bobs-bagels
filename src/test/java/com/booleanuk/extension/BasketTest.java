e8e224c61181ca00ce63dd2e3c3181e950fb6454
Marit <marit.moe2@ntebb.no>
2024-01-23 13:19:14 +0100

    Test for coffee and bagel offer.

--- src/test/java/com/booleanuk/extension/BasketTest.java
+++ src/test/java/com/booleanuk/extension/BasketTest.java
@@ -54,6 +54,33 @@ public class BasketTest {
     }
 
     @Test
+    public void discountForBlackCoffeeAndBagel() {
+        HashMap<Item, Integer> testInventory = new HashMap<>();
+        Item bglo = new Bagel("BGLO", "Onion", 0.49);
+        Item cofb = new Coffee("COFB", "Black", 0.99);
+        testInventory.put(bglo, 30);
+        testInventory.put(cofb, 30);
+        BobsBagelsShop shop = new BobsBagelsShop(testInventory);
+        Basket basket = new Basket(shop, 6);
+        basket.add(bglo); basket.add(bglo); basket.add(bglo);
+        basket.add(cofb);
+
+        // One coffee and one bagel are discounted.
+        Assertions.assertEquals(0.49+0.49+1.25*(1.0/3), basket.discountPerItem().get(bglo)[0]);
+        Assertions.assertEquals(0.49-1.25*(1.0/3), basket.discountPerItem().get(bglo)[1]);
+        Assertions.assertEquals(1.25*(2.0/3), basket.discountPerItem().get(cofb)[0]);
+        Assertions.assertEquals(0.99-1.25*(2.0/3), basket.discountPerItem().get(cofb)[1]);
+
+        basket.add(cofb);
+
+        // Two coffees and two bagels are discounted.
+        Assertions.assertEquals(0.49+2*(1.25*(1.0/3)), basket.discountPerItem().get(bglo)[0]);
+        Assertions.assertEquals(0.49*2-2*(1.25*(1.0/3)), basket.discountPerItem().get(bglo)[1]);
+        Assertions.assertEquals((1.25*(2.0/3))*2, basket.discountPerItem().get(cofb)[0]);
+        Assertions.assertEquals((0.99-1.25*(2.0/3))*2, basket.discountPerItem().get(cofb)[1]);
+    }
+
+    @Test
     public void totalCostWithDiscount() {
         HashMap<Item, Integer> testInventory = new HashMap<>();
         Item bglo = new Bagel("BGLO", "Onion", 0.49);
