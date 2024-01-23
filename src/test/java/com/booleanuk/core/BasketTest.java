7685afc2c40cfc8579fee0afef3a3d1a7e58b5a6
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-23 14:55:36 +0100

    Broke out user story 9 to be in separate Test Class

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -109,50 +109,7 @@ public class BasketTest {
         Assertions.assertFalse(addedFilling);
     }
 
-    //User story 9
-    @Test
-    public void assertPriceOfEveryFilling() {
-        Inventory inventory = new Inventory();
-
-        ArrayList<Filling> returnedFillings = inventory.getAllFillings();
-
-        int allTested = 0;
-
-        for(Filling filling : returnedFillings) {
-            switch (filling.getName()) {
-                case "Bacon":
-                    Assertions.assertEquals(filling.getPrice(), 0.12);
-                    allTested++;
-                    break;
-                case "Egg":
-                    Assertions.assertEquals(filling.getPrice(), 0.12);
-                    allTested++;
-                    break;
-                case "Cheese":
-                    Assertions.assertEquals(filling.getPrice(), 0.12);
-                    allTested++;
-                    break;
-                case "Cream Cheese":
-                    Assertions.assertEquals(filling.getPrice(), 0.12);
-                    allTested++;
-                    break;
-                case "Smoked Salmon":
-                    Assertions.assertEquals(filling.getPrice(), 0.12);
-                    allTested++;
-                    break;
-                case "Ham":
-                    Assertions.assertEquals(filling.getPrice(), 0.12);
-                    allTested++;
-                    break;
-                default:
-                    Assertions.assertEquals(1, 2);
-                    break;
-            }
-        }
-        if(allTested != 6) {
-            Assertions.assertEquals(1, 2);
-        }
-    }
+    //User story 9 is in InventoryTest
 
     //User story 10
     @Test
