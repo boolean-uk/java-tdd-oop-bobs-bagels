4dd70f81d0a96608b9acdec5434fdeb28d3808bf
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-24 14:59:25 +0100

    Implement user function in bobsBagels

--- src/main/java/com/booleanuk/core/BobsBagels.java
+++ src/main/java/com/booleanuk/core/BobsBagels.java
@@ -1,18 +1,23 @@
 package com.booleanuk.core;
 
 import com.booleanuk.extension.Discounts;
+import com.booleanuk.extension.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BobsBagels {
     private Map<String, Basket> baskets;
+    private List<User> users;
     private Inventory inventory;
     private Discounts discounts;
     private Checkout checkout;
 
     public BobsBagels() {
         this.baskets = new HashMap<>();
+        this.users = new ArrayList<>();
         this.inventory = new Inventory();
         this.discounts = new Discounts(inventory);
         this.checkout = new Checkout(inventory, discounts);
@@ -25,6 +30,15 @@ public class BobsBagels {
     public Basket getBasket(String phoneNumber) {
         this.baskets.putIfAbsent(phoneNumber, new Basket(this.inventory, this.checkout));
         return this.baskets.get(phoneNumber);
+    }
+
+    public User getUser(String phoneNumber) {
+        List<User> l = this.users.stream().filter(x->x.getPhoneNumber().equals(phoneNumber)).toList();
+        if (l.isEmpty()) {
+            this.users.add(new User(phoneNumber));
+        }
+        l = this.users.stream().filter(x->x.getPhoneNumber().equals(phoneNumber)).toList();
+        return l.get(0);
     }
 
     public String setBasketCapacity(int cap) {
