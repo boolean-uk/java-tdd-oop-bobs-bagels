9b3c2a4fef9657fca62663f16399c36c7487c90e
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-24 13:04:29 +0100

    Add filling to Bagel class

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -1,9 +1,18 @@
 package com.booleanuk.core;
 
 public class Bagel extends Product{
+    Filling filling;
 
     public Bagel(String sku, double price, String name, String variant) {
         super(sku, price, name, variant);
+        this.filling = null;
+    }
 
+    public Filling getFilling() {
+        return filling;
+    }
+
+    public void setFilling(Filling filling) {
+        this.filling = filling;
     }
 }
