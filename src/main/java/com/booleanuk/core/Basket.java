d4b9195911979149c50a63ae98ca1bad0ce4aed1
Alexander <alexander.ellnestam@gmail.com>
2024-01-28 13:00:37 +0100

    Add multiple items to basket

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,13 +1,18 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Basket {
 
+    private List<String> items = new ArrayList<>();
+
     public int numberOfItems() {
-        return 1;
+        return items.size();
     }
 
-    public void add(String bagel) {
+    public void add(String item) {
+        items.add(item);
     }
 }
