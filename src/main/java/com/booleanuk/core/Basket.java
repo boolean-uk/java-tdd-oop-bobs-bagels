51e845d5b4c4a0298c01a4d7058c3f41a7ad6fe7
Alexander <alexander.ellnestam@gmail.com>
2024-01-28 12:54:10 +0100

    First test fails

---
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -0,0 +1,10 @@
+package com.booleanuk.core;
+
+public class Basket {
+    public int numberOfItems() {
+        return 0;
+    }
+
+    public void add(String bagel) {
+    }
+}
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 public class BasketTest {
+    @Test
+    public void addBagelToBasket(){
+       Basket basket = new Basket();
+       basket.add("Bagel");
+        Assertions.assertEquals(1, basket.numberOfItems());
+    }
 }
