e24237df7e2f1aa13e7f97203cd1d6b2e8a0ece3
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-29 00:05:51 +0100

    Green: Extend basket class, set discount, override get totalprice function to include discounts

---
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -0,0 +1,52 @@
+package com.booleanuk.extension;
+
+import com.booleanuk.core.Basket;
+import com.booleanuk.core.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Extension {
+    public static class ExtendedBasket extends Basket {
+        private final Map<Item, DiscountInfo> discounts;
+        public ExtendedBasket(int capacity) {
+            super(capacity);
+            discounts = new HashMap<>();
+        }
+
+        public void setDiscount(Item item, int quantity, double price) {
+            discounts.put(item, new DiscountInfo(quantity, price));
+        }
+
+        private record DiscountInfo(int quantity, double price) {
+        }
+
+        @Override
+        public double getTotalPrice() {
+            double totalPrice = 0;
+
+            for (Map.Entry<Item, Integer> entry : items.entrySet()) {
+                Item item = entry.getKey();
+                int quantity = entry.getValue();
+
+                if (discounts.containsKey(item)) {
+                    DiscountInfo discountInfo = discounts.get(item);
+                    int discountedQuantity = Math.min(quantity, discountInfo.quantity());
+                    int remainingQuantity = quantity - discountedQuantity;
+                    double discountedPrice = discountInfo.price();
+                    double regularPrice = item.getPrice();
+                    double discountedTotal = discountedPrice * discountedQuantity;
+                    double regularTotal = regularPrice * remainingQuantity;
+                    totalPrice += discountedTotal + regularTotal;
+                } else {
+                    double regularTotal = item.getPrice() * quantity;
+                    totalPrice += regularTotal;
+                }
+            }
+            return totalPrice;
+        }
+
+
+    }
+
+}
