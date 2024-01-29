6ff3b36ac353a1f62da83d50adc450302d0cccf4
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-29 03:44:31 +0100

    Green: Add discounted invoices, edited logic for new class, no new helper methods needed.

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -11,7 +11,8 @@ import java.util.Map;
 public class Extension {
 
     public static class ExtendedBasket extends Basket {
-        private final Map<Item, DiscountInfo> discounts;
+        private static Map<Item, DiscountInfo> discounts;
+
         public ExtendedBasket(int capacity) {
             super(capacity);
             discounts = new HashMap<>();
@@ -32,7 +33,7 @@ public class Extension {
                 Item item = entry.getKey();
                 int quantity = entry.getValue();
 
-                if (discounts.containsKey(item) &&  quantity >= discounts.get(item).quantity() && quantity <= discounts.get(item).quantity()*2 ) {
+                if (discounts.containsKey(item) && quantity >= discounts.get(item).quantity() && quantity <= discounts.get(item).quantity() * 2) {
                     DiscountInfo discountInfo = discounts.get(item);
                     int discountedQuantity = Math.min(quantity, discountInfo.quantity());
                     int remainingQuantity = quantity - discountedQuantity;
@@ -50,33 +51,72 @@ public class Extension {
         }
 
         public static void printReceipt(ExtendedBasket basket) {
+            System.out.println("");
             System.out.println("    ~~~" + " Bob's Bagels " + "~~~    ");
             System.out.println("");
             System.out.println("    " + new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss").format(new Date()));
             System.out.println("");
-            System.out.println("------------------------------");
+            System.out.println("-----------------------------");
             System.out.println("");
 
-            double total = 0;
+            double total = basket.getTotalPrice();
 
             for (Map.Entry<Item, Integer> entry : basket.items.entrySet()) {
                 Item item = entry.getKey();
                 int quantity = entry.getValue();
                 double itemTotal = item.getPrice() * quantity;
-
-                System.out.printf("     " + "%-5s %d    " + "\u00A3%.2f%n", item.getName(), quantity, itemTotal);
-                total += itemTotal;
+                System.out.printf("%-10s %3d    \u00A3%6.2f%n", item.getName(), quantity, itemTotal);
             }
 
             System.out.println("");
+            System.out.println("-----------------------------");
             System.out.println("");
-            System.out.println("----------------------------");
-            System.out.println("");
-            System.out.printf("Total                 £%.2f%n", total);
+
+            System.out.printf("%-18s \u00A3%6.2f%n", "Total", total);
             System.out.println("        Thank you");
-            System.out.println("      for your order!");
+            System.out.println("");
         }
 
+        public static void printReceiptDiscount(ExtendedBasket basket) {
+            System.out.println("");
+            System.out.println("    ~~~" + " Bob's Bagels " + "~~~    ");
+            System.out.println("");
+            System.out.println("    " + new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss").format(new Date()));
+            System.out.println("");
+            System.out.println("-----------------------------");
+            System.out.println("");
+
+            double total = basket.getTotalPrice();
+            double totalSavings = 0;
+
+            for (Map.Entry<Item, Integer> entry : basket.items.entrySet()) {
+                Item item = entry.getKey();
+                int quantity = entry.getValue();
+                double itemTotal = item.getPrice() * quantity;
+                double itemSavings = 0;
+
+                if (discounts.containsKey(item) && quantity >= discounts.get(item).quantity() && quantity <= discounts.get(item).quantity() * 2) {
+                    double ItemTotalDiscount = discounts.get(item).price() + ((quantity - discounts.get(item).quantity()) * item.getPrice());
+                    itemSavings = (itemTotal - ItemTotalDiscount);
+                    itemTotal = ItemTotalDiscount;
+                    totalSavings =+ itemSavings;
+                }
+                System.out.printf("%-14s %3d    \u00A3%6.2f%n", item.getName(), quantity, itemTotal);
+                if (itemSavings > 0) {
+                    System.out.printf("%20s (-\u00A3%.2f)%n", "", itemSavings);
+                }
+            }
+
+            System.out.println("");
+            System.out.println("-----------------------------");
+            System.out.println("");
+
+            System.out.printf("%-21s \u00A3%6.2f%n", "Total", total);
+            System.out.printf("%-18s \u00A3%6.2f%n", "You saved a total of ", totalSavings);
+            System.out.println("       on this shop");
+            System.out.println("        Thank you");
+            System.out.println("");
+        }
 
     }
 
