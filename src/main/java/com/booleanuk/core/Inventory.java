2b59322c5bed2157923e8e930e7f4edf1e3d90d5
Alexander <alexander.ellnestam@gmail.com>
2024-02-10 19:09:49 +0100

    test pass

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -8,38 +8,38 @@ public class Inventory {
     private List<Filling> fillings;
     private List<Bagel> bagels;
 
-    public Inventory(){
+    public Inventory() {
         bagels = new ArrayList<>();
         fillings = new ArrayList<>();
     }
 
-    public boolean isItemExisting(String sku){
-        for (Bagel bagel : bagels){
-            if(bagel.getSku().equals(sku)){
+    public boolean isItemExisting(String sku) {
+        for (Bagel bagel : bagels) {
+            if (bagel.getSku().equals(sku)) {
                 return true;
             }
         }
-        for (Filling filling : fillings){
-            if (filling.getSku().equals(sku)){
+        for (Filling filling : fillings) {
+            if (filling.getSku().equals(sku)) {
                 return true;
             }
         }
         return false;
     }
 
-    public void addFilling(Filling filling){
+    public void addFilling(Filling filling) {
         fillings.add(filling);
         System.out.println("Added filling");
     }
 
-    public void removeFilling(Filling filling){
+    public void removeFilling(Filling filling) {
         fillings.remove(filling);
         System.out.println("Removed filling");
     }
 
-    public Bagel getBagelBySku(String sku){
-        for (Bagel bagel : bagels){
-            if(bagel.getSku().equals(sku)){
+    public Bagel getBagelBySku(String sku) {
+        for (Bagel bagel : bagels) {
+            if (bagel.getSku().equals(sku)) {
                 return bagel;
             }
         }
@@ -47,8 +47,8 @@ public class Inventory {
     }
 
     public Filling getFillingBySku(String sku) {
-        for (Filling filling : fillings){
-            if (filling.getSku().equals(sku)){
+        for (Filling filling : fillings) {
+            if (filling.getSku().equals(sku)) {
                 return filling;
             }
         }
