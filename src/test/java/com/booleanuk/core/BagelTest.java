54cafc6a2ed79768631882a8753340fb06dfbb70
Alexander <alexander.ellnestam@gmail.com>
2024-02-10 17:34:10 +0100

    New tests

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -11,4 +11,22 @@ public class BagelTest {
        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
        assertEquals("BGLO", bagel.getSku());
    }
+
+    @Test
+    public void testGetPrice(){
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        assertEquals(0.49, bagel.getPrice());
+    }
+
+    @Test
+    public void testGetVariant(){
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        assertEquals("Bagel", bagel.getVariant());
+    }
+
+    @Test
+    public void testGetName(){
+        Bagel bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        assertEquals("Onion", bagel.getName());
+    }
 }
