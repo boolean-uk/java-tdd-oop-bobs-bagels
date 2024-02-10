34a387c483d16df75182c47b79dca38dcbc60ed2
Alexander <alexander.ellnestam@gmail.com>
2024-02-10 18:51:54 +0100

    Added test

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InventoryTest {
+
+    @Test
+    public void testAddFilling(){
+        Inventory inventory = new Inventory();
+        Filling filling = new Filling("FILB", 0.12, "Bacon");
+        inventory.addFilling(filling);
+        Assertions.assertTrue(inventory.isItemExisting("FILB"));
+    }
 }
