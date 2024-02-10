43715c0c87566ce3c1e11568daf83d0c7b60043e
Alexander <alexander.ellnestam@gmail.com>
2024-02-10 18:56:48 +0100

    Test pass

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -1,4 +1,34 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+
+    private List<Filling> fillings;
+    private List<Bagel> bagels;
+
+    public Inventory(){
+        bagels = new ArrayList<>();
+        fillings = new ArrayList<>();
+    }
+
+    public boolean isItemExisting(String sku){
+        for (Bagel bagel : bagels){
+            if(bagel.getSku().equals(sku)){
+                return true;
+            }
+        }
+        for (Filling filling : fillings){
+            if (filling.getSku().equals(sku)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addFilling(Filling filling){
+        fillings.add(filling);
+        System.out.println("Added filling");
+    }
 }
