5ed357ed07af8f5d70f478c00dccd817131a013a
Sebastian <sebgro@kth.se>
2024-08-16 14:57:51 +0200

    First test testing for capacity pass

--- src/main/java/com/booleanuk/core/Store.java
+++ src/main/java/com/booleanuk/core/Store.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
 public class Store {
+
+   private int capacity = 10;
+
+    public void updateCapacity(int capacity) {
+        this.capacity += capacity;
+
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
--- src/test/java/com/booleanuk/core/MainTest.java
+++ src/test/java/com/booleanuk/core/MainTest.java
@@ -4,14 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
-
+    Store store = new Store();
     MainTest() {
-        Store store = new Store();
+
     }
 
     @Test
     public void testUpdateCapacity() {
-        Assertions.assertEquals(11, store.updateCapacity(1));
+        store.updateCapacity(1);
+        Assertions.assertEquals(11, store.getCapacity());
     }
+
+
 
 }
