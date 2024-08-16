b75c058d5c5c442932691298af4624aafd0c55eb
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-16 13:57:57 +0200

    Making source

---
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -0,0 +1,19 @@
+package com.booleanuk.core;
+
+import java.util.ArrayList;
+
+public class Basket {
+    public ArrayList<String> listOfBasket;
+    public int capacity;
+
+
+    public Basket(int capacity) {
+        this.listOfBasket = new ArrayList<>();
+        this.capacity = capacity;
+
+    }
+
+
+}
+
+
