791214fc5044526c17451792bf42f3f897e934a0
sebgro98 <sebgro@kth.se>
2024-08-16 19:14:32 +0200

    Test for making sure that you cannot add items that are not in inventory is passing

--- src/main/java/com/booleanuk/core/Order.java
+++ src/main/java/com/booleanuk/core/Order.java
@@ -4,11 +4,16 @@ import java.util.HashMap;
 
 public class Order {
    HashMap<String, Integer> basket = new HashMap<>();
-
+    Store store = new Store();
     public void addProduct (Product product) {
-        if (basket.isEmpty() ||!basket.containsKey(product.getSKU())) {
-            {basket.put(product.getSKU(), 1);}
-        } else basket.put(product.getSKU(), basket.get(product.getSKU()) + 1);
+        for(int i = 0; i < store.inventory.length; i++) {
+            if(store.inventory[i].getSKU().equals(product.getSKU())) {
+                if (basket.isEmpty() ||!basket.containsKey(product.getSKU())) {
+                    {basket.put(product.getSKU(), 1);}
+                } else basket.put(product.getSKU(), basket.get(product.getSKU()) + 1);
+            }
+        }
+
     }
 
 }
