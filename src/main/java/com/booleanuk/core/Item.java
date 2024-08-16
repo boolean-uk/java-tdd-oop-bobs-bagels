e628b243f8e404e0f9b2c3712b6f7dce8f21b524
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-16 15:12:19 +0200

    You can now create a bagel and get the SKU of the bagel

--- src/main/java/com/booleanuk/core/Item.java
+++ src/main/java/com/booleanuk/core/Item.java
@@ -1,4 +1,19 @@
 package com.booleanuk.core;
 
 public class Item {
+    String SKU;
+    double price;
+    String type;
+    String name;
+
+    public Item(String SKU, double price, String type, String name) {
+        this.SKU = SKU;
+        this.price = price;
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getSKU() {
+        return this.SKU;
+    }
 }
