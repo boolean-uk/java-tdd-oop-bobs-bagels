0e73b75391dedb5fd387e81c317d37ceacd6f0e5
Connor Stokes <connorjordanstokes@gmail.com>
2024-08-16 17:44:18 +0100

    First inventoryTest built and fails

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,5 +1,6 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -7,8 +8,13 @@ import java.util.Map;
 
 public class InventoryTest {
     @Test
-    public void testGetItemDetails(){
+    public void testAddInventoryItem(){
         Inventory inventory = new Inventory();
+        InventoryItem item =  new InventoryItem("BGLO",0.49, "Bagel","Onion");
+        inventory.addInventoryItem(item);
+        HashMap<String, InventoryItem> map = inventory.getInventory();
+        HashMap<String, InventoryItem> expectedMap = new HashMap<String, InventoryItem>("BGLO",item);
+        Assertions.assertEquals(map.get("BGLO"), expectedMap);
 
     }
 }
