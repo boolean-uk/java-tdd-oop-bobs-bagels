948d041166fc602c5fe5ef361619568e2381e83c
Dag Andreas <daanfofo@gmail.com>
2024-08-16 14:05:36 +0200

    created bagel and bageltype

---
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+public class Bagel extends Item{
+    // fillings
+    // type
+
+    public Bagel(double price, String SKA, String name, BagelType variant) {
+        super(price, SKA, name);
+        this.variant = type;
+
+    }
+
+
+}
