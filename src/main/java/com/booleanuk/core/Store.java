bbc77fa51ff8b204a134eb89c198bb2d9aa97213
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-16 16:22:24 +0200

    testCreateStore() if inventory exists pass

--- src/main/java/com/booleanuk/core/Store.java
+++ src/main/java/com/booleanuk/core/Store.java
@@ -2,12 +2,18 @@ package com.booleanuk.core;
 
 public class Store {
     private final String name;
+    private final Inventory inventory;
 
     public Store(String name) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be null");
         }
         this.name = name;
+        this.inventory = new Inventory();
+    }
+
+    public Inventory getInventory() {
+        return inventory;
     }
 
     public String getName() {
