aaac755ca84d30b492d8d73be00ea64185d1e25f
Dag Andreas <daanfofo@gmail.com>
2024-08-16 14:05:24 +0200

    created testitem & testBagel

---
+++ src/test/java/com/booleanuk/core/TestBagel.java
@@ -0,0 +1,11 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class TestBagel {
+
+    @Test
+    public void testBagelConstr(){
+        Bagel b = new Bagel(2.5, )
+    }
+}
---
+++ src/test/java/com/booleanuk/core/TestItem.java
@@ -0,0 +1,12 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class TestItem {
+
+
+    @Test
+    public void testItem(){
+        // Item abstractCantBeInstanciated = new Item();
+    }
+}
