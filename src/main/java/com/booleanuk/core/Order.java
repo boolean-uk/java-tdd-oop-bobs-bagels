6b57bc99169c6f6268399dafcd11e8a40c9ee7e6
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-16 15:14:22 +0200

    testCreateOrder() pass

---
+++ src/main/java/com/booleanuk/core/Order.java
@@ -0,0 +1,13 @@
+package com.booleanuk.core;
+
+public class Order {
+    private int totalSum;
+
+    public Order() {
+        this.totalSum = 0;
+    }
+
+    public int getTotalSum() {
+        return totalSum;
+    }
+}
