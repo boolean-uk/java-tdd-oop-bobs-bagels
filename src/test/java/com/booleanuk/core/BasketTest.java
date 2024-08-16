e7c7d2d1d4abacbd13696a302cc9ccbd6029fc32
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-16 15:39:34 +0200

    testaddBagel

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -12,6 +12,15 @@ public class BasketTest {
         Assertions.assertEquals(10, basket.capacity);
     }
 
+    @Test
+    public void testaddBagel() {
+        Basket basket = new Basket(10);
+        Bagel bagel = new Bagel("BGLO",0.49, "Bagel", Bagel.BagelVariant.ONION);
+        basket.addBagel(bagel);
+        Assertions.assertTrue(basket.getListOfBasket().contains(bagel), "The bagel should be in the basket");
+
+    }
+
 
 
 }
