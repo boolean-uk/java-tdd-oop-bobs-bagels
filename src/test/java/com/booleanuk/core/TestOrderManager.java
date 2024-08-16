43632ee074a40261156dc510f63554592bc03f3a
Dag Andreas <daanfofo@gmail.com>
2024-08-16 15:57:25 +0200

    created testordermanager

---
+++ src/test/java/com/booleanuk/core/TestOrderManager.java
@@ -0,0 +1,11 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class TestOrderManager {
+
+    @Test
+    public void testOrderExists(){
+        TestOrderManager tom = new TestOrderManager();
+    }
+}
