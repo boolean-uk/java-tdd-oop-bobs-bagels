ccfe633f9ba973401c469c60b3ab014ec8a31e1f
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-16 15:03:13 +0200

    testCreateStore() fails

---
+++ src/test/java/com/booleanuk/core/StoreTest.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StoreTest {
+
+    @Test
+    public void testCreateStore() {
+        Store store = new Store("Bob's Bagels");
+        Assertions.assertNotNull(store);
+        Assertions.assertEquals("Bob's Bagels", store.getName());
+    }
+}
