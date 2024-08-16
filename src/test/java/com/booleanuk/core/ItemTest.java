b360e9c65930e9ab83e900f36cb69ebab406ca15
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-16 15:10:17 +0200

    Added a test to get SKU

--- src/test/java/com/booleanuk/core/ItemTest.java
+++ src/test/java/com/booleanuk/core/ItemTest.java
@@ -1,4 +1,13 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ItemTest {
+    @Test
+    public void createItemTest() {
+        Item test = new Item("BGLO", 0.49, "Bagel", "Onion");
+
+        Assertions.assertEquals("BGLO", test.getSKU());
+    }
 }
