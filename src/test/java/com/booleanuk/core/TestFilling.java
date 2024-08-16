a5306ab7be9c385a247ef1206e4fe3c7c8c6032f
Dag Andreas <daanfofo@gmail.com>
2024-08-16 15:01:38 +0200

    created test for filling

---
+++ src/test/java/com/booleanuk/core/TestFilling.java
@@ -0,0 +1,11 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class TestFilling {
+
+    @Test
+    public void testFilling(){
+        Filling f = new Filling();
+    }
+}
