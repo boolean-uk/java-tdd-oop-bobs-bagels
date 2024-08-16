105c08f293b9124b4cfceee7bebe24076a78bec9
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-16 15:25:36 +0200

    Added prodocut and test for it pass

---
+++ src/main/java/com/booleanuk/core/Product.java
@@ -0,0 +1,15 @@
+package com.booleanuk.core;
+
+public class Product {
+
+    private String SKU;
+
+    public Product(String SKU) {
+        this.SKU = SKU;
+    }
+
+    public String getSKU() {
+        return this.SKU;
+    }
+
+}
