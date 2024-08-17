962bb7661f802b784626e598004c560e06889888
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-17 20:34:43 +0200

    red test ShouldAddProductToBasket

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,16 +3,44 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class BasketTest {
 
     @Test
     public void ShouldShowProduct(){
         Bagel myBagel = new Bagel("BGLO",	0.49, "Bagel",	"Onion");
+        Coffee blackCoffee = new Coffee("COFB",	0.99,	"Coffee",	"Black");
 
-        Assertions.assertNotEquals("BGLO 0.49 Bagel Onion", myBagel.showProduct());
+        Assertions.assertEquals("BGLO 0.49 Bagel Onion", myBagel.showProduct());
+        Assertions.assertEquals("COFB 0.99 Coffee Black", blackCoffee.showProduct());
+
         myBagel.showProduct();
     }
 
+    @Test
+    public void ShouldAddProductToBasket(){
+        Basket basket = new Basket();
+
+        Bagel onionBagel = new Bagel("BGLO",	0.49, "Bagel",	"Onion");
+        Coffee blackCoffee = new Coffee("COFB",	0.99,	"Coffee",	"Black");
+
+        basket.addToCurrentBasket(onionBagel);
+        basket.addToCurrentBasket(blackCoffee);
+
+        ArrayList<String> expectedNames = new ArrayList<>();
+        expectedNames.add("Coffee");
+        expectedNames.add("Bagel");
+
+        ArrayList<String> actualNames = new ArrayList<>();
+        for (Product product : basket.currentBasket.keySet()){
+            String name = product.getName();
+            actualNames.add(name);
+        }
+
+        Assertions.assertNotEquals(expectedNames, actualNames);
+    }
 
 }
 
