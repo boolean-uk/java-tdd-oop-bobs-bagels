98283339fc3f8bcbb43e45102bf9955fea2c1f59
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-17 20:35:18 +0200

    add to current basket method

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,10 +1,33 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class Basket {
+    HashMap<Product, Integer> currentBasket;
+    private int basketCapacity = 10;
 
     public Basket(){
-        Bagel myBagel = new Bagel("BGLO",	0.49, "Bagel",	"Onion");
+        currentBasket = new HashMap<>();
+    }
 
-        myBagel.showProduct();
+    public String addToCurrentBasket(Product chosenProduct) {
+        String chosenName = chosenProduct.getName();
+
+        for (Product product : currentBasket.keySet()) {
+            String name = product.getName();
+
+            if (Objects.equals(name, chosenName)) {
+                int currentQuantity = currentBasket.get(product);
+
+                currentBasket.put(product, currentQuantity + 1);
+
+                return "Existing product added to basket";
+            }
+        }
+
+        currentBasket.put(chosenProduct, 1);
+
+        return "New product added to basket";
     }
 }
