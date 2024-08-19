02335eccd02b2e74c4ec12c90eaaa3d4782ac4ce
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-19 08:56:07 +0200

    testCreateProduct() with illegal argument pass

--- src/test/java/com/booleanuk/core/ProductTest.java
+++ src/test/java/com/booleanuk/core/ProductTest.java
@@ -10,5 +10,10 @@ public class ProductTest {
         Product product = new Product("SKU", 10, "Variant");
         Assertions.assertNotNull(product);
         Assertions.assertEquals(10, product.getPrice());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Product(null, 20, "Variant2"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Product("SKU3", -20, "Variant3"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Product("SKU4", 20, null));
+
     }
 }
