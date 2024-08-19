f468b939a7c4c4c48f88034ff12b9ec766e91c38
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-19 09:22:33 +0200

    testGetBasket() pass

--- src/main/java/com/booleanuk/core/Order.java
+++ src/main/java/com/booleanuk/core/Order.java
@@ -1,10 +1,15 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Order {
+    private Map<String, Integer> basket;
     private int totalSum;
 
     public Order() {
         this.totalSum = 0;
+        this.basket = new HashMap<>();
     }
 
     public int getTotalSum() {
@@ -12,6 +17,11 @@ public class Order {
     }
 
     public void addProduct(Product product) {
+        basket.put(product.getSKU(), 1);
         totalSum += product.getPrice();
+    }
+
+    public Map<String, Integer> getBasket() {
+        return basket;
     }
 }
