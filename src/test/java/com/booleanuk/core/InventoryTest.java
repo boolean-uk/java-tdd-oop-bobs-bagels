1e409c629598fb52ab44c74ddf89ac03e9050875
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-19 16:03:19 +0200

    Added test for checking if a product is in the inventory

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,4 +1,22 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InventoryTest {
+
+    public InventoryTest() {
+
+    }
+
+    @Test
+    public void testCheckIfProductInInventory() {
+        Inventory inv = new Inventory();
+        Basket basket = new Basket();
+
+        Bagel bagel = new OnionBagel();
+
+        Assertions.assertTrue(inv.productIsInStock(bagel));
+    }
+
 }
