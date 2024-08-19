d00d4d7c5497a2a4dc3a9c2b955df97851e0446a
gremble0 <hermanstornes@gmail.com>
2024-08-19 15:42:57 +0200

    Add test case for multiple same items on receipt

--- src/test/java/com/booleanuk/core/ReceiptTest.java
+++ src/test/java/com/booleanuk/core/ReceiptTest.java
@@ -16,15 +16,37 @@ class ReceiptTest {
     Inventory inventory = new Inventory(inventoryStock);
 
     Basket basket = new Basket();
+    basket.add(new Coffee(CoffeeType.BLACK));
+    basket.add(new Coffee(CoffeeType.WHITE));
     basket.add(new Bagel(BagelType.ONION));
+
+    Receipt receipt = inventory.purchase(basket);
+    String expected = "~~~ Bob's Bagels ~~~\n" +
+        "----------------------\n" +
+        "1x Black coffee 0.99\n" +
+        "1x White coffee 1.19\n" +
+        "1x Onion bagel 0.49\n" +
+        "----------------------";
+    Assertions.assertEquals(expected, receipt.toString());
+  }
+
+  @Test
+  public void testReceiptWithSameItems() {
+    List<Product> inventoryStock = new ArrayList<>();
+    inventoryStock.add(new Coffee(CoffeeType.BLACK));
+    inventoryStock.add(new Coffee(CoffeeType.BLACK));
+    inventoryStock.add(new Coffee(CoffeeType.BLACK));
+    Inventory inventory = new Inventory(inventoryStock);
+
+    Basket basket = new Basket();
+    basket.add(new Coffee(CoffeeType.BLACK));
+    basket.add(new Coffee(CoffeeType.BLACK));
     basket.add(new Coffee(CoffeeType.BLACK));
 
     Receipt receipt = inventory.purchase(basket);
-    String expected = "~~~ Bob's Bagels ~~~" +
-        "----------------------" +
-        "1x Black coffee 0.99" +
-        "1x White coffee 1.19" +
-        "1x Onion bagel 0.49" +
+    String expected = "~~~ Bob's Bagels ~~~\n" +
+        "----------------------\n" +
+        "3x Black coffee 2.97\n" +
         "----------------------";
     Assertions.assertEquals(expected, receipt.toString());
   }
