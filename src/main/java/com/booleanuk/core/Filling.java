d16dbef8a6ac506dfba663ba288e814a4da9b8b0
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-19 09:27:08 +0200

    testFilling pass

--- src/main/java/com/booleanuk/core/Filling.java
+++ src/main/java/com/booleanuk/core/Filling.java
@@ -3,4 +3,49 @@ package com.booleanuk.core;
 public class Filling extends Item{
 
     private Bagel bagel;
+
+    public Filling(String SKU, int id, Bagel bagel) {
+        this.bagel = bagel;
+        this.setId(id);
+        this.setName("Filling");
+        this.setPrice(0.12);
+        switch (SKU) {
+            case "FILB": {
+                this.setSKU(SKU);
+                this.setVariant("Bacon");
+                this.bagel.addFillings(this);
+                break;
+            }
+            case "FILE": {
+                this.setSKU(SKU);
+                this.setVariant("Egg");
+                this.bagel.addFillings(this);
+                break;
+            }
+            case "FILC": {
+                this.setSKU(SKU);
+                this.setVariant("Cheese");
+                this.bagel.addFillings(this);
+                break;
+            }
+            case "FILX": {
+                this.setSKU(SKU);
+                this.setVariant("Cream Cheese");
+                this.bagel.addFillings(this);
+                break;
+            }
+            case "FILS": {
+                this.setSKU(SKU);
+                this.setVariant("Smoked Salmon");
+                this.bagel.addFillings(this);
+                break;
+            }
+            case "FILH": {
+                this.setSKU(SKU);
+                this.setVariant("Ham");
+                this.bagel.addFillings(this);
+                break;
+            }
+        }
+    }
 }
