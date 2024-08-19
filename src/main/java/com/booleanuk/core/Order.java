9cafdba580a30fa740ccbdd73605d1085e7ddc66
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-19 09:39:18 +0200

    testBasketIsFull() pass

--- src/main/java/com/booleanuk/core/Order.java
+++ src/main/java/com/booleanuk/core/Order.java
@@ -6,23 +6,31 @@ import java.util.Map;
 public class Order {
     private Map<String, Integer> basket;
     private int totalSum;
+    private int maxBasketCapacity = 20;
+    private int currentBasketCapacity;
 
     public Order() {
         this.totalSum = 0;
         this.basket = new HashMap<>();
+        this.currentBasketCapacity = 0;
     }
 
     public int getTotalSum() {
         return totalSum;
     }
 
-    public void addProduct(Product product) {
+    public boolean addProduct(Product product) {
+        if (currentBasketCapacity == maxBasketCapacity) {
+            return false;
+        }
         if (basket.containsKey(product.getSKU())) {
             basket.put(product.getSKU(), basket.get(product.getSKU()) + 1);
         } else {
             basket.put(product.getSKU(), 1);
         }
         totalSum += product.getPrice();
+        currentBasketCapacity++;
+        return true;
     }
 
     public Map<String, Integer> getBasket() {
