b71b31fbabbf82a04a814e000c312da655a7a9d0
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-19 10:29:30 +0200

    testAddBasicItem pass

--- src/test/java/com/booleanuk/core/ItemHandlerTest.java
+++ src/test/java/com/booleanuk/core/ItemHandlerTest.java
@@ -2,13 +2,13 @@ package com.booleanuk.core;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ItemHandlerTest {
-    ItemHandler itemHandler = new ItemHandler();
+    ItemHandler itemHandler;
     @Test
     public void testCreateItemHandler() {
+        itemHandler = new ItemHandler();
         assertEquals(0, itemHandler.getIdTracker());
         assertEquals(1, itemHandler.getBasketCapacity());
         assertEquals(0, itemHandler.getBasket().size());
@@ -17,6 +17,7 @@ public class ItemHandlerTest {
 
     @Test
     public void testAddBasicItem() {
+        itemHandler = new ItemHandler();
         Item coffee1 = itemHandler.addBasicItem("COFL");
         assertEquals(1.29, coffee1.getPrice());
 
@@ -24,6 +25,9 @@ public class ItemHandlerTest {
         Item bagel1 = itemHandler.addBasicItem("BGLS");
         assertEquals(1, itemHandler.getBasket().size());
 
-
+        itemHandler = new ItemHandler();
+        Item filling1 = itemHandler.addBasicItem("FILC");
+        assertEquals(0, itemHandler.getBasket().size());
+        assertNull(filling1);
     }
 }
