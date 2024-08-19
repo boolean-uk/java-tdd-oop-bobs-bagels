d3f264971480b3982423b6add1edc2c9d98e5234
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-19 09:42:05 +0200

    testCreateItemHandler pass

--- src/main/java/com/booleanuk/core/ItemHandler.java
+++ src/main/java/com/booleanuk/core/ItemHandler.java
@@ -8,4 +8,24 @@ public class ItemHandler {
     private int basketCapacity;
     private HashMap<String, String> allItems;
     private int idTracker;
+
+    public ItemHandler() {
+        this.idTracker = 0;
+    }
+
+    public HashMap<String, Item> getBasket() {
+        return basket;
+    }
+
+    public int getBasketCapacity() {
+        return basketCapacity;
+    }
+
+    public HashMap<String, String> getAllItems() {
+        return allItems;
+    }
+
+    public int getIdTracker() {
+        return idTracker;
+    }
 }
