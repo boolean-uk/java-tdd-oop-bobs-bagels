57396c5291c9341bc8b840098e6ad8aa6e092bfe
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-19 08:43:29 +0200

    testBagel pass

--- src/test/java/com/booleanuk/core/ItemTest.java
+++ src/test/java/com/booleanuk/core/ItemTest.java
@@ -3,6 +3,7 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ItemTest {
 
@@ -10,5 +11,8 @@ public class ItemTest {
     public void testBagel() {
         Bagel bagel = new Bagel("BGLO", 0);
         assertEquals(bagel.getPrice(), 0.49);
+
+        Bagel bagel1 = new Bagel("BGLOF", 0);
+        assertNull(bagel1.getVariant());
     }
 }
