a6db574acb74eead353448f18719c5ecd64beb04
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-19 08:48:24 +0200

    testAddProduct() fails

--- src/test/java/com/booleanuk/core/OrderTest.java
+++ src/test/java/com/booleanuk/core/OrderTest.java
@@ -10,4 +10,12 @@ public class OrderTest {
         Order order = new Order();
         Assertions.assertEquals(0, order.getTotalSum());
     }
+
+    @Test
+    public void testAddProduct() {
+        Order order = new Order();
+        Product product = new Product("SKU", 10, "Variant");
+        order.addProduct(product);
+        Assertions.assertEquals(10, order.getTotalSum());
+    }
 }
