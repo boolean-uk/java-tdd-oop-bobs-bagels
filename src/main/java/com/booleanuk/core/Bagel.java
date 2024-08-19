9fe880d5dcf617c0ec029cf4964b689fc2f5ead4
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-19 15:09:43 +0200

    Bagel can now hold a filling and get a filling

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -2,8 +2,18 @@ package com.booleanuk.core;
 
 public class Bagel extends Product {
 
+    private Filling currentFilling;
+
     public Bagel(String SKU, double price) {
         super(SKU, price);
+    }
+
+    public Filling getFilling() {
+        return this.currentFilling;
+    }
+
+    public void setFilling(Filling newFilling) {
+        this.currentFilling = newFilling;
     }
 
 }
