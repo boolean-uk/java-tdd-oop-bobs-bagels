faaeca5b2b5e6a411a7e0d5d1124edb857093b32
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-19 10:36:51 +0200

    TestGetPriceWithFillings() fails

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -31,4 +31,15 @@ public class BagelTest {
         Assertions.assertTrue(bagel.addFilling(filling2));
         Assertions.assertFalse(bagel.addFilling(filling3));
     }
+
+    // test get price with fillings
+    @Test
+    public void TestGetPriceWithFillings() {
+        Bagel bagel = new Bagel("SKU", 10, "Variant");
+        Filling filling = new Filling("SKU", 10, "Variant");
+        Filling filling2 = new Filling("SKU2", 20, "Variant2");
+        bagel.addFilling(filling);
+        bagel.addFilling(filling2);
+        Assertions.assertEquals(40, bagel.getPrice());
+    }
 }
