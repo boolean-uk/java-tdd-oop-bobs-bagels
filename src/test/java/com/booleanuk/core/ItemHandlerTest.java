4330b6114405ee16bb6a7824305275af5bf4d95d
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-19 09:40:55 +0200

    testCreateItemHandler fail

--- src/test/java/com/booleanuk/core/ItemHandlerTest.java
+++ src/test/java/com/booleanuk/core/ItemHandlerTest.java
@@ -1,4 +1,14 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ItemHandlerTest {
+    ItemHandler itemHandler = new ItemHandler();
+    @Test
+    public void testCreateItemHandler() {
+        assertEquals(0, itemHandler.getIdTracker());
+
+    }
 }
