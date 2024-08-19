6e34a08cfb9531d935ea817ef4d0677072fcae5d
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-19 14:10:23 +0200

    Added functionality for making sure products cannot be added to basket when it is full

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -6,27 +6,37 @@ public class Basket {
 
     private final int DEFAULT_CAPACITY = 5;
 
-    private HashMap<String, Integer> basket;
+    private HashMap<String, Integer> basket; // String=SKU, Integer=quantity in the basket
     private int capacity;
+    private int size;
 
     public Basket() {
         this.capacity = DEFAULT_CAPACITY;
         this.basket = new HashMap<>();
+        this.size = 0; // Number of products in the basket
     }
 
     public boolean addProduct(Product p) {
+        if (isFull()) {
+            return false;
+        }
+
         if (this.basket.containsKey(p.getSKU())) {
             this.basket.replace(p.getSKU(), this.basket.get(p.getSKU()) + 1);
-            return true;
         } else {
             this.basket.put(p.getSKU(), 1);
         }
 
+        ++this.size;
         return true;
     }
 
     public HashMap<String, Integer> getBasket() {
         return this.basket;
+    }
+
+    public boolean isFull() {
+        return this.size == this.capacity;
     }
 
 }
