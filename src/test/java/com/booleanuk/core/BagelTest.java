04d5a2c0b55d0a4ae589184e407ff1d2fb901299
gremble0 <hermanstornes@gmail.com>
2024-08-19 10:31:46 +0200

    Add testSku to BagelTest

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -11,4 +11,13 @@ class BagelTest {
     myBagel.add(new Filling(FillingType.BACON));
     Assertions.assertEquals(myBagel.price(), 0.49 + 0.12 + 0.12);
   }
+
+  @Test
+  public void testSku() {
+    Bagel onionBagel = new Bagel(BagelType.ONION);
+    Assertions.assertEquals(onionBagel.sku(), Sku.BGLO);
+
+    Bagel everythingBagel = new Bagel(BagelType.EVERYTHING);
+    Assertions.assertEquals(everythingBagel.sku(), Sku.BGLE);
+  }
 }
