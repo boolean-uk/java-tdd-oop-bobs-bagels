45faf9e520d9db5fa1b0b1b1da114c769ff31df2
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-19 09:53:33 +0200

    testCreateItemHandler pass

--- src/main/java/com/booleanuk/core/ItemHandler.java
+++ src/main/java/com/booleanuk/core/ItemHandler.java
@@ -13,7 +13,27 @@ public class ItemHandler {
         this.idTracker = 0;
         this.basketCapacity = 1;
         this.basket = new HashMap<>();
+        setUpAllItems();
+    }
 
+    public void setUpAllItems() {
+        this.allItems = new HashMap<>();
+        this.allItems.put("BGLO", "Bagel");
+        this.allItems.put("BGLP", "Bagel");
+        this.allItems.put("BGLE", "Bagel");
+        this.allItems.put("BGLS", "Bagel");
+
+        this.allItems.put("COFB", "Coffee");
+        this.allItems.put("COFW", "Coffee");
+        this.allItems.put("COFC", "Coffee");
+        this.allItems.put("COFL", "Coffee");
+
+        this.allItems.put("FILB", "Filling");
+        this.allItems.put("FILE", "Filling");
+        this.allItems.put("FILC", "Filling");
+        this.allItems.put("FILX", "Filling");
+        this.allItems.put("FILS", "Filling");
+        this.allItems.put("FILH", "Filling");
     }
 
     public HashMap<String, Item> getBasket() {
