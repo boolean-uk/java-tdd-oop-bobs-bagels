8874aab99cf43affac8556d72d3ff7675a7fa47f
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-20 10:51:20 +0200

    Added test to find filling price before adding to basket. Functionality already in place due to previous implemented functionality.

--- src/test/java/com/booleanuk/core/FillingTest.java
+++ src/test/java/com/booleanuk/core/FillingTest.java
@@ -1,5 +1,21 @@
 package com.booleanuk.core;
 
-public class FillingTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
+public class FillingTest {
+    @Test
+    public void findPriceForFillingTest(){
+        /*
+        As a customer,
+        So I don't over-spend,
+        I'd like to know the cost of each filling before I add it to my bagel order.
+         */
+        Item item = Menu.getFillingFromMenu("Filling", "Bacon");
+        assert item != null;
+
+        Assertions.assertEquals(0.12, (double) item.getPrice() /100);
+    }
 }
