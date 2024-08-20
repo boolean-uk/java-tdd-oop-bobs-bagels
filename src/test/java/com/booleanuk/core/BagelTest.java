5b8688397246b7fe907c7f18f2685412eede12e5
Iris Flystam <badde00@gmail.com>
2024-08-20 15:17:56 +0200

    RED: testCalculateCost

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -1,7 +1,14 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BagelTest {
-
+    @Test
+    public void testCalculateCost() {
+        Bagel bagel = new Bagel(Controller.prices.get("BGLO"), Controller.prices.get("FILB"));
+        Assertions.assertEquals(0.61f, bagel.calculateCost(), 0.001);
+    }
 }
