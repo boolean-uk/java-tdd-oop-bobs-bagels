9159590fc35362a7a3f333f6ec1c34188a2e37cb
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-20 08:34:33 +0200

    changed inventory

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -12,7 +12,21 @@ public class Inventory {
         //Bagels
         this.menu.put("OnionBagel", new Bagel("BGLO", 0.49, "Bagel", "Onion"));
         this.menu.put("PlainBagel", new Bagel("BGLP", 0.39, "Bagel", "Plain"));
-        this.menu.put("BaconFilling", new Bagel("FILB",	0.12,	"Filling",	"Bacon"));
+        this.menu.put("EverythingBagel", new Bagel("BGLE",	0.49,	"Bagel",	"Everything"));
+        this.menu.put("SesameBagel", new Bagel("BGLS",	0.49,	"Bagel",	"Sesame"));
+
+        //Coffees
+        this.menu.put("BlackCoffee", new Coffee("COFB",	0.99,	"Coffee",	"Black"));
+        this.menu.put("WhiteCoffee", new Coffee("COFW",	1.19,	"Coffee",	"White"));
+        this.menu.put("CapuccinoCoffee", new Coffee("COFC",	1.29,	"Coffee",	"Capuccino"));
+        this.menu.put("LatteCoffee", new Coffee("COFL",	1.29,	"Coffee",	"Latte"));
+
+        //Fillings
+        this.menu.put("BaconFilling", new Filling("FILB",	0.12,	"Filling",	"Bacon"));
+        this.menu.put("EggFilling", new Filling("FILE",	0.12,	"Filling",	"Egg"));
+        this.menu.put("CheeseFilling", new Filling("FILC",	0.12,	"Filling",	"Cheese"));
+        this.menu.put("SmokedSalmonFilling", new Bagel("FILS",	0.12,	"Filling",	"Smoked Salmon"));
+        this.menu.put("HamFilling", new Bagel("FILH",	0.12,	"Filling",	"Ham"));
 
     }
 
