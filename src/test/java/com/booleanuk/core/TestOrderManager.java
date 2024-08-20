ee1d15507e9005e77ebe162db6525bd53fc09212
Dag Andreas <daanfofo@gmail.com>
2024-08-20 11:10:27 +0200

    added test for getprice

--- src/test/java/com/booleanuk/core/TestOrderManager.java
+++ src/test/java/com/booleanuk/core/TestOrderManager.java
@@ -25,6 +25,40 @@ public class TestOrderManager {
 
         result = om.addItem(CoffeeType.Cappuccino);
         Assertions.assertEquals("Cappuccino: 2", result);
+
+        for (int i = 0; i < OrderManager.getMaxFillings(); i++) {
+            result = om.addItem(FillingType.Bacon);
+            Assertions.assertEquals("Bacon: " + (i+1), result);
+        }
+
+        result = om.addItem(FillingType.Bacon);
+        Assertions.assertEquals("Item not in stock.", result);
     }
+
+    @Test
+    public void testGetPrice(){ // core
+        OrderManager om = new OrderManager();
+        om.addItem(BagelType.Onion);
+        om.addItem(BagelType.Onion);
+        om.addItem(BagelType.Onion);
+
+        double pricePerOnion = 0.49;
+        double expectedPrice = 3* pricePerOnion;
+        Assertions.assertEquals(expectedPrice, om.getPrice());
+
+        om.addItem(CoffeeType.Cappuccino);
+        double pricePerCappuccino = 1.29;
+        expectedPrice += pricePerCappuccino;
+        Assertions.assertEquals(expectedPrice, om.getPrice());
+
+        om.addItem(CoffeeType.Cappuccino);
+        om.addItem(CoffeeType.Cappuccino);
+        expectedPrice += pricePerCappuccino*2;
+        Assertions.assertEquals(expectedPrice, om.getPrice());
+
+
+    }
+
+
 
 }
