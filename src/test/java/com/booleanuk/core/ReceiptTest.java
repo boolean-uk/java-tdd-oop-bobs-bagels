955e0d667dfee4da675ee523e31fc97078e3d4cd
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-20 10:49:57 +0200

    testReceiptGetTotalSum() pass

--- src/test/java/com/booleanuk/core/ReceiptTest.java
+++ src/test/java/com/booleanuk/core/ReceiptTest.java
@@ -18,4 +18,14 @@ public class ReceiptTest {
         Receipt receipt = new Receipt(order);
         Assertions.assertEquals(order, receipt.getOrder());
     }
+
+    @Test
+    public void testReceiptTotal() {
+        Order order = new Order();
+        Store store = new Store("Bob's Bagels");
+        Product product = store.getInventory().getProduct("BGLO");
+        order.addProduct(product);
+        Receipt receipt = new Receipt(order);
+        Assertions.assertEquals(product.getPrice(), receipt.getOrder().getTotalSum());
+    }
 }
