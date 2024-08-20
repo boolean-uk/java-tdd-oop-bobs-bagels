b53c08936ed0b21489f5515cc7ba5147c3164409
gremble0 <hermanstornes@gmail.com>
2024-08-20 11:20:05 +0200

    Make test for extension 3

--- src/test/java/com/booleanuk/core/ReceiptTest.java
+++ src/test/java/com/booleanuk/core/ReceiptTest.java
@@ -45,5 +45,26 @@ class ReceiptTest {
     Receipt receipt = inventory.purchase(basket);
     String actual = receipt.toString();
     Assertions.assertTrue(actual.contains("3x Black coffee " + String.format("%.2f", Sku.COFB.price() * 3)));
+    Assertions.assertTrue(
+        actual.contains("Total " + String.format("%.2f", Sku.COFB.price() * 3)));
+  }
+
+  @Test
+  public void testReceiptWithDiscount() {
+    List<Product> inventoryStock = new ArrayList<>();
+    inventoryStock.add(new Coffee(CoffeeType.BLACK));
+    inventoryStock.add(new Bagel(BagelType.ONION));
+    Inventory inventory = new Inventory(inventoryStock);
+
+    Basket basket = new Basket();
+    basket.add(new Bagel(BagelType.ONION));
+    basket.add(new Coffee(CoffeeType.BLACK));
+    Receipt receipt = inventory.purchase(basket);
+    String actual = receipt.toString();
+    Assertions.assertEquals("", actual); // temporary to see full output
+    Assertions.assertTrue(actual.contains("1x Black coffee " + String.format("%.2f", Sku.COFB.price())));
+    Assertions.assertTrue(actual.contains("1x Onion bagel " + String.format("%.2f", Sku.BGLO.price())));
+    Assertions.assertTrue(actual.contains("Total 1.25"));
+
   }
 }
