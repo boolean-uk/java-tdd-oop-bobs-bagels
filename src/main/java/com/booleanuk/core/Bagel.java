deeb7dee45cbef41c0a837715553d5b2e3f2b494
Iris Flystam <badde00@gmail.com>
2024-08-20 15:18:17 +0200

    GREEN: testCalculateCost passes and made a toString()

--- src/main/java/com/booleanuk/core/Bagel.java
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -1,11 +1,24 @@
 package com.booleanuk.core;
 
 public class Bagel {
-    private Triple<String, String, Float> bagelType;
-    private Triple<String, String, Float> fillingType;
+    private final Triple<String, String, Float> bagelType;
+    private final Triple<String, String, Float> fillingType;
 
     public Bagel(Triple<String, String, Float> bagelType, Triple<String, String, Float> fillingType) {
         this.bagelType = bagelType;
         this.fillingType = fillingType;
+    }
+
+    @Override
+    public String toString() {
+        return "Bagel{" +
+                "bagelType=" + bagelType.b() +
+                ", fillingType=" + fillingType.b() +
+                ", cost=" + calculateCost() +
+                '}';
+    }
+
+    public float calculateCost() {
+        return bagelType.c() + fillingType.c();
     }
 }
