94850fc3293d10463a77c44b1d477f82832f318d
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-20 16:20:19 +0200

    Inventory finished

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -3,12 +3,12 @@ import java.util.HashMap;
 
 public class Inventory {
     HashMap<String, String[]> bagels;
-    HashMap<String, String[]> filling;
+    HashMap<String, String[]> fillings;
     HashMap<String, String[]> coffees;
 
     public Inventory(){
         this.bagels = new HashMap<>();
-        this.filling = new HashMap<>();
+        this.fillings = new HashMap<>();
         this.coffees = new HashMap<>();
 
         this.bagels.put("BGLO", new String[]{"BGLO", "0.49", "Bagel", "Onion"});
@@ -16,17 +16,23 @@ public class Inventory {
         this.bagels.put("BGLE", new String[]{"BGLE",	"0.49",	"Bagel", "Everything"});
         this.bagels.put("BGLS", new String[]{"BGLS",	"0.49",	"Bagel", "Sesame"});
 
-        this.filling.put("FILB", new String[]{"FILB",	"0.12",	"Filling",	"Bacon"});
-        this.filling.put("FILE", new String[]{"FILE",	"0.12",	"Filling",	"Egg"});
-        this.filling.put("FILC", new String[]{"FILC",	"0.12",	"Filling",	"Cheese"});
-        this.filling.put("FILS", new String[]{"FILS",	"0.12",	"Filling",	"Smoked Salmon"});
-        this.filling.put("FILH", new String[]{"FILH",	"0.12",	"Filling",	"Ham"});
+        this.fillings.put("FILB", new String[]{"FILB",	"0.12",	"Filling",	"Bacon"});
+        this.fillings.put("FILE", new String[]{"FILE",	"0.12",	"Filling",	"Egg"});
+        this.fillings.put("FILC", new String[]{"FILC",	"0.12",	"Filling",	"Cheese"});
+        this.fillings.put("FILS", new String[]{"FILS",	"0.12",	"Filling",	"Smoked Salmon"});
+        this.fillings.put("FILH", new String[]{"FILH",	"0.12",	"Filling",	"Ham"});
+
+        this.coffees.put("COFB", new String[]{"COFB",	"0.99",	"Coffee",	"Black"});
+        this.coffees.put("COFW", new String[]{"COFW",	"1.19",	"Coffee",	"White"});
+        this.coffees.put("COFC", new String[]{"COFC",	"1.29",	"Coffee",	"Cappuccino"});
+        this.coffees.put("COFL", new String[]{"COFL",	"1.29",	"Coffee",	"Latte"});
 
     }
 
     public Product getItem(String referenceSku){
         String[] isBagel = bagels.get(referenceSku);
-        String[] isFilling = filling.get(referenceSku);
+        String[] isFilling = fillings.get(referenceSku);
+        String[] isCoffee = coffees.get(referenceSku);
 
         if(isBagel != null){
             return new Bagel(isBagel[0], Double.valueOf(isBagel[1]), isBagel[2], isBagel[3]);
@@ -36,12 +42,13 @@ public class Inventory {
             return new Filling(isFilling[0], Double.valueOf(isFilling[1]), isFilling[2], isFilling[3]);
         }
 
+        else if (isCoffee != null){
+            return new Coffee(isCoffee[0], Double.valueOf(isCoffee[1]), isCoffee[2], isCoffee[3]);
+        }
+
         else {
             return null;
         }
-
-
-
     }
 
 }
