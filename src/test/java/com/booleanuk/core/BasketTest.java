b2610504ff9673103cff007148e13de6ece2e454
Iris Flystam <badde00@gmail.com>
2024-08-20 14:37:09 +0200

    RED: testAddBagelToFullBasket

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -15,4 +15,16 @@ class BasketTest {
         basket.addBagel(bagel);
         Assertions.assertEquals(1, basket.size());
     }
+
+    @Test
+    public void testAddBagelToFullBasket() {
+        Basket basket = new Basket(1);
+        Bagel bagel1 = new Bagel(Controller.prices.get("BGLO"), Controller.prices.get("FILB"));
+        Bagel bagel2 = new Bagel(Controller.prices.get("BGLO"), Controller.prices.get("FILB"));
+        Assertions.assertEquals(0, basket.size());
+        basket.addBagel(bagel1);
+        Assertions.assertEquals(1, basket.size());
+        basket.addBagel(bagel2);
+        Assertions.assertEquals(1, basket.size());
+    }
 }
