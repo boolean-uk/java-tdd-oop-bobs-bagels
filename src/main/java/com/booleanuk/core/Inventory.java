2d14e676a58f1a5a201a5148622d5aff997279b5
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-20 11:50:39 +0200

    refactored to create inventory whit right product type

--- src/main/java/com/booleanuk/core/Inventory.java
+++ src/main/java/com/booleanuk/core/Inventory.java
@@ -14,20 +14,20 @@ public class Inventory {
     }
 
     public void setInventory() {
-        products.put("BGLO", new Product("BGLO", 49, "Onion"));
-        products.put("BGLP", new Product("BGLP", 39, "Plain"));
-        products.put("BGLE", new Product("BGLE", 49, "Everything"));
-        products.put("BGLS", new Product("BGLS", 49, "Sesame"));
-        products.put("COFB", new Product("COFB", 99, "Black"));
-        products.put("COFW", new Product("COFW", 119, "White"));
-        products.put("COFC", new Product("COFC", 129, "Cappuccino"));
-        products.put("COFL", new Product("COFL", 129, "Latte"));
-        products.put("FILB", new Product("FILB", 12, "Bacon"));
-        products.put("FILE", new Product("FILE", 12, "Egg"));
-        products.put("FILC", new Product("FILC", 12, "Cheese"));
-        products.put("FILX", new Product("FILX", 12, "Cream Cheese"));
-        products.put("FILS", new Product("FILS", 12, "Smoked Salmon"));
-        products.put("FILH", new Product("FILH", 12, "Ham"));
+        products.put("BGLO", new Bagel("BGLO", 49, "Onion"));
+        products.put("BGLP", new Bagel("BGLP", 39, "Plain"));
+        products.put("BGLE", new Bagel("BGLE", 49, "Everything"));
+        products.put("BGLS", new Bagel("BGLS", 49, "Sesame"));
+        products.put("COFB", new Coffee("COFB", 99, "Black"));
+        products.put("COFW", new Coffee("COFW", 119, "White"));
+        products.put("COFC", new Coffee("COFC", 129, "Cappuccino"));
+        products.put("COFL", new Coffee("COFL", 129, "Latte"));
+        products.put("FILB", new Filling("FILB", 12, "Bacon"));
+        products.put("FILE", new Filling("FILE", 12, "Egg"));
+        products.put("FILC", new Filling("FILC", 12, "Cheese"));
+        products.put("FILX", new Filling("FILX", 12, "Cream Cheese"));
+        products.put("FILS", new Filling("FILS", 12, "Smoked Salmon"));
+        products.put("FILH", new Filling("FILH", 12, "Ham"));
 
     }
     public Product getProduct(String SKU) {
