21ff6deff17b184d1533b805e489be9f9923056d
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-20 11:51:04 +0200

    refactored to create a constructor

--- src/main/java/com/booleanuk/core/Coffee.java
+++ src/main/java/com/booleanuk/core/Coffee.java
@@ -1,4 +1,8 @@
 package com.booleanuk.core;
 
-public class Coffee {
+public class Coffee extends Product {
+
+    public Coffee(String sku, int price, String variant) {
+        super(sku, price, variant);
+    }
 }
