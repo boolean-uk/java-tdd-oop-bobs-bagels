3a2c789cd7c51fe1565b2399fd22b55db63d2e7e
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-20 07:48:34 +0200

    extend topping from bagel

--- src/main/java/com/booleanuk/core/Filling.java
+++ src/main/java/com/booleanuk/core/Filling.java
@@ -1,4 +1,8 @@
 package com.booleanuk.core;
 
-public class Filling {
+public class Filling extends Bagel {
+    Filling(String sku, double price, String name, String variant) {
+        super(sku, price, name, variant);
+    }
+
 }
