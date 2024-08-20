135d96053814e4cd08e042e0017b17fe747be2ff
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-20 10:57:34 +0200

    Remade imports in Bagel test (importing everything from the bagel package)

--- src/test/java/com/booleanuk/core/BagelTest.java
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -1,8 +1,6 @@
 package com.booleanuk.core;
 
-import com.booleanuk.core.products.bagels.Bagel;
-import com.booleanuk.core.products.bagels.OnionBagel;
-import com.booleanuk.core.products.bagels.PlainBagel;
+import com.booleanuk.core.products.bagels.*;
 import com.booleanuk.core.products.fillings.SmokedSalmonFilling;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
