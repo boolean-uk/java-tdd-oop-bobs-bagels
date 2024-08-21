91f4c8f40ba51b224de10b9533cc0551a05fc73a
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-21 08:15:27 +0200

    Added test for 12 Plain Bagel discount

--- src/test/java/com/booleanuk/core/DiscountTest.java
+++ src/test/java/com/booleanuk/core/DiscountTest.java
@@ -2,9 +2,11 @@ package com.booleanuk.core;
 
 import com.booleanuk.core.products.bagels.EverythingBagel;
 import com.booleanuk.core.products.bagels.OnionBagel;
+import com.booleanuk.core.products.bagels.PlainBagel;
 import com.booleanuk.core.products.bagels.SesameBagel;
 import com.booleanuk.core.products.coffees.BlackCoffee;
 import com.booleanuk.core.products.coffees.WhiteCoffee;
+import com.booleanuk.core.products.fillings.BaconFilling;
 import com.booleanuk.core.products.fillings.HamFilling;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -51,6 +53,32 @@ public class DiscountTest {
         basket.addProduct(new BlackCoffee());
 
         Assertions.assertEquals(3.48, basket.getTotalCost(), 0.001);
+    }
+
+    @Test
+    public void test12PlainBagelDiscount() {
+        Basket basket = new Basket();
+        basket.changeCapacity(1000, true);
+
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+        basket.addProduct(new PlainBagel());
+
+        Assertions.assertEquals(3.99, basket.getTotalCost(), 0.001);
+
+        basket.addProduct(new WhiteCoffee()); // 1.19 cost
+        basket.addProduct(new BaconFilling()); // 0.12 cost
+
+        Assertions.assertEquals(5.3, basket.getTotalCost(), 0.001);
     }
 
 }
