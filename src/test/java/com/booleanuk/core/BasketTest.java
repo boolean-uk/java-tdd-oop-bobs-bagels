1367ffc5ae9cdfb1e0357b15fe2b584738b65da4
shyye <emma.art.dev@gmail.com>
2024-08-21 12:33:12 +0200

    Add addCoffee test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -8,10 +8,17 @@ public class BasketTest {
     Basket basket;
 
     @Test
+    public void checkPrintBasket() {
+        this.inventory = new Inventory();
+        this.basket = new Basket(new Inventory());
+        basket.printBasket();
+    }
+
+    @Test
     public void addCoffee() {
         this.inventory = new Inventory();
-        this.basket = new Basket();
-        Product coffee = basket.addCoffee(CoffeeVariant.CAPUCCINO);
-        Assertions.assertTrue(basket.getAll().contains(coffee));
+        this.basket = new Basket(new Inventory());
+        basket.addCoffee("COFW");
+        Assertions.assertEquals("COFW", basket.getAll().get(1).getSKU());
     }
 }
