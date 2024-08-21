bf6356d4b116d77c1ac0f7f582d8011c1fcd0ff4
shyye <emma.art.dev@gmail.com>
2024-08-21 11:09:26 +0200

    Add test comment

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,5 +1,6 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InventoryTest {
@@ -14,5 +15,18 @@ public class InventoryTest {
         inventory.printMenu();
     }
 
-    // TODO: Add more tests
+    @Test
+    public void getProductBasedOnSKU() {
+        this.inventory = new Inventory();
+
+        Coffee coffee = inventory.getCoffeeProduct("COFC");
+        Assertions.assertEquals(ProductName.COFFEE, coffee.getName());
+
+        Bagel bagel = inventory.getBagelProduct("BAGE");
+        Assertions.assertEquals(ProductName.BAGEL, bagel.getName());
+
+        Filling filling = inventory.getFillingProduct("FILB");
+        Assertions.assertEquals(ProductName.FILLING, filling.getName());
+    }
+    // TODO: Add test for exception returned when SKU is invalid / Does npt exist
 }
