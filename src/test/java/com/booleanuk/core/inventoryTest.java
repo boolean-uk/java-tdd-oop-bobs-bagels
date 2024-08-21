12e6bf25ffacf75bc7996e2ea81f17ff8d681129
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-21 13:38:53 +0200

    red test TestShouldMakeBagelWithFilling

--- src/test/java/com/booleanuk/core/inventoryTest.java
+++ src/test/java/com/booleanuk/core/inventoryTest.java
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.Test;
 public class inventoryTest {
 
     @Test
-    public void ShouldShowProduct(){
+    public void TestShouldMakeBagelWithFilling(){
+        Inventory inventory = new Inventory();
 
+        Bagel bagel = (Bagel) inventory.getItem("BGLP", "FILS");
 
+        Filling fillingValue = bagel.getFillingSku();
 
+        //This should fail because the filling is not the right one
+        Assertions.assertNotEquals(fillingValue.retrieveSku(), "FILB");
 
     }
 
