11eb1782caa083274e43c87dec6aff63e0ec6357
Iris Flystam <badde00@gmail.com>
2024-08-22 13:20:12 +0200

    REFACTOR: Can now add several fillings to bagel through ui

--- src/main/java/com/booleanuk/core/View.java
+++ src/main/java/com/booleanuk/core/View.java
@@ -38,9 +38,20 @@ public class View {
     public Bagel addBagel() {
         System.out.println("Choose a bagel");
         Triple<String, String, Float> bagelType = displayPrices("Bagel");
-        System.out.println("Choose a filling");
-        Triple<String, String, Float> fillingType = displayPrices("Filling");
-        Bagel b = new Bagel(bagelType, fillingType);
+        if (bagelType == null) return null;
+
+        System.out.println("Choose filling, choose None to stop choosing.");
+        boolean loop = true;
+        ArrayList<Triple<String, String, Float>> fillings = new ArrayList<>();
+        while (loop) {
+            Triple<String, String, Float> temp = displayPrices("Filling");
+            if (temp != null) {
+                fillings.add(temp);
+            } else {
+                loop = false;
+            }
+        }
+        Bagel b = new Bagel(bagelType, fillings);
         System.out.printf("It will cost $%.2f, is this ok?%n", b.calculateCost());
         System.out.println("1. Yes");
         System.out.println("2. No");
@@ -55,6 +66,7 @@ public class View {
     private Triple<String, String, Float> displayPrices(String filter) {
         ArrayList<Triple<String, String, Float>> temp = new ArrayList<>();
         int increment = 1;
+        System.out.println("0. None");
         for (Triple<String, String, Float> t: Controller.prices.values()) {
             if (t.a().equals(filter) || filter.isEmpty()) {
                 System.out.printf("%h. %s: $%.2f%n", increment, t.b(), t.c());
@@ -68,7 +80,7 @@ public class View {
         }
         System.out.println("Please write what you want");
         int output = getInt() - 1;
-        if (output == temp.size()) {
+        if (output >= temp.size() || output < 0) {
             return null;
         }
         return temp.get(output);
