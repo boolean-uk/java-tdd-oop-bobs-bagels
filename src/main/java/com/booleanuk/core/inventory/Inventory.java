34303f0a4edd2ef371cbd247ab5d6c01d185ca14
shyye <emma.art.dev@gmail.com>
2024-08-22 12:34:03 +0200

    Remake of inventory in another package

---
+++ src/main/java/com/booleanuk/core/inventory/Inventory.java
@@ -0,0 +1,51 @@
+package com.booleanuk.core.inventory;
+
+import com.booleanuk.core.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory {
+    Map<String, InventoryItem> inventoryItems;
+
+    public Inventory() {
+        this.inventoryItems = new HashMap<>();
+        fillInventory();
+    }
+
+    /**
+     * Fill inventory with pre-prepared data.
+     */
+    private void fillInventory() {
+
+        // TODO: Is it okay to initialize the data here?
+        // For a more general approach -> Use Dependency Injection:
+        // make product list as input instead of creating it here.
+
+        // Create list with inventory items
+        ArrayList<InventoryItem> items = new ArrayList<>();
+
+        items.add(new BagelItem(0.49f, BagelVariant.ONION));
+        items.add(new BagelItem(0.39f, BagelVariant.PLAIN));
+        items.add(new BagelItem(0.49f, BagelVariant.EVERYTHING));
+        items.add(new BagelItem(0.49f, BagelVariant.SESAME));
+
+        items.add(new CoffeeItem(0.99f, CoffeeVariant.BLACK));
+        items.add(new CoffeeItem(1.19f, CoffeeVariant.WHITE));
+        items.add(new CoffeeItem(1.29f, CoffeeVariant.CAPUCCINO));
+        items.add(new CoffeeItem(1.29f, CoffeeVariant.LATTE));
+
+        items.add(new FillingItem(0.12f, FillingVariant.BACON));
+        items.add(new FillingItem(0.12f, FillingVariant.EGG));
+        items.add(new FillingItem(0.12f, FillingVariant.CHEESE));
+        items.add(new FillingItem(0.12f, FillingVariant.CREAM_CHEESE));
+        items.add(new FillingItem(0.12f, FillingVariant.SMOKED_SALMON));
+        items.add(new FillingItem(0.12f, FillingVariant.HAM));
+
+        // Add items to inventoryItems and put the SKU value as the key
+        for (InventoryItem item : items) {
+            inventoryItems.put(item.getSKU(), item);
+        }
+    }
+}
