86f63df4fea1ee8d01869c68129c0e215a1e0e8d
Stian Rusvik <stian.rusvik@no.experis.com>
2024-08-22 13:06:16 +0200

    wrote test on second discount - Extension 1

--- src/test/java/com/booleanuk/core/OrderTest.java
+++ src/test/java/com/booleanuk/core/OrderTest.java
@@ -29,4 +29,28 @@ public class OrderTest {
         Double total = this.order.getTotalCost();
         Assertions.assertEquals(total, 2.49);
     }
+
+    @Test
+    public void testGetTotalReturnsDiscountOnTwelveBagels() throws FullBasketException {
+        Basket basket = this.order.getBasket();
+        basket.changeCapacity(12);
+
+        basket.addProduct(BagelType.ONION);
+        basket.addProduct(BagelType.ONION);
+        basket.addProduct(BagelType.ONION);
+        basket.addProduct(BagelType.ONION);
+        basket.addProduct(BagelType.ONION);
+        basket.addProduct(BagelType.ONION);
+
+        basket.addProduct(BagelType.PLAIN);
+        basket.addProduct(BagelType.PLAIN);
+        basket.addProduct(BagelType.PLAIN);
+        basket.addProduct(BagelType.PLAIN);
+        basket.addProduct(BagelType.PLAIN);
+        basket.addProduct(BagelType.PLAIN);
+
+        Double total = this.order.getTotalCost();
+        Assertions.assertEquals(total, 3.99);
+
+    }
 }
