03ec39371a77396fceb4d3576ab7b517a666b0e7
Dag Andreas <daanfofo@gmail.com>
2024-08-23 11:42:45 +0200

    renamed emum

--- src/test/java/com/booleanuk/core/TestOrderManager.java
+++ src/test/java/com/booleanuk/core/TestOrderManager.java
@@ -14,44 +14,44 @@ public class TestOrderManager {
     public void testaddItem(){
         OrderManager om = new OrderManager();
         om.setMaxCartSize(100);
-        String result = om.addItem(BagelType.Onion);
+        String result = om.addItem(Bagel.Onion);
         Assertions.assertEquals("Onion: 1", result);
 
 
-        result = om.addItem(CoffeeType.Cappuccino);
+        result = om.addItem(Coffee.Cappuccino);
         Assertions.assertEquals("Cappuccino: 1", result);
 
 
-        result = om.addItem(CoffeeType.Cappuccino);
+        result = om.addItem(Coffee.Cappuccino);
         Assertions.assertEquals("Cappuccino: 2", result);
 
         for (int i = 0; i < OrderManager.getMaxFillings(); i++) {
-            result = om.addItem(FillingType.Bacon);
+            result = om.addItem(Filling.Bacon);
             Assertions.assertEquals("Bacon: " + (i+1), result);
         }
 
-        result = om.addItem(FillingType.Bacon);
+        result = om.addItem(Filling.Bacon);
         Assertions.assertEquals("Item not in stock.", result);
     }
 
     @Test
     public void testGetPrice(){ // core
         OrderManager om = new OrderManager();
-        om.addItem(BagelType.Onion);
-        om.addItem(BagelType.Onion);
-        om.addItem(BagelType.Onion);
+        om.addItem(Bagel.Onion);
+        om.addItem(Bagel.Onion);
+        om.addItem(Bagel.Onion);
 
         double pricePerOnion = 0.49;
         double expectedPrice = 3* pricePerOnion;
         Assertions.assertEquals(expectedPrice, om.getTotalCartPrice());
 
-        om.addItem(CoffeeType.Cappuccino);
+        om.addItem(Coffee.Cappuccino);
         double pricePerCappuccino = 1.29;
         expectedPrice += pricePerCappuccino;
         Assertions.assertEquals(expectedPrice, om.getTotalCartPrice());
 
-        om.addItem(CoffeeType.Cappuccino);
-        om.addItem(CoffeeType.Cappuccino);
+        om.addItem(Coffee.Cappuccino);
+        om.addItem(Coffee.Cappuccino);
         expectedPrice += pricePerCappuccino*2;
         Assertions.assertEquals(expectedPrice, om.getTotalCartPrice());
     }
@@ -60,22 +60,22 @@ public class TestOrderManager {
     public void testRemoveItem(){
         OrderManager om = new OrderManager();
         String result;
-        result = om.removeItem(BagelType.Onion);
+        result = om.removeItem(Bagel.Onion);
         Assertions.assertEquals("Onion is not in cart.", result);
 
-        Assertions.assertEquals(20, om.getStockOfItem(BagelType.Onion));
-        om.addItem(BagelType.Onion);
-        Assertions.assertEquals(19, om.getStockOfItem(BagelType.Onion));
+        Assertions.assertEquals(20, om.getStockOfItem(Bagel.Onion));
+        om.addItem(Bagel.Onion);
+        Assertions.assertEquals(19, om.getStockOfItem(Bagel.Onion));
 
 
-        result = om.removeItem(BagelType.Onion);
+        result = om.removeItem(Bagel.Onion);
         Assertions.assertEquals("Removed Onion from cart.", result);
-        Assertions.assertEquals(20, om.getStockOfItem(BagelType.Onion));
+        Assertions.assertEquals(20, om.getStockOfItem(Bagel.Onion));
 
 
-        result = om.removeItem(BagelType.Onion);
+        result = om.removeItem(Bagel.Onion);
         Assertions.assertEquals("Onion is not in cart.", result);
-        Assertions.assertEquals(20, om.getStockOfItem(BagelType.Onion));
+        Assertions.assertEquals(20, om.getStockOfItem(Bagel.Onion));
     }
 
 
@@ -84,47 +84,47 @@ public class TestOrderManager {
         OrderManager om = new OrderManager();
 
         // Bagel tests
-        Assertions.assertEquals(0.49, om.getPriceOfItem(BagelType.Onion));
-        Assertions.assertEquals(0.39, om.getPriceOfItem(BagelType.Plain));
-        Assertions.assertEquals(0.49, om.getPriceOfItem(BagelType.Everything));
-        Assertions.assertEquals(0.49, om.getPriceOfItem(BagelType.Sesame));
+        Assertions.assertEquals(0.49, om.getPriceOfItem(Bagel.Onion));
+        Assertions.assertEquals(0.39, om.getPriceOfItem(Bagel.Plain));
+        Assertions.assertEquals(0.49, om.getPriceOfItem(Bagel.Everything));
+        Assertions.assertEquals(0.49, om.getPriceOfItem(Bagel.Sesame));
 
         // Coffee tests
-        Assertions.assertEquals(0.99, om.getPriceOfItem(CoffeeType.Black));
-        Assertions.assertEquals(1.19, om.getPriceOfItem(CoffeeType.White));
-        Assertions.assertEquals(1.29, om.getPriceOfItem(CoffeeType.Cappuccino));
-        Assertions.assertEquals(1.29, om.getPriceOfItem(CoffeeType.Latte));
+        Assertions.assertEquals(0.99, om.getPriceOfItem(Coffee.Black));
+        Assertions.assertEquals(1.19, om.getPriceOfItem(Coffee.White));
+        Assertions.assertEquals(1.29, om.getPriceOfItem(Coffee.Cappuccino));
+        Assertions.assertEquals(1.29, om.getPriceOfItem(Coffee.Latte));
 
         // Filling tests
-        Assertions.assertEquals(0.12, om.getPriceOfItem(FillingType.Bacon));
-        Assertions.assertEquals(0.12, om.getPriceOfItem(FillingType.Egg));
-        Assertions.assertEquals(0.12, om.getPriceOfItem(FillingType.Cheese));
-        Assertions.assertEquals(0.12, om.getPriceOfItem(FillingType.Cream_Cheese));
-        Assertions.assertEquals(0.12, om.getPriceOfItem(FillingType.Smoked_Salmon));
-        Assertions.assertEquals(0.12, om.getPriceOfItem(FillingType.Ham));
+        Assertions.assertEquals(0.12, om.getPriceOfItem(Filling.Bacon));
+        Assertions.assertEquals(0.12, om.getPriceOfItem(Filling.Egg));
+        Assertions.assertEquals(0.12, om.getPriceOfItem(Filling.Cheese));
+        Assertions.assertEquals(0.12, om.getPriceOfItem(Filling.Cream_Cheese));
+        Assertions.assertEquals(0.12, om.getPriceOfItem(Filling.Smoked_Salmon));
+        Assertions.assertEquals(0.12, om.getPriceOfItem(Filling.Ham));
     }
 
     @Test
      public void testDiscountPrice(){
         OrderManager om = new OrderManager();
         for (int i = 0; i < 16 ; i++) {
-            om.addItem(BagelType.Plain);
+            om.addItem(Bagel.Plain);
         }
         Assertions.assertEquals(5.55, om.getTotalDiscountedPrice());
         System.out.println(om.getTotalDiscountRecieptString());
 
         om = new OrderManager();
         for (int i = 0; i < 2; i++) {
-            om.addItem(BagelType.Onion); // 0.49*3
+            om.addItem(Bagel.Onion); // 0.49*3
         }
         for (int i = 0; i < 12; i++) {
-            om.addItem(BagelType.Plain); // 3.99
+            om.addItem(Bagel.Plain); // 3.99
         }
         for (int i = 0; i < 6; i++) {
-            om.addItem(BagelType.Everything); // 2.49
+            om.addItem(Bagel.Everything); // 2.49
         }
         for (int i = 0; i < 3; i++) {
-            om.addItem(CoffeeType.Black); // 0.99*3
+            om.addItem(Coffee.Black); // 0.99*3
         }
 
 
@@ -138,8 +138,8 @@ public class TestOrderManager {
     @Test
     public void testDiscountCoffee(){
         OrderManager om = new OrderManager();
-        om.addItem(CoffeeType.Black);
-        om.addItem(BagelType.Plain);
+        om.addItem(Coffee.Black);
+        om.addItem(Bagel.Plain);
         Assertions.assertEquals(1.25, om.getTotalDiscountedPrice());
 
         String res = om.getTotalDiscountRecieptString();
@@ -150,7 +150,7 @@ public class TestOrderManager {
     /** My sample receipt:
      *
      *        ~~~Bob's Bagels~~~
-     *             2024-08-22
+     *            2024-08-22
      *             14:54:47
      * =================================
      *
@@ -173,38 +173,38 @@ public class TestOrderManager {
 
 
         // 2 of each bagel type
-        om.addItem(BagelType.Plain);
-        om.addItem(BagelType.Plain);
-        om.addItem(BagelType.Onion);
-        om.addItem(BagelType.Onion);
-        om.addItem(BagelType.Everything);
-        om.addItem(BagelType.Everything);
-        om.addItem(BagelType.Sesame);
-        om.addItem(BagelType.Sesame);
+        om.addItem(Bagel.Plain);
+        om.addItem(Bagel.Plain);
+        om.addItem(Bagel.Onion);
+        om.addItem(Bagel.Onion);
+        om.addItem(Bagel.Everything);
+        om.addItem(Bagel.Everything);
+        om.addItem(Bagel.Sesame);
+        om.addItem(Bagel.Sesame);
 
         // 2 of each coffee type
-        om.addItem(CoffeeType.Black);
-        om.addItem(CoffeeType.Black);
-        om.addItem(CoffeeType.White);
-        om.addItem(CoffeeType.White);
-        om.addItem(CoffeeType.Cappuccino);
-        om.addItem(CoffeeType.Cappuccino);
-        om.addItem(CoffeeType.Latte);
-        om.addItem(CoffeeType.Latte);
+        om.addItem(Coffee.Black);
+        om.addItem(Coffee.Black);
+        om.addItem(Coffee.White);
+        om.addItem(Coffee.White);
+        om.addItem(Coffee.Cappuccino);
+        om.addItem(Coffee.Cappuccino);
+        om.addItem(Coffee.Latte);
+        om.addItem(Coffee.Latte);
 
         // 2 of each filling type
-        om.addItem(FillingType.Bacon);
-        om.addItem(FillingType.Bacon);
-        om.addItem(FillingType.Egg);
-        om.addItem(FillingType.Egg);
-        om.addItem(FillingType.Cheese);
-        om.addItem(FillingType.Cheese);
-        om.addItem(FillingType.Cream_Cheese);
-        om.addItem(FillingType.Cream_Cheese);
-        om.addItem(FillingType.Smoked_Salmon);
-        om.addItem(FillingType.Smoked_Salmon);
-        om.addItem(FillingType.Ham);
-        om.addItem(FillingType.Ham);
+        om.addItem(Filling.Bacon);
+        om.addItem(Filling.Bacon);
+        om.addItem(Filling.Egg);
+        om.addItem(Filling.Egg);
+        om.addItem(Filling.Cheese);
+        om.addItem(Filling.Cheese);
+        om.addItem(Filling.Cream_Cheese);
+        om.addItem(Filling.Cream_Cheese);
+        om.addItem(Filling.Smoked_Salmon);
+        om.addItem(Filling.Smoked_Salmon);
+        om.addItem(Filling.Ham);
+        om.addItem(Filling.Ham);
 
         Double expectedPrice = 14.81-0.39;
         String receipt = om.getTotalDiscountRecieptString();
@@ -219,8 +219,8 @@ public class TestOrderManager {
     public void testCoffeeAndBagel(){
         OrderManager om = new OrderManager();
         for (int i = 0; i < 2; i++) {
-            om.addItem(CoffeeType.Black);
-            om.addItem(BagelType.Plain);
+            om.addItem(Coffee.Black);
+            om.addItem(Bagel.Plain);
         }
 
         String expectedSubString =
@@ -242,7 +242,7 @@ public class TestOrderManager {
     public void testTestDiscountValues(){
         OrderManager om = new OrderManager();
         for (int i = 0; i < 12; i++)
-            om.addItem(BagelType.Plain);
+            om.addItem(Bagel.Plain);
 
         double priceOfTwelvePlain = 0.39*12;
         Assertions.assertEquals(3.99, om.getTotalDiscountReciept().price);
@@ -251,7 +251,7 @@ public class TestOrderManager {
 
         om = new OrderManager();
         for (int i = 0; i < 12; i++)
-            om.addItem(BagelType.Onion);
+            om.addItem(Bagel.Onion);
 
         double priceOfTwelveOnion = 0.49*12;
         Assertions.assertEquals(3.99, om.getTotalDiscountReciept().price);
@@ -259,7 +259,7 @@ public class TestOrderManager {
 
         om = new OrderManager();
         for (int i = 0; i < 12; i++)
-            om.addItem(BagelType.Everything);
+            om.addItem(Bagel.Everything);
 
         double priceOfTwelveEverything = 0.49*12;
         Assertions.assertEquals(3.99, om.getTotalDiscountReciept().price);
@@ -267,7 +267,7 @@ public class TestOrderManager {
 
         om = new OrderManager();
         for (int i = 0; i < 12; i++)
-            om.addItem(BagelType.Sesame);
+            om.addItem(Bagel.Sesame);
 
 
         double priceOfTwelveSesame = 0.49*12;
@@ -275,10 +275,14 @@ public class TestOrderManager {
         Assertions.assertEquals(priceOfTwelveSesame, om.getTotalCartPrice());
 
 
+        Receipt r = om.getTotalDiscountReciept();
+        String res = r.getTotalDiscountRecieptString();
+        System.out.println(res);
 
 
-
-
+        for (int i = 0; i < 200; i++) {
+            r = om.getTotalDiscountReciept();
+        }
 
 
     }
