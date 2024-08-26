6794ecb367474544ccde38361e38a9b636af29f3
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-26 10:49:21 +0200

    Updated test

--- src/test/java/com/booleanuk/extension/ItemTest.java
+++ src/test/java/com/booleanuk/extension/ItemTest.java
@@ -13,7 +13,7 @@ public class ItemTest {
     @Test
     public void testBagel() {
         Bagel bagel = new Bagel("BGLO", 0);
-        assertEquals(0.49, bagel.getPrice());
+        assertEquals(4900, bagel.getPrice());
 
         Bagel bagel1 = new Bagel("BGLOF", 0);
         assertNull(bagel1.getVariant());
@@ -22,10 +22,10 @@ public class ItemTest {
     @Test
     public void testCoffee() {
         Coffee coffee = new Coffee("COFB", 0);
-        assertEquals(0.99, coffee.getPrice());
+        assertEquals(9900, coffee.getPrice());
 
         Coffee coffee1 = new Coffee("COFL", 0);
-        assertEquals(1.29, coffee1.getPrice());
+        assertEquals(12900, coffee1.getPrice());
 
         Coffee coffee2 = new Coffee("COFBu", 0);
         assertNull(coffee2.getVariant());
