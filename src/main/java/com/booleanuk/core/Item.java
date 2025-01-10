b9d1c4836443abb9968631e4f48af7abfcba8d54
Robin Kaga <robin.kaga@no.experis.com>
2025-01-10 13:22:52 +0100

    added methods for Item

--- src/main/java/com/booleanuk/core/Item.java
+++ src/main/java/com/booleanuk/core/Item.java
@@ -1,4 +1,23 @@
 package com.booleanuk.core;
 
 public class Item {
+    private String sku;
+    private String name;
+    private String variant;
+    private float price;
+
+    public Item(String sku, String name, String variant, float price){
+        this.sku = sku;
+        this.name = name;
+        this.variant = variant;
+        this.price = price;
+    }
+
+    public String getSku() {return sku;}
+
+    public String getName() {return name;}
+
+    public String getVariant() {return variant;}
+
+    public float getPrice() {return price;}
 }
--- src/test/java/com/booleanuk/core/ItemTest.java
+++ src/test/java/com/booleanuk/core/ItemTest.java
@@ -7,9 +7,9 @@ public class ItemTest {
 
     @Test
     public void testGetSku(){
-        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39);
-        Item item2 = new Item("COFB", "Coffee", "Black", 0.99);
-        Item item3 = new Item("FILE", "Filling", "Egg", 0.12);
+        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39f);
+        Item item2 = new Item("COFB", "Coffee", "Black", 0.99f);
+        Item item3 = new Item("FILE", "Filling", "Egg", 0.12f);
         Assertions.assertEquals("BGLP", item1.getSku());
         Assertions.assertEquals("COFB", item2.getSku());
         Assertions.assertEquals("FILE", item3.getSku());
@@ -17,19 +17,19 @@ public class ItemTest {
 
     @Test
     public void testGetName(){
-        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39);
-        Item item2 = new Item("COFB", "Coffee", "Black", 0.99);
-        Item item3 = new Item("FILE", "Filling", "Egg", 0.12);
+        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39f);
+        Item item2 = new Item("COFB", "Coffee", "Black", 0.99f);
+        Item item3 = new Item("FILE", "Filling", "Egg", 0.12f);
         Assertions.assertEquals("Bagel", item1.getName());
-        Assertions.assertEquals("Coffe", item2.getName());
+        Assertions.assertEquals("Coffee", item2.getName());
         Assertions.assertEquals("Filling", item3.getName());
     }
 
     @Test
     public void testGetVariant(){
-        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39);
-        Item item2 = new Item("COFB", "Coffee", "Black", 0.99);
-        Item item3 = new Item("FILE", "Filling", "Egg", 0.12);
+        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39f);
+        Item item2 = new Item("COFB", "Coffee", "Black", 0.99f);
+        Item item3 = new Item("FILE", "Filling", "Egg", 0.12f);
         Assertions.assertEquals("Plain", item1.getVariant());
         Assertions.assertEquals("Black", item2.getVariant());
         Assertions.assertEquals("Egg", item3.getVariant());
@@ -37,11 +37,11 @@ public class ItemTest {
 
     @Test
     public void testGetPrice(){
-        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39);
-        Item item2 = new Item("COFB", "Coffee", "Black", 0.99);
-        Item item3 = new Item("FILE", "Filling", "Egg", 0.12);
-        Assertions.assertEquals(0.39, item1.getPrice());
-        Assertions.assertEquals(0.99, item2.getPrice());
-        Assertions.assertEquals(0.12, item3.getPrice());
+        Item item1 = new Item("BGLP", "Bagel", "Plain", 0.39f);
+        Item item2 = new Item("COFB", "Coffee", "Black", 0.99f);
+        Item item3 = new Item("FILE", "Filling", "Egg", 0.12f);
+        Assertions.assertEquals(0.39f, item1.getPrice());
+        Assertions.assertEquals(0.99f, item2.getPrice());
+        Assertions.assertEquals(0.12f, item3.getPrice());
     }
 }
