af250018a18f58e011280314ca52ccc803fb3ef1
Marcus Ikdal <marcus.ikdal@gmail.com>
2025-01-10 15:45:09 +0100

    created extension files

---
+++ src/main/java/com/booleanuk/extension/Basket.java
@@ -0,0 +1,46 @@
+package com.booleanuk.extension;
+
+import java.util.ArrayList;
+
+public class Basket {
+    private Inventory inventory;
+    private int capacity;
+    private ArrayList<Item> items = new ArrayList<>();
+
+    public Basket(Inventory inventory, int capacity) {
+        this.inventory = inventory;
+        this.capacity = capacity;
+    }
+
+    boolean addItem(Item item) {
+        // if basket is not full and item is in stock
+        if (items.size() < capacity && inventory.checkStock(item.getSku())) {
+            items.add(item);
+            inventory.removeStock(item.getSku(), 1); // remove item from stock
+            return true;
+        }
+        return false;
+    }
+
+    boolean removeItem(SKU sku) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getSku() == sku) {
+                items.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    float getTotalCost() {
+        float total = 0f;
+        for (Item i : items) {
+            total += i.getPrice();
+        }
+        return total;
+    }
+
+    ArrayList<Item> getItems() {
+        return items;
+    }
+}
---
+++ src/test/java/com/booleanuk/extension/TestBasketExtension.java
@@ -0,0 +1,69 @@
+package com.booleanuk.extension;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestBasketExtension {
+    @Test    
+    void testAddItemBasketNotFullInStock() {
+        // add onion bagle to stock
+        Inventory inventory = new Inventory();
+        inventory.addStock(SKU.BGLO, 1);
+
+        Basket basket = new Basket(inventory, 3);
+
+        Item item = new Item(SKU.BGLO);
+
+        Assertions.assertTrue(basket.addItem(item));
+        Assertions.assertEquals(SKU.BGLO, basket.getItems().get(0).getSku());
+    }
+
+    @Test    
+    void testAddItemNotInStock() {
+        Inventory inventory = new Inventory();
+        Basket basket = new Basket(inventory, 3);
+
+        Item item = new Item(SKU.BGLO);
+
+        Assertions.assertFalse(basket.addItem(item));
+    }
+
+    @Test    
+    void testAddItemFullBasket() {
+        Inventory inventory = new Inventory();
+        Basket basket = new Basket(inventory, 0);
+
+        Item item = new Item(SKU.BGLO);
+
+        Assertions.assertFalse(basket.addItem(item));
+    }
+
+    @Test
+    void testRemoveItemThatExists() {
+        // add onion bagle to stock
+        Inventory inventory = new Inventory();
+        inventory.addStock(SKU.BGLO, 1);
+
+        Basket basket = new Basket(inventory, 5);
+        basket.addItem(new Item(SKU.BGLO));
+
+        Assertions.assertTrue(basket.removeItem(SKU.BGLO));
+        Assertions.assertFalse(inventory.checkStock(SKU.BGLO));
+    }
+
+    @Test
+    void testGetTotalCost() {
+        Inventory inventory = new Inventory();
+
+        inventory.addStock(SKU.BGLO,2);
+        inventory.addStock(SKU.COFB, 1);
+
+        Basket basket = new Basket(inventory, 5);
+        basket.addItem(new Item(SKU.BGLO));
+        basket.addItem(new Item(SKU.BGLO));
+        basket.addItem(new Item(SKU.COFB));
+
+        Assertions.assertEquals(1.97f, basket.getTotalCost());
+
+    }
+}
