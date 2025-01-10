361fbdb3308d9afdd0d818e93cca2571cbf0a8b1
Marcus Ikdal <marcus.ikdal@gmail.com>
2025-01-10 13:27:27 +0100

    changed type to be String

--- src/main/java/com/booleanuk/core/Item.java
+++ src/main/java/com/booleanuk/core/Item.java
@@ -3,10 +3,17 @@ package com.booleanuk.core;
 public class Item {
     private SKU sku;
     private float price;
-    private ItemType type;
+    private String type;
     private String variant;
 
     public Item(SKU sku) {
         this.sku = sku;
+
+        // set price, type and variant based on sku
+        if (sku == SKU.BGLO) {
+            price = 0.49f;
+            type = "Bagel";
+            variant = "Onion";
+        } 
     }
 }
