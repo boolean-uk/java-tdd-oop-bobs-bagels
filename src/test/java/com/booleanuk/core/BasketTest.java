4b2f21b3b1ace169cdbb6e75ea45354d85e90403
Mattias Hedbom <mattias.hedbom@se.experis.com>
2025-01-13 14:30:15 +0100

    Initial test for addItem() in basket-class completed

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,4 +1,33 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class BasketTest {
+
+    private ArrayList<Item> itemsInBasket = new ArrayList<>(){{
+        // Adding the bagels
+        Item onionBagel = new OnionBagel(0.49, "BGLO", "Onion", "Bagel") {};
+        add(onionBagel);
+        Item plainBagel = new PlainBagel(0.39, "BGLP", "Plain", "Bagel") {};
+        add(plainBagel);
+        Item cappucinoCoffee = new CappucinoCoffee(1.29, "COFC", "Cappuccino", "Coffee"){};
+        add(cappucinoCoffee);
+        Item latteCoffee = new LatteCoffee(1.29, "COFL", "Latte", "Coffee"){};
+        add(latteCoffee);
+        Item baconFilling = new BaconFilling(0.12, "FILB", "Bacon", "Filling"){};
+        add(baconFilling);
+        Item eggFilling = new EggFilling(0.12, "FILE", "Egg", "Filling"){};
+        add(eggFilling);
+    }};
+
+    @Test
+    public void basketIsFullWhenAdding(){
+        Basket basket = new Basket(itemsInBasket, 6);
+        Item itemToAdd = new OnionBagel(0.49, "BGLO", "Onion", "Bagel"){};
+
+        Assertions.assertEquals("Basket is full!", basket.addItem(itemToAdd));
+    }
 }
