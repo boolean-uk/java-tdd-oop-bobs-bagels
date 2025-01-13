e0df841d74423329cdc2406fedc2bdfb7a0a30f3
Mattias Hedbom <mattias.hedbom@se.experis.com>
2025-01-13 12:58:03 +0100

    Created the initial seePrice() method

--- src/main/java/com/booleanuk/core/Menu.java
+++ src/main/java/com/booleanuk/core/Menu.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Menu {
+    private ArrayList<Item> itemsOnMenu;
+
+    public Menu(){
+        this.itemsOnMenu = new ArrayList<Item>();
+    }
+
+    public String seePrice(Item itemToCheck){
+        return "Item dont exist on the menu!";
+    }
 }
