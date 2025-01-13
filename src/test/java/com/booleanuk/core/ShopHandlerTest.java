ff0fabb22cb51deee952a0ca5d96787d2b71b50c
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-13 11:35:05 +0100

    Add test for showing stock (excl. filling)

--- src/test/java/com/booleanuk/core/ShopHandlerTest.java
+++ src/test/java/com/booleanuk/core/ShopHandlerTest.java
@@ -1,11 +1,19 @@
 package com.booleanuk.core;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class ShopHandlerTest {
     @Test
-    public void test() {
-
+    public void showItemsShowsAllItemsThatAreNotFillings() {
+        ShopHandler sh = new ShopHandler();
+        String list = sh.showItems();
+        System.out.println(list);
+        for (Item item : ShopHandler.getStock()) {
+            if (!item.getName().equals("Filling")) {
+                // if item.name & price is in list, good
+                assertTrue(list.contains(item.getName() + "\t" + item.getVariant() + (item.getVariant().length() > 6 ? "\t" : "\t\t") + item.getPrice()));
+            }
+        }
     }
 }
