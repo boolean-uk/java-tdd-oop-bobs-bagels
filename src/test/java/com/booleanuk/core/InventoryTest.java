0c5c2822c6140a742798dc5ca886211b64cc5e26
Robin Kaga <robin.kaga@no.experis.com>
2025-01-13 10:13:35 +0100

    added inventory tests

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -6,11 +6,22 @@ import org.junit.jupiter.api.Test;
 public class InventoryTest {
 
     @Test
-    public voidAddStockItem(){
+    public void testAddStockItem(){
         Inventory inventory  = new Inventory();
         inventory.addStockItem("BGLP", 1);
         inventory.addStockItem("FILE", 1);
         Assertions.assertEquals(2, inventory.stock.size());
+    }
+
+    @Test
+    public void testRemoveStockItem(){
+        Inventory inventory = new Inventory();
+        inventory.addStockItem("BGLP", 1);
+        inventory.addStockItem("FILE", 1);
+        Assertions.assertEquals(2, inventory.stock.size());
+
+        inventory.removeStockItem("FILE", 1);
+        Assertions.assertEquals(1, inventory.stock.size());
     }
 
     @Test
