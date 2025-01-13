2d4834bd115839a1f8b2828dce106b1ce095b832
Magnus-droid <magnuskrumbacher@gmail.com>
2025-01-13 11:53:50 +0100

    Imporved tests and added tests for max capacity

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -14,6 +14,7 @@ public class BasketTest {
         Item item = factory.createItem("BGLP");
         boolean successfullyAdded = basket.addItem(item); // add item
         Assertions.assertTrue(successfullyAdded);
+        Assertions.assertEquals(item, basket.getBasket().getFirst());
     }
 
     @Test
@@ -30,6 +31,8 @@ public class BasketTest {
         boolean successfullyAdded6 = basket.addItem(item);
         Assertions.assertTrue(successfullyAdded5);
         Assertions.assertFalse(successfullyAdded6);
+        //check if 6 elements were added despite the max capacity being 5
+        Assertions.assertThrows(IndexOutOfBoundsException.class , () -> basket.getBasket().get(5));
     }
 
     @Test
@@ -40,20 +43,27 @@ public class BasketTest {
         basket.addItem(item);
         boolean successfullyRemoved = basket.removeItem(item);
         Assertions.assertTrue(successfullyRemoved);
+        Assertions.assertFalse(basket.getBasket().contains(item));
     }
 
     @Test
     public void testRemoveNonExistingItem() {
         Basket basket = new Basket();
         ItemFactory factory = new ItemFactory();
-        Item item = factory.createItem("BGLP"); //Item never gets added to basket
-        boolean successfullyRemoved = basket.removeItem(item);
+        Item item1 = factory.createItem("BGLO");
+        basket.addItem(item1);
+        Item item2 = factory.createItem("BGLP"); //Item2 never gets added to basket
+        boolean successfullyRemoved = basket.removeItem(item2);
         Assertions.assertFalse(successfullyRemoved);
+        Assertions.assertEquals(1, basket.getBasket().size()); //Check if the basket still holds the items it should
+
     }
 
     @Test
     public void testChangeCapacity() {
-
+        Basket basket = new Basket(); //basket has capacity of 5 by default
+        basket.changeCapacity(7);
+        Assertions.assertEquals(7, basket.getMaxCapacity());
     }
 
     @Test
