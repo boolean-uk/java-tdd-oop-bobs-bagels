0ee6ece9e860bee5481bd599d9db28265dd8d738
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-13 15:43:01 +0100

    Refactor Bagel and Coffee to distinct classes

---
+++ src/main/java/com/booleanuk/extension/Bagel.java
@@ -0,0 +1,20 @@
+package com.booleanuk.extension;
+
+public class Bagel extends Item {
+    private Item filling = null;
+    public Bagel(String sku, double price, String name, String variant, int size) {
+        super(sku, price, name, variant, size);
+    }
+
+    public Bagel(String sku, double price, String name, String variant) {
+        super(sku, price, name, variant);
+    }
+
+    public void setFilling(Item filling) {
+        this.filling = filling;
+    }
+
+    public Item getFilling() {
+        return this.filling;
+    }
+}
---
+++ src/main/java/com/booleanuk/extension/Coffee.java
@@ -0,0 +1,21 @@
+package com.booleanuk.extension;
+
+public class Coffee extends Item {
+    private Bagel discountBagel = null;
+
+    public Coffee(String sku, double price, String name, String variant, int size) {
+        super(sku, price, name, variant, size);
+    }
+
+    public Coffee(String sku, double price, String name, String variant) {
+        super(sku, price, name, variant);
+    }
+
+    public Bagel getDiscountBagel() {
+        return discountBagel;
+    }
+
+    public void setDiscountBagel(Bagel discountBagel) {
+        this.discountBagel = discountBagel;
+    }
+}
