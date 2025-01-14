84502ed528595abf0659e03f64e9ee05fcfb73c4
Frida Anselin <frida.anselin@se.experis.com>
2025-01-15 00:11:07 +0100

    Refactor inventory class, add testPrintInventory - red

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InventoryTest {
     private Inventory inventory;
@@ -14,20 +14,17 @@ public class InventoryTest {
 
     @BeforeEach
     public void setUp() {
-        ArrayList<Item> inventoryItems = new ArrayList<>();
+        HashMap<String, Item> inventoryItems = new HashMap<>() {{
+            put("BGLS", itemB);
+            put("COFB", itemC);
+        }};
         inventory = new Inventory(inventoryItems);
         itemB = new Item("BGLS", 0.49, "Bagel", "Sesame");
         itemC = new Item("COFB", 0.99, "Coffee" , "Black");
-        itemF = new Item("FILX", 0.12, "Filling", "Cream Cheese");
     }
 
     @Test
-    public void testHasItem() {
-        ArrayList<Item> inventoryItems = new ArrayList<>() {{
-            add(itemB);
-            add(itemC);
-            add(itemF);
-        }};
-        Assertions.assertTrue(inventoryItems.hasItem(itemF));
+    public void testPrintInventory() {
+        Assertions.assertEquals("BGLS, 0.49, Bagel, Sesame \n COFB, 0.99, Coffee, Black" ,inventory.printInventory());
     }
 }
